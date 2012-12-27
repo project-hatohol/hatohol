014cc88f3f5f8ec25340a9758888b7920078bf7c
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2012-12-27 17:34:50 +0900

    added a part of code to handshake with MySQL.

--- src/FaceMySQLWorker.cc
+++ src/FaceMySQLWorker.cc
@@ -1,4 +1,6 @@
 #include <cstring>
+#include <map>
+using namespace std;
 
 #include <Logger.h>
 using namespace mlpl;
@@ -6,7 +8,51 @@ using namespace mlpl;
 #include "Utils.h"
 #include "FaceMySQLWorker.h"
 
-static const int SERVER_STATUS_AUTOCOMMIT = 0x02;
+static const uint32_t SERVER_STATUS_AUTOCOMMIT = 0x00000002;
+
+static const uint32_t CLIENT_LONG_PASSWORD     = 0x00000001;
+static const uint32_t CLIENT_CLIENT_FOUND_ROWS = 0x00000002;
+static const uint32_t CLIENT_CLIENT_LONG_FLAG  = 0x00000004;
+static const uint32_t CLIENT_CONNECT_WITH_DB   = 0x00000008;
+static const uint32_t CLIENT_NO_SCHEMA         = 0x00000010;
+static const uint32_t CLIENT_COMPRESS          = 0x00000020;
+static const uint32_t CLIENT_ODBC              = 0x00000040;
+static const uint32_t CLIENT_LOCAL_FILES       = 0x00000080;
+static const uint32_t CLIENT_IGNORE_SPACE      = 0x00000100;
+static const uint32_t CLIENT_PROTOCOL_41       = 0x00000200;
+static const uint32_t CLIENT_INTERACTIVE       = 0x00000400;
+static const uint32_t CLIENT_SSL               = 0x00000800;
+static const uint32_t CLIENT_IGNORE_SIGPIPE    = 0x00001000;
+static const uint32_t CLIENT_TRANSACTIONS      = 0x00002000;
+static const uint32_t CLIENT_RESERVED          = 0x00004000;
+static const uint32_t CLIENT_SECURE_CONNECTION = 0x00008000;
+static const uint32_t CLIENT_MULTI_STATEMENTS  = 0x00010000;
+static const uint32_t CLIENT_MULTI_RESULTS     = 0x00020000;
+static const uint32_t CLIENT_PS_MULTI_RESULTS  = 0x00040000;
+static const uint32_t CLIENT_PLUGIN_AUTH       = 0x00080000;
+static const uint32_t CLIENT_CONNECT_ATTRS     = 0x00100000;
+static const uint32_t CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA = 0x00200000;
+
+struct HandshakeResponse41
+{
+	uint32_t capability;
+	uint32_t maxPacketSize;
+	uint8_t  characterSet;
+	uint8_t  reserved[23];
+	string   username;
+	uint16_t lenAuthLesponse;
+	string   authResponse;
+
+	//  if capabilities & CLIENT_CONNECT_WITH_DB
+	string   database;
+
+	// if capabilities & CLIENT_PLUGIN_AUTH
+	string authPluginName;
+
+	// if capabilities & CLIENT_CONNECT_ATTRS
+	uint16_t lenKeyValue;
+	map<string, string> keyValueMap;
+};
 
 // ---------------------------------------------------------------------------
 // Public methods
@@ -50,6 +96,8 @@ gpointer FaceMySQLWorker::mainThread(void)
 	makeHandshakeV10(buf);
 	if (!send(buf))
 		return NULL;
+	if (!receiveHandshakeResponse41())
+		return NULL;
 
 	return NULL;
 }
@@ -58,6 +106,7 @@ void FaceMySQLWorker::makeHandshakeV10(SmartBuffer &buf)
 {
 	// protocol version
 	static const char SERVER_VERSION[] = "5.5.10";
+	static const char AUTH_PLUGIN_NAME[] = "mysql_native_password";
 	char authPluginData[] = "AUTH_PLUGIN_DATA";
 
 	static const int LEN_PKT_LENGTH = 4;
@@ -72,25 +121,48 @@ void FaceMySQLWorker::makeHandshakeV10(SmartBuffer &buf)
 	static const int LEN_CAPABILITY_FLAGS_2 = 2;
 	static const int LEN_LENGTH_AUTH_PLUGIN_DATA = 1;
 	static const int LEN_RESERVED = 10;
+	static const int LEN_AUTH_PLUGIN_NAME = sizeof(AUTH_PLUGIN_NAME);
 
-	static const uint32_t LEN_TOTAL =
+	const uint8_t lenAuthPluginData = strlen(authPluginData);
+	const uint32_t lenAuthPluginDataPart2 = 
+	        lenAuthPluginData - LEN_AUTH_PLUGIN_DATA_PART_1 + 1;
+	const uint32_t LEN_TOTAL =
 	  LEN_PKT_LENGTH + LEN_PROTOCOL_VERSION +
 	  LEN_SERVER_VERSION + LEN_CONNECTION_ID +
 	  LEN_AUTH_PLUGIN_DATA_PART_1 + LEN_FILLER_1 +
 	  LEN_CAPABILITY_FLAGS_1 + LEN_CARACTER_SET + LEN_STATUS_FLAGS +
-	  LEN_CAPABILITY_FLAGS_2 + LEN_LENGTH_AUTH_PLUGIN_DATA + LEN_RESERVED;
-
-	static const uint8_t PROTOCOL_VERSION = 10;
+	  LEN_CAPABILITY_FLAGS_2 + LEN_LENGTH_AUTH_PLUGIN_DATA + LEN_RESERVED +
+	  lenAuthPluginDataPart2 + 
+	  LEN_AUTH_PLUGIN_NAME;
 
 	buf.alloc(LEN_TOTAL);
-	buf.add32(LEN_TOTAL);
+	buf.add32(LEN_TOTAL - LEN_PKT_LENGTH);
+
+	static const uint8_t PROTOCOL_VERSION = 10;
 	buf.add8(PROTOCOL_VERSION);
+
 	buf.add(SERVER_VERSION, LEN_SERVER_VERSION);
 	buf.add32(m_connId);
 	buf.add(authPluginData, LEN_AUTH_PLUGIN_DATA_PART_1);
 	buf.add8(0); // Filler
 
-	static const uint16_t capability1 = 0;
+	static const uint16_t capability1 =
+	  CLIENT_LONG_PASSWORD |
+	  CLIENT_CLIENT_FOUND_ROWS |
+	  CLIENT_CLIENT_LONG_FLAG |
+	  CLIENT_CONNECT_WITH_DB |
+	  CLIENT_NO_SCHEMA |
+	  CLIENT_COMPRESS |
+	  CLIENT_ODBC |
+	  CLIENT_LOCAL_FILES |
+	  CLIENT_IGNORE_SPACE |
+	  CLIENT_PROTOCOL_41 |
+	  CLIENT_INTERACTIVE  |
+	  // CLIENT_SSL
+	  CLIENT_IGNORE_SIGPIPE |
+	  CLIENT_TRANSACTIONS |
+	  CLIENT_RESERVED |
+	  CLIENT_SECURE_CONNECTION;
 	buf.add16(capability1);
 
 	static const uint8_t char_set = 8; // latin1?
@@ -99,23 +171,65 @@ void FaceMySQLWorker::makeHandshakeV10(SmartBuffer &buf)
 	static const uint8_t status = SERVER_STATUS_AUTOCOMMIT;
 	buf.add16(status);
 
-	static const uint16_t capability2 = 0;
+	static const uint16_t capability2 = (
+	  CLIENT_MULTI_STATEMENTS |
+	  CLIENT_MULTI_RESULTS |
+	  CLIENT_PS_MULTI_RESULTS |
+	  CLIENT_PLUGIN_AUTH
+	  // CLIENT_CONNECT_ATTRS
+	  // CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA
+	) >> 16;
+
 	buf.add16(capability2);
 
-	uint8_t lenAuthPluginData = strlen(authPluginData);
 	buf.add8(lenAuthPluginData);
 
 	buf.addZero(LEN_RESERVED);
 
 	buf.add(&authPluginData[LEN_AUTH_PLUGIN_DATA_PART_1],
-	        lenAuthPluginData - LEN_AUTH_PLUGIN_DATA_PART_1 + 1);
+	        lenAuthPluginDataPart2);
+	buf.add(AUTH_PLUGIN_NAME, LEN_AUTH_PLUGIN_NAME);
+}
+
+bool FaceMySQLWorker::receiveHandshakeResponse41(void)
+{
+	uint32_t pktSize;
+	if (!recive(reinterpret_cast<char *>(&pktSize), sizeof(pktSize)))
+		return false;
+	MLPL_INFO("pktSize: %08x\n", pktSize);
+	return true;
+}
+
+bool FaceMySQLWorker::recive(char* buf, size_t size)
+{
+	GError *error = NULL;
+
+	size_t remain_size = size;
+	while (remain_size > 0) {
+		gssize ret = g_socket_receive(m_socket, buf, remain_size,
+		                              NULL, &error);
+		if (ret == -1) {
+			MLPL_ERR("Failed to call g_socket_receive: %s\n",
+			        error->message);
+			g_error_free(error);
+			return false;
+		}
+		if (ret == 0)
+			return false;
+		if (ret > remain_size) {
+			MLPL_BUG("ret: %d > remain_size: %zd\n",
+			         ret, remain_size);
+			return false;
+		}
+		remain_size -= ret;
+	}
+	return true;
 }
 
 bool FaceMySQLWorker::send(SmartBuffer &buf)
 {
 	GError *error = NULL;
-	gssize ret = g_socket_send(m_socket, buf, buf.size(),
-	                           NULL, &error);
+	gssize ret = g_socket_send(m_socket, buf, buf.index(), NULL, &error);
 	if (ret == -1) {
 		MLPL_ERR("Failed to call g_socket_send: %s\n",
 		        error->message);
@@ -123,8 +237,8 @@ bool FaceMySQLWorker::send(SmartBuffer &buf)
 		return false;
 	}
 
-	if (ret != (gssize)buf.size()) {
-		MLPL_ERR("ret: %zd != buf.size(); %zd\n", ret, buf.size());
+	if (ret != (gssize)buf.index()) {
+		MLPL_ERR("ret: %zd != buf.index(); %zd\n", ret, buf.index());
 		return false;
 	}
 	return true;
--- src/FaceMySQLWorker.h
+++ src/FaceMySQLWorker.h
@@ -10,20 +10,24 @@ using namespace mlpl;
 #include "Utils.h"
 
 class FaceMySQLWorker {
+public:
+	FaceMySQLWorker(GSocket *sock, uint32_t connId);
+	virtual ~FaceMySQLWorker();
+	void start(void);
+
+protected:
+	gpointer mainThread(void);
+	void makeHandshakeV10(SmartBuffer &buf);
+	bool receiveHandshakeResponse41(void);
+	bool send(SmartBuffer &buf);
+	bool recive(char* buf, size_t size);
+
+private:
 	GThread *m_thread;
 	GSocket *m_socket;
 	uint32_t m_connId;
 
 	static gpointer _mainThread(gpointer data);
-protected:
-	gpointer mainThread(void);
-	void makeHandshakeV10(SmartBuffer &buf);
-	bool send(SmartBuffer &buf);
-
-public:
-	FaceMySQLWorker(GSocket *sock, uint32_t connId);
-	virtual ~FaceMySQLWorker();
-	void start(void);
 };
 
 #endif // FaceMySQLWorker_h
