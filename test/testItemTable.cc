44d5701cd6e957424db41662bd2e5553d15138e9
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-01-11 18:15:26 +0900

    added a part of test_corssJoin.

--- test/testItemTable.cc
+++ test/testItemTable.cc
@@ -22,7 +22,7 @@ enum {
 	ITEM_ID_AGE,
 	ITEM_ID_NAME,
 	ITEM_ID_FAVORITE_COLOR,
-	ITEM_ID_MONEY,
+	ITEM_ID_HEIGHT,
 	ITEM_ID_NICKNAME,
 };
 
@@ -34,27 +34,69 @@ struct TableStruct0 {
 
 struct TableStruct1 {
 	const char *name;
-	int haveMoney;
+	int height;
 	const char *nickname;
 };
 
-static const int NUM_TABLE0 = 2;
-static const int NUM_TABLE1 = 3;
+static tableContent0[] = {
+  {10, "anri", "red"},
+  {20, "mai", "blue"},
+};
+static const int NUM_TABLE0 = sizeof(tableContent0) / sizeof(TableStruct0);
 
+static tableContent0[] = {
+  {"anri", 150, "ann"},
+  {"nobita",200, "snake"},
+  {"maria", 170, "mai"},
+};
+static const int NUM_TABLE1 = sizeof(tableContent1) / sizeof(TableStruct1);
+
+static void addItemTable0(ItemGroup *grp, TableStruct0 *table)
+{
+	grp->add(new ItemInt(ITEM_ID_AGE, table->age), false);
+	grp->add(new ItemSring(ITEM_ID_NAME, table->.name), false);
+	grp->add(new ItemSring(ITEM_ID_FAVORITE_COLOR, table->favoriteColor), false);
+}
+
+static void addItemTable1(ItemGroup *grp, TableStruct1 *table)
+{
+	grp->add(new ItemString(ITEM_ID_NAME, table->age), false);
+	grp->add(new ItemInt(ITEM_ID_HEIGHT, table->height), false);
+	grp->add(new ItemSring(ITEM_ID_NICKNAME, table->nickname), false);
+}
+
+template<typename T>
+static ItemTable *
+addItmes(ItemTable *table, T* srcTable, int numTable, int groupId,
+         void (*addFunc)(ItemGroup *, T *))
+{
+	ItemTable *table = new ItemTable(groupId);
+	for (int i = 0; i < numTable; i++) {
+		ItemGroup *grp = new ItemGroup(table->getItemGroupId());
+		srcTable->add(grp, false);
+		addFunc(grp, srcTable);
+	}
+	return table;
+}
+
+// ---------------------------------------------------------------------------
+// Test cases
+// ---------------------------------------------------------------------------
 void test_crossJoin(void)
 {
-	ItemTable *table0 = new ItemTable(GROUP_ID_0);
-	ItemGroup *grp = new ItemGroup(GROUP_ID_0);
-	table0->add(grp, false);
-	grp->add(new ItemInt(ITEM_ID_AGE, 2), false);
+	ItemTable *table0 =
+	  addItems<TableStruct0>(table0, tableContent0,
+	                         NUM_TABLE0, GROUP_ID_0, addItemTable0);
 
-	ItemTable *table1 = new ItemTable(GROUP_ID_1);
-	ItemGroup *grp = new ItemGroup(GROUP_ID_1);
-	table1->add(grp, false);
-	grp->add(new ItemInt(ITEM_ID_NAME, ""), false);
+	ItemTable *table1 =
+	  addItems<TableStruct1>(table1, tableContent1,
+	                         NUM_TABLE1, GROUP_ID_1, addItemTable1);
+
+	ItemTable *tableA = table0->crossJoin(table1);
 
 	table0->unref();
 	table1->unref();
+	tableA->unref();
 }
 
 } // namespace testItemData
