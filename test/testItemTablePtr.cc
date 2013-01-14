4aed3313cfead3518aba1bdff55fa2e5e22e5720
Kazuhiro Yamato <kz0817@gmail.com>
2013-01-14 19:46:43 +0900

    added a test case file: testItemTablePtr.cc.

---
+++ test/testItemTablePtr.cc
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+#include <ParsableString.h>
+#include <StringUtils.h>
+using namespace mlpl;
+
+#include <cstdio>
+#include <cutter.h>
+#include <cppcutter.h>
+#include "ItemTablePtr.h"
+
+namespace testItemTablePtr {
+// ---------------------------------------------------------------------------
+// Test cases
+// ---------------------------------------------------------------------------
+void test_constructor(void)
+{
+	ItemTablePtr tablePtr;
+	cut_assert_not_null((ItemTable *)tablePtr);
+}
+
+} // namespace testItemTablePtr
+
