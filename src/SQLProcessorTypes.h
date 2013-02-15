d6722f2df2e88acba402b406d27508844e0d686f
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-02-15 11:58:05 +0900

    added SQLTableStaticInfo::tableGetFunc() that provides the ItemTablePtr.

--- src/SQLProcessorTypes.h
+++ src/SQLProcessorTypes.h
@@ -55,11 +55,15 @@ class SQLProcessor;
 typedef const ItemTablePtr
 (SQLProcessor::*SQLTableMakeFunc)(SQLSelectInfo &selectInfo,
                                   const SQLTableInfo &tableInfo);
+typedef ItemTablePtr (*SQLTableGetFunc)(void);
 
 struct SQLTableStaticInfo {
 	int                        tableId;
 	const char                *tableName;
+	// TODO: Consider that 'tableMakeFunc' can be removed
+	//       and use tableGetFunc instead.
 	SQLTableMakeFunc           tableMakeFunc;
+	SQLTableGetFunc            tableGetFunc;
 	const ColumnBaseDefList    columnBaseDefList;
 
 	// The value (ColumnBaseDefinition *) points an instance in
