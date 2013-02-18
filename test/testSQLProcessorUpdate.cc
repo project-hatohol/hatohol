2cea7c951e4d518cf0a7b67faf8a3e8d2c6a1f51
Kazuhiro Yamato <kz0817@gmail.com>
2013-02-18 22:23:02 +0900

    added a new test: testSQLProcessorUpdate.

---
+++ test/testSQLProcessorUpdate.cc
@@ -0,0 +1,93 @@
+#include <ParsableString.h>
+#include <StringUtils.h>
+using namespace mlpl;
+
+#include <cppcutter.h>
+#include "Asura.h"
+#include "SQLProcessorUpdate.h"
+#include "SQLProcessorException.h"
+
+namespace testSQLProcessorUpdate {
+
+static TableNameStaticInfoMap dummyMap;
+
+static const char *dummyTableName = "tableName";
+
+class TestSQLProcessorUpdate : public SQLProcessorUpdate {
+public:
+	TestSQLProcessorUpdate(void)
+	: SQLProcessorUpdate(dummyMap)
+	{
+	}
+
+	void callParseUpdateStatement(SQLUpdateInfo &updateInfo)
+	{
+		parseUpdateStatement(updateInfo);
+	}
+};
+
+static void _asssertExecUpdate(SQLUpdateInfo &updateInfo)
+{
+	try {
+		TestSQLProcessorUpdate proc;
+		proc.callParseUpdateStatement(updateInfo);
+	} catch (exception *e) {
+		cut_fail("Got exception: %s", e->what());
+	}
+}
+#define asssertExecUpdate(I) cut_trace(_asssertExecUpdate(I))
+
+#define DEFINE_UPDATEINFO_AND_ASSERT_SELECT(INS_VAR, STATEMENT) \
+ParsableString _parsable(STATEMENT); \
+SQLUpdateInfo INS_VAR(_parsable); \
+asssertExecUpdate(INS_VAR)
+
+static void _assertStringVector(StringVector &expected, StringVector &actual)
+{
+	cppcut_assert_equal(expected.size(), actual.size());
+	for (size_t i = 0; i < expected.size(); i++)
+		cppcut_assert_equal(expected[i], actual[i]);
+}
+#define assertStringVector(E,A) cut_trace(_assertStringVector(E,A))
+
+void setupDummyMap(void)
+{
+	if (!dummyMap.empty())
+		return;
+	dummyMap[dummyTableName] = NULL;
+}
+
+void setup(void)
+{
+	asuraInit();
+	setupDummyMap();
+}
+
+// ---------------------------------------------------------------------------
+// Test cases
+// ---------------------------------------------------------------------------
+void test_parseOneColumn(void)
+{
+	const char *tableName  = "tableName";
+	const char *columnName = "columnName";
+	const char *valueStr   = "10";
+	const char *whereColumn = "a";
+	const char *whereValue  = "5";
+	string statement =
+	  StringUtils::sprintf("update %s SET %s=%s where %s=%s",
+	                       tableName, columnName, valueStr,
+	                       whereColumn, whereValue);
+	DEFINE_UPDATEINFO_AND_ASSERT_SELECT(updateInfo, statement);
+
+	StringVector expectedColumns;
+	expectedColumns.push_back(columnName);
+	StringVector expectedValues;
+	expectedValues.push_back(valueStr);
+
+	cppcut_assert_equal(string(tableName), updateInfo.table);
+	assertStringVector(expectedColumns, updateInfo.columnVector);
+	assertStringVector(expectedValues, updateInfo.valueVector);
+	// TODO: add assertions for where section
+}
+
+} // namespace testSQLProcessorUpdate
