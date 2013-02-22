ec59218fabf2b60fdd55af4d4d4539e1f5e699be
Kazuhiro Yamato <kz0817@gmail.com>
2013-02-23 08:57:24 +0900

    moved assertItemData() to Helpers.h to be used from other test source files.

--- test/Helpers.h
+++ test/Helpers.h
@@ -4,10 +4,38 @@
 #include <StringUtils.h>
 using namespace mlpl;
 
+#include "ItemTable.h"
+
 typedef pair<int,int>      IntIntPair;
 typedef vector<IntIntPair> IntIntPairVector;
 
 void _assertStringVector(StringVector &expected, StringVector &actual);
 #define assertStringVector(E,A) cut_trace(_assertStringVector(E,A))
+
+template<typename T>
+static ItemTable * addItems(T* srcTable, int numTable,
+                            void (*addFunc)(ItemGroup *, T *))
+{
+	ItemTable *table = new ItemTable();
+	for (int i = 0; i < numTable; i++) {
+		ItemGroup *grp = table->addNewGroup();
+		(*addFunc)(grp, &srcTable[i]);
+	}
+	return table;
+}
+
+template<typename T>
+static void _assertItemData(const ItemGroup *itemGroup, T expected, int &idx)
+{
+	T val;
+	ItemData *itemZ = itemGroup->getItemAt(idx);
+	cut_assert_not_null(itemZ);
+	idx++;
+	itemZ->get(&val);
+	cut_trace(cppcut_assert_equal(expected, val));
+}
+#define assertItemData(T, IGRP, E, IDX) \
+cut_trace(_assertItemData<T>(IGRP, E, IDX))
+
 
 #endif // Helpers_h
--- test/testItemTable.cc
+++ test/testItemTable.cc
@@ -69,31 +69,6 @@ static void addItemTable1(ItemGroup *grp, TableStruct1 *table)
 	grp->add(new ItemString(ITEM_ID_NICKNAME, table->nickname), false);
 }
 
-template<typename T>
-static ItemTable * addItems(T* srcTable, int numTable,
-                            void (*addFunc)(ItemGroup *, T *))
-{
-	ItemTable *table = new ItemTable();
-	for (int i = 0; i < numTable; i++) {
-		ItemGroup *grp = table->addNewGroup();
-		(*addFunc)(grp, &srcTable[i]);
-	}
-	return table;
-}
-
-template<typename T>
-static void _assertItemData(const ItemGroup *itemGroup, T expected, int &idx)
-{
-	T val;
-	ItemData *itemZ = itemGroup->getItemAt(idx);
-	cut_assert_not_null(itemZ);
-	idx++;
-	itemZ->get(&val);
-	cut_trace(cppcut_assert_equal(expected, val));
-}
-#define assertItemData(T, IGRP, E, IDX) \
-cut_trace(_assertItemData<T>(IGRP, E, IDX))
-
 static void assertJoinRunner(const ItemGroup *itemGroup,
                              TableStruct0 *refData0, TableStruct1 *refData1,
                              size_t data0Index, size_t data1Index)
