29fbe53c7bb21e0b5b9f42fa74c5477017670140
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-03-25 16:53:21 +0900

    added new files: PrimaryCondition.h and PrimaryCondition.cc

---
+++ src/PrimaryCondition.cc
@@ -0,0 +1,88 @@
+/* Asura
+   Copyright (C) 2013 MIRACLE LINUX CORPORATION
+ 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "PrimaryCondition.h"
+
+// ---------------------------------------------------------------------------
+// PrimaryCondition
+// ---------------------------------------------------------------------------
+PrimaryCondition::~PrimaryCondition()
+{
+}
+
+// ---------------------------------------------------------------------------
+// PrimaryConditionColumnsEqual
+// ---------------------------------------------------------------------------
+PrimaryConditionColumnsEqual::PrimaryConditionColumnsEqual
+  (const string &leftTableName, const string &leftColumnName,
+   const string &rightTableName, const string &rightColumnName)
+: m_leftTableName(leftTableName),
+  m_leftColumnName(leftColumnName),
+  m_rightTableName(rightTableName),
+  m_rightColumnName(rightColumnName)
+{
+}
+
+const string &PrimaryConditionColumnsEqual::getLeftTableName(void) const
+{
+	return m_leftTableName;
+}
+
+const string &PrimaryConditionColumnsEqual::getLeftColumnName(void) const
+{
+	return m_leftColumnName;
+}
+
+const string &PrimaryConditionColumnsEqual::getRightTableName(void) const
+{
+	return m_rightTableName;
+}
+
+const string &PrimaryConditionColumnsEqual::getRightColumnName(void) const
+{
+	return m_rightColumnName;
+}
+
+// ---------------------------------------------------------------------------
+// PrimaryConditionConstants
+// ---------------------------------------------------------------------------
+PrimaryConditionConstants::PrimaryConditionConstants
+  (const string &tableName, const string &columnName)
+: m_tableName(tableName),
+  m_columnName(columnName)
+{
+}
+
+const string &PrimaryConditionConstants::getTableName(void) const
+{
+	return m_tableName;
+}
+
+const string &PrimaryConditionConstants::getColumnName(void) const
+{
+	return m_columnName;
+}
+
+void PrimaryConditionConstants::add(ItemDataPtr itemData)
+{
+	m_itemGroup->add(itemData);
+}
+
+const ItemGroupPtr &PrimaryConditionConstants::getConstants(void) const
+{
+	return m_itemGroup;
+}
---
+++ src/PrimaryCondition.h
@@ -0,0 +1,81 @@
+/* Asura
+   Copyright (C) 2013 MIRACLE LINUX CORPORATION
+ 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef PrimaryCondition_h
+#define PrimaryCondition_h
+
+#include <list>
+#include <string>
+using namespace std;
+
+#include "ItemDataPtr.h"
+#include "ItemGroupPtr.h"
+
+// ---------------------------------------------------------------------------
+// PrimaryCondition
+// ---------------------------------------------------------------------------
+class PrimaryCondition {
+public:
+	virtual ~PrimaryCondition();
+};
+
+typedef list<PrimaryCondition *>             PrimaryConditionList;
+typedef PrimaryConditionList::iterator       PrimaryConditionListIterator;
+typedef PrimaryConditionList::const_iterator PrimaryConditionListConstIterator;
+
+// ---------------------------------------------------------------------------
+// PrimaryConditionColumnsEqual
+// ---------------------------------------------------------------------------
+class PrimaryConditionColumnsEqual : public PrimaryCondition {
+public:
+	PrimaryConditionColumnsEqual(const string &leftTableName,
+	                             const string &leftColumnName,
+	                             const string &rightTableName,
+	                             const string &rightColumnName);
+	const string &getLeftTableName(void) const;
+	const string &getLeftColumnName(void) const;
+	const string &getRightTableName(void) const;
+	const string &getRightColumnName(void) const;
+
+private:
+	string m_leftTableName;
+	string m_leftColumnName;
+	string m_rightTableName;
+	string m_rightColumnName;
+};
+
+// ---------------------------------------------------------------------------
+// PrimaryConditionConstants
+// ---------------------------------------------------------------------------
+class PrimaryConditionConstants : public PrimaryCondition {
+public:
+	PrimaryConditionConstants(const string &leftTableName,
+	                          const string &leftColumnName);
+	const string &getTableName(void) const;
+	const string &getColumnName(void) const;
+	void add(ItemDataPtr itemData);
+	const ItemGroupPtr &getConstants(void) const;
+
+private:
+	string       m_tableName;
+	string       m_columnName;
+	ItemGroupPtr m_itemGroup;
+};
+
+#endif // PrimaryCondition_h
+
+
