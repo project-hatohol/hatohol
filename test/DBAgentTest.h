7da9047400415d9ab2fe0801b30de4ee7e59b50d
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-04-01 15:30:11 +0900

    moved the body of DB test data into DBAgentTest.cc to avoid compiler's warning.
    The previous way (static definition in a header file) causes
    compiler warnings when a variable defined as a static is not used.
    We solved this problem by the define of data in libHelper and
    declaration of them as extern in the header file.

--- test/DBAgentTest.h
+++ test/DBAgentTest.h
@@ -3,59 +3,11 @@
 
 #include "DBAgent.h"
 
-static MonitoringServerInfo serverInfo[] = 
-{{
-	1,                        // id
-	MONITORING_SYSTEM_ZABBIX, // type
-	"pochi.dog.com",          // hostname
-	"192.168.0.5",            // ip_address
-	"POCHI",                  // nickname
-},{
-	2,                        // id
-	MONITORING_SYSTEM_ZABBIX, // type
-	"mike.dog.com",           // hostname
-	"192.168.1.5",            // ip_address
-	"MIKE",                   // nickname
-},{
-	3,                        // id
-	MONITORING_SYSTEM_ZABBIX, // type
-	"hachi.dog.com",          // hostname
-	"192.168.10.1",           // ip_address
-	"8",                      // nickname
-}};
-static size_t NumServerInfo = sizeof(serverInfo) / sizeof(MonitoringServerInfo);
+extern MonitoringServerInfo serverInfo[];
+extern size_t NumServerInfo;
 
-static TriggerInfo testTriggerInfo[] = 
-{{
-	1,                        // id
-	TRIGGER_STATUS_OK,        // status
-	TRIGGER_SEVERITY_INFO,    // severity
-	{1362957197,0},           // lastChangeTime
-	1,                        // serverId
-	"235012",                 // hostId,
-	"hostX1",                 // hostName,
-	"TEST Trigger 1",         // brief,
-},{
-	2,                        // id
-	TRIGGER_STATUS_PROBLEM,   // status
-	TRIGGER_SEVERITY_WARN,    // severity
-	{1362957200,0},           // lastChangeTime
-	3,                        // serverId
-	"10001",                  // hostId,
-	"hostZ1",                 // hostName,
-	"TEST Trigger 2",         // brief,
-},{
-	3,                        // id
-	TRIGGER_STATUS_PROBLEM,   // status
-	TRIGGER_SEVERITY_INFO,    // severity
-	{1362951000,0},           // lastChangeTime
-	3,                        // serverId
-	"10002",                  // hostId,
-	"hostZ2",                 // hostName,
-	"TEST Trigger 3",         // brief,
-}};
-static size_t NumTestTriggerInfo =
-   sizeof(testTriggerInfo) / sizeof(TriggerInfo);
+extern TriggerInfo testTriggerInfo[];
+extern size_t NumTestTriggerInfo;
 
 #endif // DBAgentTest_h
 
