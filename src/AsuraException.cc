bf61867a92481e83fa4253e0c110cdbd1e8945b1
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-04-03 13:58:46 +0900

    changed the message of what() to that of getFancyMessage() to show more information about the exception.

--- src/AsuraException.cc
+++ src/AsuraException.cc
@@ -49,7 +49,7 @@ AsuraException::~AsuraException() _GLIBCXX_USE_NOEXCEPT
 
 const char* AsuraException::what() const _GLIBCXX_USE_NOEXCEPT
 {
-	return m_what.c_str();
+	return getFancyMessage().c_str();
 }
 
 const string AsuraException::getFancyMessage(void) const
