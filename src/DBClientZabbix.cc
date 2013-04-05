ffea614a4c660f7c172cd387ceaafaf637bb5396
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-04-05 09:16:34 +0900

    changed a variable name whose type is static const to capital letters according to the project policy.
    This project uses capital letters with underscores for static const
    variables.

--- src/DBClientZabbix.cc
+++ src/DBClientZabbix.cc
@@ -20,12 +20,12 @@
 #include "ItemEnum.h"
 #include "ConfigManager.h"
 
-static const char *tableNameTriggersRaw2_0 = "triggers_raw_2_0";
+static const char *TABLE_NAME_TRIGGERS_RAW_2_0 = "triggers_raw_2_0";
 
 static ColumnDef triggersRaw2_0[] = {
 {
 	ITEM_ID_NOT_SET,                   // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"replica_generation_id",           // columnName
 	SQL_COLUMN_TYPE_INT,               // type
 	11,                                // columnLength
@@ -36,7 +36,7 @@ static ColumnDef triggersRaw2_0[] = {
 	NULL,                              // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_TRIGGERID,    // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"triggerid",                       // columnName
 	SQL_COLUMN_TYPE_BIGUINT,           // type
 	20,                                // columnLength
@@ -47,7 +47,7 @@ static ColumnDef triggersRaw2_0[] = {
 	NULL,                              // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_EXPRESSION,   // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"expression",                      // columnName
 	SQL_COLUMN_TYPE_VARCHAR,           // type
 	255,                               // columnLength
@@ -58,7 +58,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"",                                // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_DESCRIPTION,  // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"description",                     // columnName
 	SQL_COLUMN_TYPE_VARCHAR,           // type
 	255,                               // columnLength
@@ -69,7 +69,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"",                                // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_URL,          // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"url",                             // columnName
 	SQL_COLUMN_TYPE_VARCHAR,           // type
 	255,                               // columnLength
@@ -80,7 +80,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"",                                // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_STATUS,       // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"status",                          // columnName
 	SQL_COLUMN_TYPE_INT,               // type
 	11,                                // columnLength
@@ -91,7 +91,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"0",                               // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_VALUE,        // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"value",                           // columnName
 	SQL_COLUMN_TYPE_INT,               // type
 	11,                                // columnLength
@@ -102,7 +102,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"0",                               // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_PRIORITY,     // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"priority",                        // columnName
 	SQL_COLUMN_TYPE_INT,               // type
 	11,                                // columnLength
@@ -113,7 +113,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"0",                               // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_LASTCHANGE,   // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"lastchange",                      // columnName
 	SQL_COLUMN_TYPE_INT,               // type
 	11,                                // columnLength
@@ -124,7 +124,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"0",                               // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_COMMENTS,     // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"comments",                        // columnName
 	SQL_COLUMN_TYPE_INT,               // type
 	11,                                // columnLength
@@ -135,7 +135,7 @@ static ColumnDef triggersRaw2_0[] = {
 	NULL,                              // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_ERROR,        // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"error",                           // columnName
 	SQL_COLUMN_TYPE_VARCHAR,           // type
 	128,                               // columnLength
@@ -146,7 +146,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"",                                // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_TEMPLATEID,   // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"templateid",                      // columnName
 	SQL_COLUMN_TYPE_BIGUINT,           // type
 	20,                                // columnLength
@@ -157,7 +157,7 @@ static ColumnDef triggersRaw2_0[] = {
 	NULL,                              // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_TYPE,         // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"type",                            // columnName
 	SQL_COLUMN_TYPE_INT,               // type
 	11,                                // columnLength
@@ -168,7 +168,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"0",                               // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_VALUE_FLAGS,  // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"value_flags",                     // columnName
 	SQL_COLUMN_TYPE_INT,               // type
 	11,                                // columnLength
@@ -179,7 +179,7 @@ static ColumnDef triggersRaw2_0[] = {
 	"0",                               // defaultValue
 }, {
 	ITEM_ID_ZBX_TRIGGERS_FLAGS,        // itemId
-	tableNameTriggersRaw2_0,           // tableName
+	TABLE_NAME_TRIGGERS_RAW_2_0,           // tableName
 	"flags",                           // columnName
 	SQL_COLUMN_TYPE_INT,               // type
 	11,                                // columnLength
@@ -260,7 +260,7 @@ DBClientZabbix::~DBClientZabbix()
 void DBClientZabbix::dbSetupFunc(DBDomainId domainId)
 {
 	const string dbPath = DBAgentSQLite3::findDBPath(domainId);
-	if (DBAgentSQLite3::isTableExisting(dbPath, tableNameTriggersRaw2_0))
+	if (DBAgentSQLite3::isTableExisting(dbPath, TABLE_NAME_TRIGGERS_RAW_2_0))
 		return;
 	createTableTriggersRaw2_0(dbPath);
 }
@@ -268,7 +268,7 @@ void DBClientZabbix::dbSetupFunc(DBDomainId domainId)
 void DBClientZabbix::createTableTriggersRaw2_0(const string &dbPath)
 {
 	TableCreationArg arg;
-	arg.tableName  = tableNameTriggersRaw2_0;
+	arg.tableName  = TABLE_NAME_TRIGGERS_RAW_2_0;
 	arg.numColumns = NumTriggersRaw2_0;
 	arg.columnDefs = triggersRaw2_0;
 	DBAgentSQLite3::createTable(dbPath, arg);
