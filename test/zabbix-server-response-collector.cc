7db14bc51048d887132715eff25f1a9113b88582
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-04-11 10:43:38 +0900

    added (mostly stub) class: ZabbixAPIResponseCollector, which is the body of zabbix-server-response-collect server.

--- test/zabbix-server-response-collector.cc
+++ test/zabbix-server-response-collector.cc
@@ -1,7 +1,45 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include "ArmZabbixAPI.h"
+
+static const int DEFAULT_PORT = 80;
+static const int ZBX_SVR_ID = 0;
+
+class ZabbixAPIResponseCollector : public ArmZabbixAPI
+{
+public:
+	ZabbixAPIResponseCollector(const string &server,
+	                           int port = DEFAULT_PORT);
+	virtual ~ZabbixAPIResponseCollector();
+};
+
+ZabbixAPIResponseCollector::ZabbixAPIResponseCollector
+  (const string &server, int port)
+: ArmZabbixAPI(ZBX_SVR_ID, server.c_str(), port)
+{
+}
+
+ZabbixAPIResponseCollector::~ZabbixAPIResponseCollector()
+{
+}
+
+static void printUsage(void)
+{
+	printf("Usage:\n");
+	printf("\n");
+	printf("$ zabbix-server-response-collect server\n");
+	printf("\n");
+}
+
 int main(int argc, char *argv[])
 {
+	if (argc < 2) {
+		printUsage();
+		return EXIT_FAILURE;
+	}
+	string server = argv[1];
+
+	ZabbixAPIResponseCollector collector(server);
 	return EXIT_SUCCESS;
 }
