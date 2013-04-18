68ba900ec617c18983dc567b622d7bd7adbe44bd
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-04-18 12:14:06 +0900

    added a new class: DBClientAsura.
    DBClientAsura handles the monitoring software independent database.

---
+++ src/DBClientAsura.cc
@@ -0,0 +1,44 @@
+/* Asura
+   Copyright (C) 2013 MIRACLE LINUX CORPORATION
+ 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <memory>
+
+#include "DBAgentFactory.h"
+#include "DBClientAsura.h"
+
+struct DBClientAsura::PrivateContext
+{
+};
+
+// ---------------------------------------------------------------------------
+// Public methods
+// ---------------------------------------------------------------------------
+DBClientAsura::DBClientAsura(void)
+: m_ctx(NULL)
+{
+	m_ctx = new PrivateContext();
+}
+
+DBClientAsura::~DBClientAsura()
+{
+	if (m_ctx)
+		delete m_ctx;
+}
+
+// ---------------------------------------------------------------------------
+// Protected methods
+// ---------------------------------------------------------------------------
---
+++ src/DBClientAsura.h
@@ -0,0 +1,35 @@
+/* Asura
+   Copyright (C) 2013 MIRACLE LINUX CORPORATION
+ 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DBClientAsura_h
+#define DBClientAsura_h
+
+#include "DBAgent.h"
+
+class DBClientAsura {
+public:
+	DBClientAsura(void);
+	virtual ~DBClientAsura();
+
+protected:
+
+private:
+	struct PrivateContext;
+	PrivateContext *m_ctx;
+};
+
+#endif // DBClientAsura_h
