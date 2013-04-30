db25f30e6dc31fc9f1d91e8267b59205f7ac79e3
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-04-30 09:59:45 +0900

    moved a local header: Asura.h below the system header.

--- src/Asura.cc
+++ src/Asura.cc
@@ -15,9 +15,9 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "Asura.h"
 #include <glib.h>
 
+#include "Asura.h"
 #include "SQLUtils.h"
 #include "SQLProcessorZabbix.h"
 #include "SQLProcessorFactory.h"
