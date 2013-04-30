2cc8075701668a478a1a3a6b6f141b1f9b5754f7
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-04-30 14:54:55 +0900

    fixed the problem that sets digits: 11 inspite SQL_COLUMN_TYPE_BIGUINT.

--- src/DBClientAsura.cc
+++ src/DBClientAsura.cc
@@ -33,7 +33,7 @@ static const ColumnDef COLUMN_DEF_TRIGGERS[] = {
 	TABLE_NAME_TRIGGERS,               // tableName
 	"id",                              // columnName
 	SQL_COLUMN_TYPE_BIGUINT,           // type
-	11,                                // columnLength
+	20,                                // columnLength
 	0,                                 // decFracLength
 	false,                             // canBeNull
 	SQL_KEY_PRI,                       // keyType
