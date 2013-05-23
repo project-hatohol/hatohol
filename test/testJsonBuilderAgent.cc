eb56e808eeafa8375f2411c73bbed315a8b72245
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-05-23 10:37:04 +0900

    added a new test: testJsonBuilderAgent::test_array().

--- test/testJsonBuilderAgent.cc
+++ test/testJsonBuilderAgent.cc
@@ -1,5 +1,8 @@
 #include <cppcutter.h>
+#include <StringUtils.h>
 #include "JsonBuilderAgent.h"
+
+using namespace mlpl;
 
 namespace testJsonBuilderAgent {
 
@@ -12,6 +15,30 @@ void test_emptyObject(void)
 	agent.startObject();
 	agent.endObject();
 	string expected = "{}";
+	cppcut_assert_equal(expected, agent.generate());
+}
+
+void test_array(void)
+{
+	string arrayName = "foo";
+	const int numArray = 5;
+	JsonBuilderAgent agent;
+	agent.startObject();
+	agent.startArray(arrayName);
+	for (int i = 0; i < numArray; i++)
+		agent.add(i);
+	agent.endArray();
+	agent.endObject();
+
+	// check
+	string expected = StringUtils::sprintf("{\"%s\":[", arrayName.c_str());
+	for (int i = 0; i < numArray; i++) {
+		if (i < numArray - 1)
+			expected += StringUtils::sprintf("%d,", i);
+		else
+			expected += StringUtils::sprintf("%d]", i);
+	}
+	expected += "}";
 	cppcut_assert_equal(expected, agent.generate());
 }
 
