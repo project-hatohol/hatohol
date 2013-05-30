99ebc39ca68535175cdb2a92c2ba7d50abbe3d0e
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-05-30 11:29:41 +0900

    added a new class DataStoreNagios.

---
+++ src/DataStoreNagios.cc
@@ -0,0 +1,49 @@
+/* Asura
+   Copyright (C) 2013 MIRACLE LINUX CORPORATION
+ 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "DataStoreNagios.h"
+
+#include <cstdio>
+
+struct DataStoreNagios::PrivateContext
+{
+	ArmNagiosNDOUtils  armNDO;
+
+	PrivateContext(const MonitoringServerInfo &serverInfo)
+	: armNDO(serverInfo)
+	{
+	}
+};
+
+// ---------------------------------------------------------------------------
+// Public methods
+// ---------------------------------------------------------------------------
+DataStoreNagios::DataStoreNagios(const MonitoringServerInfo &serverInfo)
+: m_ctx(NULL)
+{
+	m_ctx = new PrivateContext(serverInfo);
+	m_ctx->armNDO.start();
+}
+
+DataStoreNagios::~DataStoreNagios()
+{
+	if (m_ctx)
+		delete m_ctx;
+}
+
+// ---------------------------------------------------------------------------
+// Protected methods
+// ---------------------------------------------------------------------------
---
+++ src/DataStoreNagios.h
@@ -0,0 +1,33 @@
+/* Asura
+   Copyright (C) 2013 MIRACLE LINUX CORPORATION
+ 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DataStoreNagios_h
+#define DataStoreNagios_h
+
+#include "ArmNagiosNDOUtils.h"
+#include "DataStore.h"
+
+class DataStoreNagios : public DataStore {
+public:
+	DataStoreNagios(const MonitoringServerInfo &serverInfo);
+	virtual ~DataStoreNagios();
+private:
+	struct PrivateContext;
+	PrivateContext *m_ctx;
+};
+
+#endif // DataStoreNagios_h
