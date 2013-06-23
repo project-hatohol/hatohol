2bf925485e24042543128c26dab0e46aac9ddc5a
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-06-24 08:32:48 +0900

    added a feature to parse an abitrary specified file.

--- misc/makrdown-checker/markdown-checker.php
+++ misc/makrdown-checker/markdown-checker.php
@@ -1,6 +1,9 @@
 #!/usr/bin/php
 <?php
-$md = file_get_contents("../../README.md");
+$target_file = "../../README.md";
+if ($argc >= 2)
+    $target_file = $argv[1];
+$md = file_get_contents($target_file);
 $sd = new Sundown($md);
 echo $sd->to_html();
 ?>
