85ffbd6cf1e3295b9be652b456666b6f64043cec
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-08-19 16:37:58 +0900

    Add TODO as a comment.

--- server/test/testActionManager.cc
+++ server/test/testActionManager.cc
@@ -269,6 +269,12 @@ void test_execCommandAction(void)
 	}
 }
 
+// TODO: make tests for the following error cases.
+// - The path of the actor is wrong (failed to launch)
+// - An actor is killed by the timed out.
+// - The pipe of actor connected to hatohol unexpectedly is closed.
+// - Unexpected exit of the actor.
+
 } // namespace testActionManager
 
 namespace testActionManagerWithoutDB {
