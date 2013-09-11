7541a821128cb8f6753422245bf28cfde6b2074c
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-09-11 15:58:07 +0900

    Add a new class: Reaper.
    This class provides a function automatilcally to destroy or free
    an object when the instance is freed. The basic idea is similar
    to auto pointers such as auto_ptr.

---
+++ server/mlpl/src/Reaper.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2013 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef Reaper_h
+#define Reaper_h
+
+namespace mlpl {
+
+typedef void (*ReaperDestroyFunc)(void *obj);
+
+template<typename T>
+class Reaper {
+public:
+	Reaper(T *obj, ReaperDestroyFunc destroyFunc)
+	: m_obj(obj),
+	  m_destroyFunc(destroyFunc)
+	{
+	}
+
+	virtual ~Reaper()
+	{
+		(*m_destroyFunc)(m_obj);
+	}
+
+private:
+	T *m_obj;
+	ReaperDestroyFunc m_destroyFunc;
+};
+
+} // namespace mlpl
+
+#endif // Reaper_h
+
