43a6ed446f3c724364dd7a65da20ca1c4ad2ac96
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-09-11 21:24:26 +0900

    Show literal labels for trigger status, severity, and type.

---
+++ client/static/js/hatohol_def.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2013 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// TODO: We will make this file automatically
+const TRIGGER_STATUS_OK       = 0
+const TRIGGER_STATUS_PROBLEM  = 1
+
+const TRIGGER_SEVERITY_INFO     = 0
+const TRIGGER_SEVERITY_WARN     = 1
+const TRIGGER_SEVERITY_CRITICAL = 2
+const TRIGGER_SEVERITY_UNKNOWN  = 3
+
+const ACTION_COMMAND  = 0
+const ACTION_RESIDENT = 1
+
+const CMP_INVALID = 0
+const CMP_EQ      = 1
+const CMP_EQ_GT   = 2
