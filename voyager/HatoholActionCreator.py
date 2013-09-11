8a58ecd34cdd0723c00bc233c7872f06fc07510e
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-09-11 09:45:03 +0900

    Implement a function to add an action.

--- voyager/HatoholActionCreator.py
+++ voyager/HatoholActionCreator.py
@@ -19,30 +19,69 @@
 """
 
 import sys
+import urllib
+import urllib2
+import HatoholDefine
 
 class HatoholActionCreator:
   def __init__(self, url):
-    self.url = url
+    self._url = url
 
   def add(self, options):
-    type = self.get_one_arg(options, "--type")
-    if type != "command" and type != "resident":
-      print "--type's value must be 'command' or 'resident'."
+    type = self._get_one_arg(options, "--type")
+    type_code = None
+    if type == "command":
+      type_code = HatoholDefine.ACTION_COMMAND
+    elif type == "resident":
+      type_code = HatoholDefine.ACTION_RESIDENT
+    else:
+      print "Type must be 'command' or 'resident'."
       sys.exit(-1)
 
-    command = self.get_one_arg(options, "--command")
-    working_dir = self.get_one_arg(options, "--workind-dir", True);
-    timeout = self.get_one_arg(options, "--timeout", True);
-    server_id = self.get_one_arg(options, "--server-id", True);
-    host_id = self.get_one_arg(options, "--host-id", True);
-    host_group_id = self.get_one_arg(options, "--host-group-id", True);
-    trigger_id = self.get_one_arg(options, "--trigger-id", True);
-    status = self.get_one_arg(options, "--status", True);
-    severity = self.get_one_arg(options, "--severity", True);
-    severity_cmp = 1
+    command = self._get_one_arg(options, "--command")
+    working_dir = self._get_one_arg(options, "--working-dir", True)
+    timeout = self._get_one_arg(options, "--timeout", True)
+    server_id = self._get_one_arg(options, "--server-id", True)
+    host_id = self._get_one_arg(options, "--host-id", True)
+    host_group_id = self._get_one_arg(options, "--host-group-id", True)
+    trigger_id = self._get_one_arg(options, "--trigger-id", True)
+
+    status = self._get_one_arg(options, "--status", True)
+    status_code = None
+    if status == "ok":
+      status_code = HatoholDefine.TRIGGER_STATUS_OK
+    elif status == "problem":
+      status_code = HatoholDefine.TRIGGER_STATUS_PROBLEM
+    else:
+      print "Status must be 'ok' or 'problem'."
+      sys.exit(-1)
+
+    (severity_cmp, severity) = self._get_arg(options, "--severity", True, 2)
+    severity_cmp_code = None
+    severity_code = None
+    if severity:
+      if severity == "info":
+        severity_code = HatoholDefine.TRIGGER_SEVERITY_INFO
+      elif severity == "warn":
+        severity_code = HatoholDefine.TRIGGER_SEVERITY_WARN
+      elif severity == "critical":
+        severity_code = HatoholDefine.TRIGGER_SEVERITY_CRITICAL
+      else:
+        print "Severity must be 'info', 'warn', or 'critical'."
+        sys.exit(-1)
+
+      if severity_cmp == "-eq":
+        severity_cmp_code = HatoholDefine.CMP_EQ
+      elif severity_cmp == "-ge":
+        severity_cmp_code = HatoholDefine.CMP_EQ_GT
+      else:
+        print "Severity comparator must be '-eq' or '-ge'."
+        sys.exit(-1)
 
     print "Type       : " + type
     print "Commad     : " + command
+    if working_dir:
+      print "Working dir: " + working_dir
     if timeout:
       print "Timeout    : " + timeout
     if server_id:
@@ -52,23 +91,59 @@ class HatoholActionCreator:
     if host_group_id:
       print "Host Grp ID: " + host_group_id
     if trigger_id:
-      print "Trigger ID : " + tigger_id
+      print "Trigger ID : " + trigger_id
     if status:
       print "Trig. Stat.: " + status
     if severity:
-      print "Seveirty   : " + severity
+      print "Seveirty   : " + severity_cmp + " " + severity
 
-  def get_one_arg(self, options, key, allow_none = False):
+    result = self._send(type_code, command, working_dir, timeout, server_id, host_id, host_group_id, trigger_id, status_code, severity_cmp, severity)
+
+  def _send(self, type_code, command, working_dir, timeout, server_id, host_id, host_group_id, trigger_id, status_code, severity_cmp_code, severity_code):
+    query = {}
+    query["type"] = str(type_code)
+    query["command"] = command
+    if working_dir:
+      query["&workingDirectory"] = working_dir
+    if timeout:
+      query["timeout"] = str(timeout)
+    if server_id:
+      query["serverId"] = str(server_id)
+    if host_id:
+      query["&hostId="] = str(host_id)
+    if host_group_id:
+      query["&hostGroupId="] = str(host_group_id)
+    if trigger_id:
+      query["&triggerId="] = str(trigger_id)
+    if status_code:
+      query["&triggerStatus="] = str(status_code)
+    if severity_code:
+      query["&triggerSeverity="] = str(severity_code)
+      query["&triggerSeverityCompType="] = str(severity_cmp_code)
+
+    encoded_query = urllib.urlencode(query)
+    url = self._url + "/actions.json?" + encoded_query
+    print "URL: " + url
+    response = urllib2.urlopen(url + "/actions.json")
+    print response
+
+  def _get_arg(self, options, key, allow_none = False, num_arg=1):
     if key not in options:
       if allow_none:
-        return None
+        args = []
+        for i in range(num_arg):
+          args.append(None)
+        return args
       print key + " is not defined."
       sys.exit(-1)
     idx = options.index(key)
-    if idx == len(options) - 1:
+    if idx >= len(options) - num_arg:
       print key + ": must have the argument."
       sys.exit(-1)
-    arg = options[idx+1]
-    del options[idx:idx+2]
-    return arg
+    args = options[idx+1:idx+num_arg+1]
+    del options[idx:idx+num_arg+1]
+    return args
+
+  def _get_one_arg(self, options, key, allow_none = False):
+    return self._get_arg(options, key, allow_none)[0]
 
