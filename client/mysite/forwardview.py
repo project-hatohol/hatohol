5a914562281b7d0e03d1795c506c587192e4cdb1
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-09-13 10:24:46 +0900

    Use the other type of paramters for the post.

--- client/mysite/forwardview.py
+++ client/mysite/forwardview.py
@@ -15,13 +15,18 @@
 # You should have received a copy of the GNU General Public License
 # along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
 
+import urllib
 import urllib2
 from django.http import HttpResponse
 
 def jsonforward(request, path, **kwargs):
     server  = kwargs['server']
     url     = 'http://%s/%s' % (server, path)
-    content = urllib2.urlopen(url)
+    #content = None
+    if request.method == "POST":
+      content = urllib2.urlopen(url, urllib.urlencode(request.REQUEST))
+    else:
+      content = urllib2.urlopen(url)
 
     return HttpResponse(content,
                         content_type='application/json')
