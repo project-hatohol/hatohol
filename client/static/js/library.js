04f5f4dd5d77a1429633910ec27758aca3373a10
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-09-19 13:03:11 +0900

    Move generic functions to library.js.

--- client/static/js/library.js
+++ client/static/js/library.js
@@ -150,6 +150,36 @@ function schedule(timer, table, param) {
   }, timer);
 }
 
+function makeTriggerStatusLabel(status) {
+  switch(status) {
+  case TRIGGER_STATUS_OK:
+    return gettext("OK");
+  case TRIGGER_STATUS_PROBLEM:
+    return gettext("Problem");
+  default:
+    return gettext("INVALID: ") + status;
+  }
+}
+
+function makeServerityLabel(severity) {
+  switch(severity) {
+  case TRIGGER_SEVERITY_UNKNOWN:
+    return gettext("Not classified");
+  case TRIGGER_SEVERITY_INFO:
+    return gettext("Information");
+  case TRIGGER_SEVERITY_WARNING:
+    return gettext("Warning");
+  case TRIGGER_SEVERITY_ERROR:
+    return gettext("Average");
+  case TRIGGER_SEVERITY_CRITICAL:
+    return gettext("High");
+  case TRIGGER_SEVERITY_EMERGENCY:
+    return gettext("Disaster");
+  default:
+    return gettext("INVALID") + ": " + severity;
+  }
+}
+
 function makeMonitoringSystemTypeLabel(type) {
   switch (type) {
   case MONITORING_SYSTEM_ZABBIX:
