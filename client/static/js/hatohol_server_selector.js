2112d2b9bb1ebca6cefdface867b859a6a2df1f7
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-09-19 09:41:09 +0900

    Fix indent.
    The depth of the first level was four spaces. But other sources
    uses two spaces.

--- client/static/js/hatohol_server_selector.js
+++ client/static/js/hatohol_server_selector.js
@@ -19,115 +19,115 @@
 
 var HatoholServerSelector = function(selectedCb) {
 
-    var self = this;
+  var self = this;
 
-    var dialogButtons = [{
-      text: gettext("SELECT"),
-      click: function() {
-        if (selectedCb) {
-          if (!self.selectedRow)
-            selectedCb(null);
-          else
-            selectedCb(self.serverArray[self.selectedRow.index()]);
-        }
-        $(this).dialog("close");
-        $("#server-selector").remove();
-      }
-    }, {
-      text: gettext("CANCEL"),
-      click: function() {
-        if (selectedCb)
+  var dialogButtons = [{
+    text: gettext("SELECT"),
+    click: function() {
+      if (selectedCb) {
+        if (!self.selectedRow)
           selectedCb(null);
-        $(this).dialog("close");
-        $("#server-selector").remove();
+        else
+          selectedCb(self.serverArray[self.selectedRow.index()]);
       }
-    }];
-
-    // call the constructor of the super class
-    HatoholDialog.apply(
-      this, ["server-selector", "Server selecion", dialogButtons]);
-    setSelectButtonState(false);
-    getServerList();
-
-    function setSelectButtonState(state) {
-      var btn = $(".ui-dialog-buttonpane").find("button:contains(" +
-                  gettext("SELECT") + ")");
-      if (state) {
-         btn.removeAttr("disabled");
-         btn.removeClass("ui-state-disabled");
-      } else {
-         btn.attr("disabled", "disable");
-         btn.addClass("ui-state-disabled");
-      }
+      $(this).dialog("close");
+      $("#server-selector").remove();
     }
-
-    function makeTable(data) {
-      var html =
-      '<table class="table table-condensed table-striped table-hover" id="serverSelectTable">' +
-      '  <thead>' +
-      '    <tr>' +
-      '      <th>ID</th>' +
-      '      <th>' + gettext("Type") + '</th>' +
-      '      <th>' + gettext("Hostname") + '</th>' +
-      '      <th>' + gettext("IP Address") + '</th>' +
-      '      <th>' + gettext("Nickname") + '</th>' +
-      '    </tr>' +
-      '  </thead>' +
-      '  <tbody></tbody>' +
-      '</table>'
-      return html;
+  }, {
+    text: gettext("CANCEL"),
+    click: function() {
+      if (selectedCb)
+        selectedCb(null);
+      $(this).dialog("close");
+      $("#server-selector").remove();
     }
+  }];
 
-    function makeTableBody(reply) {
-      var s;
-      self.serverArray = reply.servers;
-      for (var i = 0; i < reply.servers.length; i++) {
-        sv = reply.servers[i];
-        s += '<tr>';
-        s += '<td>' + sv.id + '</td>';
-        s += '<td>' + sv.type + '</td>';
-        s += '<td>' + sv.hostName + '</td>';
-        s += '<td>' + sv.ipAddress + '</td>';
-        s += '<td>' + sv.nickname  + '</td>';
-        s += '</tr>';
-      }
-      return s;
+  // call the constructor of the super class
+  HatoholDialog.apply(
+    this, ["server-selector", "Server selecion", dialogButtons]);
+  setSelectButtonState(false);
+  getServerList();
+
+  function setSelectButtonState(state) {
+    var btn = $(".ui-dialog-buttonpane").find("button:contains(" +
+                gettext("SELECT") + ")");
+    if (state) {
+       btn.removeAttr("disabled");
+       btn.removeClass("ui-state-disabled");
+    } else {
+       btn.attr("disabled", "disable");
+       btn.addClass("ui-state-disabled");
     }
+  }
 
-    function getServerList() {
-      $.ajax({
-        url: "/tunnel/server",
-        type: "GET",
-        success: function(reply) {
-          var replyParser = new HatoholReplyParser(reply);
-          if (!(replyParser.getStatus() === REPLY_STATUS.OK)) {
-            $("#serverSelectMsgArea").text(replyParser.getStatusMessage());
-            return;
-          }
+  function makeTable(data) {
+    var html =
+    '<table class="table table-condensed table-striped table-hover" id="serverSelectTable">' +
+    '  <thead>' +
+    '    <tr>' +
+    '      <th>ID</th>' +
+    '      <th>' + gettext("Type") + '</th>' +
+    '      <th>' + gettext("Hostname") + '</th>' +
+    '      <th>' + gettext("IP Address") + '</th>' +
+    '      <th>' + gettext("Nickname") + '</th>' +
+    '    </tr>' +
+    '  </thead>' +
+    '  <tbody></tbody>' +
+    '</table>'
+    return html;
+  }
 
-          // create a table
-          var table = $(makeTable(reply));
-          self.replaceMainElement(table);
-          $("#serverSelectTable tbody").empty();
-          $("#serverSelectTable tbody").append(makeTableBody(reply));
+  function makeTableBody(reply) {
+    var s;
+    self.serverArray = reply.servers;
+    for (var i = 0; i < reply.servers.length; i++) {
+      sv = reply.servers[i];
+      s += '<tr>';
+      s += '<td>' + sv.id + '</td>';
+      s += '<td>' + sv.type + '</td>';
+      s += '<td>' + sv.hostName + '</td>';
+      s += '<td>' + sv.ipAddress + '</td>';
+      s += '<td>' + sv.nickname  + '</td>';
+      s += '</tr>';
+    }
+    return s;
+  }
 
-          // set events
-          $("#serverSelectTable tr").click(function(){
-            if (self.selectedRow)
-              self.selectedRow.removeClass("info");
-            else
-              setSelectButtonState(true);
-            $(this).attr("class", "info");
-            self.selectedRow = $(this);
-          });
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-          var errorMsg = "Error: " + XMLHttpRequest.status + ": " +
-                         XMLHttpRequest.statusText;
-          $("#serverSelectMsgArea").text(errorMsg);
+  function getServerList() {
+    $.ajax({
+      url: "/tunnel/server",
+      type: "GET",
+      success: function(reply) {
+        var replyParser = new HatoholReplyParser(reply);
+        if (!(replyParser.getStatus() === REPLY_STATUS.OK)) {
+          $("#serverSelectMsgArea").text(replyParser.getStatusMessage());
+          return;
         }
-      })
-    }
+
+        // create a table
+        var table = $(makeTable(reply));
+        self.replaceMainElement(table);
+        $("#serverSelectTable tbody").empty();
+        $("#serverSelectTable tbody").append(makeTableBody(reply));
+
+        // set events
+        $("#serverSelectTable tr").click(function(){
+          if (self.selectedRow)
+            self.selectedRow.removeClass("info");
+          else
+            setSelectButtonState(true);
+          $(this).attr("class", "info");
+          self.selectedRow = $(this);
+        });
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        var errorMsg = "Error: " + XMLHttpRequest.status + ": " +
+                       XMLHttpRequest.statusText;
+        $("#serverSelectMsgArea").text(errorMsg);
+      }
+    })
+  }
 }
 
 HatoholServerSelector.prototype = Object.create(HatoholDialog.prototype);
