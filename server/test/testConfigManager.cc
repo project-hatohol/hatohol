b8130d43d0b448e198859ff1de49b4e1b9c0f585
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-09-25 19:17:06 +0900

    Add a test file I forgot to add in the previous commit.

---
+++ server/test/testConfigManager.cc
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2013 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cppcutter.h>
+#include "config.h"
+#include "ConfigManager.h"
+#include "Hatohol.h"
+
+namespace testConfigManager {
+
+void cut_setup(void)
+{
+	hatoholInit();
+}
+
+// ---------------------------------------------------------------------------
+// Test cases
+// ---------------------------------------------------------------------------
+void test_getActionCommandDirectory(void)
+{
+	string expect = LIBEXECDIR "/" PACKAGE "/action";
+	cppcut_assert_equal(
+	  expect, ConfigManager::getActionCommandDirectory());
+}
+
+void test_setActionCommandDirectory(void)
+{
+	string exampleDir = "/usr/hoge/example/dog";
+	ConfigManager::setActionCommandDirectory(exampleDir);
+	cppcut_assert_equal(
+	  exampleDir, ConfigManager::getActionCommandDirectory());
+}
+
+} // namespace testConfigManager
