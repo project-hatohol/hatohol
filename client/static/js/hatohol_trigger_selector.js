9685ca2dd4537cb7ff6b2eb281479036c8fbb6ee
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-09-25 10:48:56 +0900

    Clean up HatoholTriggerSelector by using HatoholSelectorDialog.

--- client/static/js/hatohol_trigger_selector.js
+++ client/static/js/hatohol_trigger_selector.js
@@ -19,124 +19,50 @@
 
 var HatoholTriggerSelector = function(serverId, hostId, selectedCb) {
   var self = this;
-
-  var dialogButtons = [{
-    text: gettext("SELECT"),
-    click: selectButtonClickedCb,
-  }, {
-    text: gettext("CANCEL"),
-    click: cancelButtonClickedCb,
-  }];
+  self.queryData = {"serverId": serverId, "hostId": hostId};
 
   // call the constructor of the super class
-  HatoholDialog.apply(
-    this, ["trigger-selector", gettext("Trigger selecion"), dialogButtons]);
-  setSelectButtonState(false);
-  getTriggerList();
-
-  function selectButtonClickedCb() {
-    if (selectedCb) {
-      if (!self.selectedRow)
-        selectedCb(null);
-      else
-        selectedCb(self.triggerArray[self.selectedRow.index()]);
-    }
-    self.closeDialog();
-  }
-
-  function cancelButtonClickedCb() {
-    if (selectedCb)
-      selectedCb(null);
-    self.closeDialog();
-  }
-
-  function setSelectButtonState(state) {
-    var btn = $(".ui-dialog-buttonpane").find("button:contains(" +
-                gettext("SELECT") + ")");
-    if (state) {
-       btn.removeAttr("disabled");
-       btn.removeClass("ui-state-disabled");
-    } else {
-       btn.attr("disabled", "disable");
-       btn.addClass("ui-state-disabled");
-    }
-  }
-
-  function makeTable(data) {
-    var html =
-    '<table class="table table-condensed table-striped table-hover" id="triggerSelectTable">' +
-    '  <thead>' +
-    '    <tr>' +
-    '      <th>ID</th>' +
-    '      <th>' + gettext("Brief") + '</th>' +
-    '    </tr>' +
-    '  </thead>' +
-    '  <tbody></tbody>' +
-    '</table>'
-    return html;
-  }
-
-  function makeTableBody(reply) {
-    var s;
-    self.triggerArray = reply.triggers;
-    for (var i = 0; i < reply.triggers.length; i++) {
-      trigger = reply.triggers[i];
-      s += '<tr>';
-      s += '<td>' + trigger.id + '</td>';
-      s += '<td>' + trigger.brief + '</td>';
-      s += '</tr>';
-    }
-    return s;
-  }
-
-  function getTriggerList() {
-    $.ajax({
-      url: "/tunnel/trigger",
-      type: "GET",
-      data: {"serverId": serverId, "hostId": hostId},
-      success: function(reply) {
-        var replyParser = new HatoholReplyParser(reply);
-        if (!(replyParser.getStatus() === REPLY_STATUS.OK)) {
-          $("#triggerSelectMsgArea").text(replyParser.getStatusMessage());
-          return;
-        }
-        if (reply.numberOfTriggers == 0) {
-          $("#triggerSelectMsgArea").text(gettext("No data."));
-          return;
-        }
-
-        // create a table
-        var table = $(makeTable(reply));
-        self.replaceMainElement(table);
-        $("#triggerSelectTable tbody").empty();
-        $("#triggerSelectTable tbody").append(makeTableBody(reply));
-
-        // set events
-        $("#triggerSelectTable tr").click(function(){
-          if (self.selectedRow)
-            self.selectedRow.removeClass("info");
-          else
-            setSelectButtonState(true);
-          $(this).attr("class", "info");
-          self.selectedRow = $(this);
-        });
-      },
-      error: function(XMLHttpRequest, textStatus, errorThrown) {
-        var errorMsg = "Error: " + XMLHttpRequest.status + ": " +
-                       XMLHttpRequest.statusText;
-        $("#triggerSelectMsgArea").text(errorMsg);
-      }
-    })
-  }
+  HatoholSelectorDialog.apply(
+    this, ["trigger-selector", gettext("Trigger selecion"), selectedCb]);
+  self.start("/tunnel/trigger", "GET");
 }
 
-HatoholTriggerSelector.prototype = Object.create(HatoholDialog.prototype);
+HatoholTriggerSelector.prototype =
+  Object.create(HatoholSelectorDialog.prototype);
 HatoholTriggerSelector.prototype.constructor = HatoholTriggerSelector;
 
-HatoholTriggerSelector.prototype.createMainElement = function() {
-  var ptag = $("<p/>");
-  ptag.attr("id", "triggerSelectMsgArea");
-  ptag.text(gettext("Now getting trigger information..."));
-  return ptag;
+HatoholTriggerSelector.prototype.makeQueryData = function() {
+    return this.queryData;
 }
 
+HatoholTriggerSelector.prototype.getNumberOfObjects = function(reply) {
+  return reply.numberOfTriggers;
+}
+
+HatoholTriggerSelector.prototype.generateMainTable = function(tableId) {
+  var html =
+  '<table class="table table-condensed table-striped table-hover" id=' +
+  tableId + '>' +
+  '  <thead>' +
+  '    <tr>' +
+  '      <th>ID</th>' +
+  '      <th>' + gettext("Brief") + '</th>' +
+  '    </tr>' +
+  '  </thead>' +
+  '  <tbody></tbody>' +
+  '</table>'
+  return html;
+}
+
+HatoholTriggerSelector.prototype.generateTableRows = function(reply) {
+  var s = "";
+  this.setObjectArray(reply.triggers);
+  for (var i = 0; i < reply.triggers.length; i++) {
+    trigger = reply.triggers[i];
+    s += '<tr>';
+    s += '<td>' + trigger.id + '</td>';
+    s += '<td>' + trigger.brief + '</td>';
+    s += '</tr>';
+  }
+  return s;
+}
