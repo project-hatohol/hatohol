32208d19906513bcd73b4fd76d5b056ba84c6ca8
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-10-04 14:55:09 +0900

    [server][DBClientHatohol] Add a test: test_makeSelectConditionUserAdminFlag().

--- server/test/testDBClientHatohol.cc
+++ server/test/testDBClientHatohol.cc
@@ -297,6 +297,16 @@ static void _assertMakeCondition(const ServerHostGrpSetMap &srvHostGrpSetMap,
 }
 #define assertMakeCondition(M,E) cut_trace(_assertMakeCondition(M,E))
 
+static UserIdType findFirstUserWithAdminFlag(void)
+{
+	for (size_t i = 0; i < NumTestUserInfo; i++) {
+		if (testUserInfo[i].flags & USER_FLAG_ADMIN)
+			return i + 1;
+	}
+	cut_fail("Failed to find user with USER_FLAG_ADMIN"); 
+	return INVALID_USER_ID;
+}
+
 void cut_setup(void)
 {
 	hatoholInit();
@@ -622,6 +632,17 @@ void test_makeSelectConditionUserAdmin(void)
 {
 	EventQueryOption option;
 	option.setUserId(USER_ID_ADMIN);
+	string actual = TestDBClientHatohol::callMakeSelectCondition(option);
+	string expect = "";
+	cppcut_assert_equal(actual, expect);
+}
+
+void test_makeSelectConditionUserAdminFlag(void)
+{
+	setupTestDBUser(true, true);
+	UserIdType userId = findFirstUserWithAdminFlag();
+	EventQueryOption option;
+	option.setUserId(userId);
 	string actual = TestDBClientHatohol::callMakeSelectCondition(option);
 	string expect = "";
 	cppcut_assert_equal(actual, expect);
