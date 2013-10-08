7eed6553ab9648a253d3f9a6c84fb06b8834b709
Kazuhiro Yamato <kz0817@gmail.com>
2013-10-08 21:26:38 +0900

    [HatoholActionCreator] Add a new test: def test_add_action_trigger_id().

--- server/tools/test/TestHatoholVoyager.py
+++ server/tools/test/TestHatoholVoyager.py
@@ -154,6 +154,16 @@ class TestHatoholVoyager(unittest.TestCase):
     self.assert_url(arg_list, "http://localhost:33194/action", None,
                     expect_query)
 
+  def test_add_action_trigger_id(self):
+    ex_cmd = "ex-cmd -x --for ABC"
+    trigger_id = 0xfedcba9876543210
+    arg_list = ["add-action", "--type", "command", "--command", ex_cmd,
+                "--trigger-id", str(trigger_id)]
+    expect_query = {"type":hatohol.ACTION_COMMAND, "command":ex_cmd,
+                    "triggerId":str(trigger_id)}
+    self.assert_url(arg_list, "http://localhost:33194/action", None,
+                    expect_query)
+
   def test_del_action(self):
     arg_list = ["del-action", "25"]
     self.assert_url(arg_list, "http://localhost:33194/action/25", "DELETE")
