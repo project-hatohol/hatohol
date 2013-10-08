89403e37ffbd007e42474a8fb6d6f1c4bfe5a6e2
Kazuhiro Yamato <kz0817@gmail.com>
2013-10-08 21:56:15 +0900

    [HatoholActionCreator] Clean up: test_add_action_server_id().

--- server/tools/test/TestHatoholVoyager.py
+++ server/tools/test/TestHatoholVoyager.py
@@ -145,14 +145,9 @@ class TestHatoholVoyager(unittest.TestCase):
                     expect_query)
 
   def test_add_action_server_id(self):
-    ex_cmd = "ex-cmd -x --for ABC"
     server_id = 23
-    arg_list = ["add-action", "--type", "command", "--command", ex_cmd,
-                "--server-id", str(server_id)]
-    expect_query = {"type":hatohol.ACTION_COMMAND, "command":ex_cmd,
-                    "serverId":str(server_id)}
-    self.assert_url(arg_list, "http://localhost:33194/action", None,
-                    expect_query)
+    self._assert_add_action_one_opt("--server-id", str(server_id),
+                                    "serverId", str(server_id))
 
   def test_add_action_host_id(self):
     host_id = 0x890abcdef1234567
