a1f97cc8669c929312c17ef78b5a1fcffb9f11b5
Kazuhiro Yamato <kz0817@gmail.com>
2013-10-08 19:48:20 +0900

    [HatoholActionCreator] Add a new test: test_add_action_timeout().

--- server/tools/test/TestHatoholVoyager.py
+++ server/tools/test/TestHatoholVoyager.py
@@ -124,6 +124,16 @@ class TestHatoholVoyager(unittest.TestCase):
     self.assert_url(arg_list, "http://localhost:33194/action", None,
                     expect_query)
 
+  def test_add_action_working_timeout(self):
+    ex_cmd = "ex-cmd -x --for ABC"
+    timeout = "305"
+    arg_list = ["add-action", "--type", "command", "--command", ex_cmd,
+                "--timeout", timeout]
+    expect_query = {"type":hatohol.ACTION_COMMAND, "command":ex_cmd,
+                    "timeout":timeout}
+    self.assert_url(arg_list, "http://localhost:33194/action", None,
+                    expect_query)
+
   def test_del_action(self):
     arg_list = ["del-action", "25"]
     self.assert_url(arg_list, "http://localhost:33194/action/25", "DELETE")
