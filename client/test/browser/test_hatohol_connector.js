c555d0c91ec81083fcf310e77a064eeecdf602bb
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-10-22 10:40:16 +0900

    [HatoholConnecto][test] Add pre-processing to make/update a user for the test.

--- client/test/browser/test_hatohol_connector.js
+++ client/test/browser/test_hatohol_connector.js
@@ -1,6 +1,28 @@
 describe('HatoholConnector', function() {
   var TEST_USER = "test-user";
-  var TEST_PASSWORD = "test*passwd";
+  var TEST_PASSWORD = "test*pass*d";
+  before(function(done) {
+    var queryData = {
+        csrfmiddlewaretoken: $("*[name=csrfmiddlewaretoken]").val(),
+    };
+    $.ajax({
+      url: "/tunnel/test/user",
+      type: "POST",
+      data: {"user":TEST_USER, "password":TEST_PASSWORD, "flags":0},
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("X-CSRFToken", queryData.csrfmiddlewaretoken);
+      },
+      success: function(data) {
+        var parser = new HatoholReplyParser(data);
+        expect(parser.getStatus()).to.be(REPLY_STATUS.OK);
+        done();
+      },
+      error: function connectError(XMLHttpRequest, textStatus, errorThrown) {
+        expect().fail(textStatus);
+        done();
+      },
+    });
+  });
 
   beforeEach(function(done) {
     HatoholSessionManager.deleteCookie();
