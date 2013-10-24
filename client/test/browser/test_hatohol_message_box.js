13db28766031d60d138158c7a45a7bcfc1f1df31
Kazuhiro Yamato <kz0817@gmail.com>
2013-10-24 21:25:54 +0900

    [HatoholMessageBox][test] Add a new test: 'specify the title'.

--- client/test/browser/test_hatohol_message_box.js
+++ client/test/browser/test_hatohol_message_box.js
@@ -38,4 +38,31 @@ describe('HatoholMessageBox', function() {
     });
     msgbox = new HatoholMessageBox(msg);
   });
+
+  it('specify the title', function(done) {
+    var msg = "Test message.";
+    var param = {title: "Title Teeeeeest"};
+    HatoholDialogObserver.registerCreatedCallback(function(id, obj) {
+      if (!("getDefaultId" in obj))
+        return;
+      if (id != obj.getDefaultId())
+        return;
+
+      // message
+      expect(obj.getMessage()).to.be(msg);
+
+      // title bar
+      expect(obj.isTitleBarVisible()).to.be(true);
+      expect(obj.getTitleString()).to.be(param.title);
+
+      // button
+      var buttons = obj.getButtons();
+      expect(buttons).to.have.length(1);
+      var button = buttons[0];
+      expect(button.text).to.be(obj.getDefaultButtonLabel());
+
+      done();
+    });
+    msgbox = new HatoholMessageBox(msg, param);
+  });
 });
