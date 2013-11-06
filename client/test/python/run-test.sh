2af4e6c740d8b6f59651f307d3c0014fe1fa5cdf
Kazuhiro Yamato <kz0817@gmail.com>
2013-11-06 21:49:45 +0900

    [UserConfig][test] Can specifiy a test case.

--- client/test/python/run-test.sh
+++ client/test/python/run-test.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
+#
+# Example
+# $ ./run-tesh.sh TestUserConfig.TestUserConfig.test_get_integer
+#
+
+testcase=""
+if [ $# -ge 1 ]; then
+  testcase=$1
+fi
+
 export PYTHONPATH=../..:.
 export DJANGO_SETTINGS_MODULE=testsettings 
 ../../manage.py syncdb
-python -m unittest discover -p 'Test*.py'
+
+if [ -z $testcase ]; then
+  python -m unittest discover -p 'Test*.py'
+else
+  python -m unittest $testcase
+fi
+
