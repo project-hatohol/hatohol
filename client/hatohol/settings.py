430b4c4a8b87a3333758c7d03d5f66a5d12fcaff
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-11-06 13:23:02 +0900

    [client][test] Revert a comment out for the feature that handles static files (django.contrib.staticfiles).

--- client/hatohol/settings.py
+++ client/hatohol/settings.py
@@ -142,7 +142,7 @@ INSTALLED_APPS = (
     #'django.contrib.sessions',
     #'django.contrib.sites',
     #'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
