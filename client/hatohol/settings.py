8e46e103fc2fa11e1f5515cd840ec25091efe79e
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-11-06 13:03:29 +0900

    [client][settings.py] Comment out the feature we don't use at present.

--- client/hatohol/settings.py
+++ client/hatohol/settings.py
@@ -137,12 +137,12 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    #'django.contrib.auth',
+    #'django.contrib.contenttypes',
+    #'django.contrib.sessions',
+    #'django.contrib.sites',
+    #'django.contrib.messages',
+    #'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
