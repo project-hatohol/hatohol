7a9b4fb5e1079102f9322dd805e842d1e706a61f
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2013-11-11 12:45:01 +0900

    [UserConfigView][test] Use the port that is different from the default port of Hatohol server.
    This patch avoids a conflict when a hatohol server is running
    on the host.

--- client/hatohol/hatoholserver.py
+++ client/hatohol/hatoholserver.py
@@ -15,12 +15,25 @@
 # You should have received a copy of the GNU General Public License
 # along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import logging
+
 SERVER_ADDR = 'localhost'
 SERVER_PORT = 33194
 SESSION_NAME_META = 'HTTP_X_HATOHOL_SESSION'
+
+def _setup():
+    global SERVER_PORT
+    server_port = os.getenv('HATOHOL_SERVER_PORT')
+    if server_port:
+        SERVER_PORT = int(server_port)
+        logger = logging.getLogger(__name__)
+        logger.info('Server port: %d' % SERVER_PORT)
 
 def get_address():
     return SERVER_ADDR
 
 def get_port():
     return SERVER_PORT
+
+_setup()
