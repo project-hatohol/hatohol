89c14245292ef94d2186da6b403cbc2e85272c07
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2013-11-20 14:23:48 +0900

    [ZabbixAPIEmulator] Add new map: JsonData

--- server/test/ZabbixAPIEmulator.cc
+++ server/test/ZabbixAPIEmulator.cc
@@ -59,6 +59,31 @@ struct ZabbixAPIEmulator::ParameterEventGet {
 	}
 };
 
+struct ZabbixAPIEmulator::JsonKeys {
+	int64_t source;
+	int64_t object;
+	int64_t objectId;
+	int64_t clock;
+	int64_t value;
+	int64_t acknowledged;
+	int64_t ns;
+	int64_t value_changed;
+
+	JsonKeys(void)
+	: source(0),
+	  object(0),
+	  objectId(0),
+	  clock(0),
+	  value(0),
+	  acknowledged(0),
+	  ns(0),
+	  value_changed(0)
+	{
+	}
+};
+
+typedef map<int64_t, ZabbixAPIEmulator::JsonKeys> JsonData;
+
 struct ZabbixAPIEmulator::PrivateContext {
 	GThread    *thread;
 	guint       port;
@@ -70,6 +95,7 @@ struct ZabbixAPIEmulator::PrivateContext {
 	size_t        currEventSliceIndex;
 	vector<string> slicedEventVector;
 	GMainContext   *gMainCtx;
+	JsonData	jsonData;
 	
 	// methods
 	PrivateContext(void)
