6602dfd794135b54bc31343f50983c36ee0b8445
Takuro Ashie <ashie@clear-code.com>
2013-12-02 11:41:20 +0900

    [FaceRest] Rename misleading function names.
    addServerIdNameHash ->
    addServersMap
    
    addHostsIdNameHash ->
    addHostsMap

--- server/src/FaceRest.cc
+++ server/src/FaceRest.cc
@@ -1087,7 +1087,7 @@ static string getHostName(const ServerID serverId, const HostID hostId)
 	return hostName;
 }
 
-static void addHostsIdNameHash(
+static void addHostsMap(
   JsonBuilderAgent &agent, MonitoringServerInfo &serverInfo,
   HostNameMaps &hostMaps, bool lookupHostName = false)
 {
@@ -1148,7 +1148,7 @@ static void addTriggersIdBriefHash(
 	agent.endObject();
 }
 
-static void addServersIdNameHash(
+static void addServersMap(
   JsonBuilderAgent &agent,
   HostNameMaps *hostMaps = NULL, bool lookupHostName = false,
   TriggerBriefMaps *triggerMaps = NULL, bool lookupTriggerBrief = false)
@@ -1164,8 +1164,8 @@ static void addServersIdNameHash(
 		agent.startObject(StringUtils::toString(serverInfo.id));
 		agent.add("name", serverInfo.hostName);
 		if (hostMaps) {
-			addHostsIdNameHash(agent, serverInfo,
-			                   *hostMaps, lookupHostName);
+			addHostsMap(agent, serverInfo,
+				    *hostMaps, lookupHostName);
 		}
 		if (triggerMaps) {
 			addTriggersIdBriefHash(agent, serverInfo, *triggerMaps,
@@ -1359,7 +1359,7 @@ void FaceRest::handlerGetTrigger(RestJob *job)
 		  = triggerInfo.hostName;
 	}
 	agent.endArray();
-	addServersIdNameHash(agent, &hostMaps);
+	addServersMap(agent, &hostMaps);
 	agent.endObject();
 
 	replyJsonData(agent, job);
@@ -1408,7 +1408,7 @@ void FaceRest::handlerGetEvent(RestJob *job)
 		  = eventInfo.hostName;
 	}
 	agent.endArray();
-	addServersIdNameHash(agent, &hostMaps);
+	addServersMap(agent, &hostMaps);
 	agent.endObject();
 
 	replyJsonData(agent, job);
@@ -1457,7 +1457,7 @@ void FaceRest::replyGetItem(RestJob *job)
 		agent.endObject();
 	}
 	agent.endArray();
-	addServersIdNameHash(agent);
+	addServersMap(agent);
 	agent.endObject();
 
 	replyJsonData(agent, job);
@@ -1571,8 +1571,8 @@ void FaceRest::handlerGetAction(RestJob *job)
 	agent.endArray();
 	const bool lookupHostName = true;
 	const bool lookupTriggerBrief = true;
-	addServersIdNameHash(agent, &hostMaps, lookupHostName,
-	                     &triggerMaps, lookupTriggerBrief);
+	addServersMap(agent, &hostMaps, lookupHostName,
+		      &triggerMaps, lookupTriggerBrief);
 	agent.endObject();
 
 	replyJsonData(agent, job);
