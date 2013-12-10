b0133aacd9b0f4ea8309e2aabdea385a559ad198
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2013-12-10 09:36:49 +0900

    [ArmZabbixAPI] Add changed function signature.
    I forgot to commit this in e09d130 revision.

--- server/src/ArmZabbixAPI.cc
+++ server/src/ArmZabbixAPI.cc
@@ -283,7 +283,7 @@ uint64_t ArmZabbixAPI::getLastEventId(void)
 	return lastEventId;
 }
 
-void ArmZabbixAPI::setItemTableData(ItemTablePtr itemPtr)
+void ArmZabbixAPI::setItemTableData(const ItemTablePtr &itemPtr)
 {
 	// This function is virtual function.
 	// This function is used on a test phase.
