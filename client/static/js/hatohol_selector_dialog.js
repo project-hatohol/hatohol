24d287a5dafe49a3e8d12c8c0f4fc34c263d3586
Takuro Ashie <ashie@clear-code.com>
2013-12-16 12:26:27 +0900

    [client][HatoholSelectorDialog] Revert wrong modification in f409459de.
    I misinterpreted HatoholSelectorDialog as HatoholPriviledgeSelectorDialog...

--- client/static/js/hatohol_selector_dialog.js
+++ client/static/js/hatohol_selector_dialog.js
@@ -17,7 +17,7 @@
  * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
  */
 
-var HatoholPriviledgeEditDialog = function(id, title, selectedCallback) {
+var HatoholSelectorDialog = function(id, title, selectedCallback) {
   var self = this;
   self.selectedCallback = selectedCallback;
   self.objectArray = null;
@@ -36,17 +36,17 @@ var HatoholPriviledgeEditDialog = function(id, title, selectedCallback) {
   self.setSelectButtonState(false);
 }
 
-HatoholPriviledgeEditDialog.prototype = Object.create(HatoholDialog.prototype);
-HatoholPriviledgeEditDialog.prototype.constructor = HatoholPriviledgeEditDialog;
+HatoholSelectorDialog.prototype = Object.create(HatoholDialog.prototype);
+HatoholSelectorDialog.prototype.constructor = HatoholSelectorDialog;
 
-HatoholPriviledgeEditDialog.prototype.createMainElement = function() {
+HatoholSelectorDialog.prototype.createMainElement = function() {
   var ptag = $("<p/>");
   ptag.attr("id", "selectorDialogMsgArea");
   ptag.text(gettext("Now getting information..."));
   return ptag;
 }
 
-HatoholPriviledgeEditDialog.prototype.selectButtonClicked = function() {
+HatoholSelectorDialog.prototype.selectButtonClicked = function() {
   if (!this.selectedCallback)
     return;
   if (!this.selectedRow)
@@ -56,13 +56,13 @@ HatoholPriviledgeEditDialog.prototype.selectButtonClicked = function() {
   this.closeDialog();
 }
 
-HatoholPriviledgeEditDialog.prototype.cancelButtonClicked = function() {
+HatoholSelectorDialog.prototype.cancelButtonClicked = function() {
   if (this.selectedCallback)
     this.selectedCallback(null);
   this.closeDialog();
 }
 
-HatoholPriviledgeEditDialog.prototype.setSelectButtonState = function(state) {
+HatoholSelectorDialog.prototype.setSelectButtonState = function(state) {
   var btn = $(".ui-dialog-buttonpane").find("button:contains(" +
               gettext("SELECT") + ")");
   if (state) {
@@ -74,27 +74,27 @@ HatoholPriviledgeEditDialog.prototype.setSelectButtonState = function(state) {
   }
 }
 
-HatoholPriviledgeEditDialog.prototype.setMessage = function(msg) {
+HatoholSelectorDialog.prototype.setMessage = function(msg) {
   $("#selectorDialogMsgArea").text(msg);
 }
 
-HatoholPriviledgeEditDialog.prototype.setObjectArray = function(ary) {
+HatoholSelectorDialog.prototype.setObjectArray = function(ary) {
   this.objectArray = ary;
 }
 
-HatoholPriviledgeEditDialog.prototype.setSelectedRow = function(row) {
+HatoholSelectorDialog.prototype.setSelectedRow = function(row) {
   this.selectedRow = row;
 }
 
-HatoholPriviledgeEditDialog.prototype.getSelectedRow = function() {
+HatoholSelectorDialog.prototype.getSelectedRow = function() {
   return this.selectedRow;
 }
 
-HatoholPriviledgeEditDialog.prototype.makeQueryData= function() {
+HatoholSelectorDialog.prototype.makeQueryData= function() {
   return {};
 }
 
-HatoholPriviledgeEditDialog.prototype.start = function(url, requestType) {
+HatoholSelectorDialog.prototype.start = function(url, requestType) {
   var self = this;
   $.ajax({
     url: url,
