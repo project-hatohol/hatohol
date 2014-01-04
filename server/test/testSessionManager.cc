9cf4fd5942ac1162fc8c4711fcd8623a7c6eaeb6
Kazuhiro Yamato <kz0817@gmail.com>
2014-01-04 10:50:39 +0900

    [server][testSessionManager] Add tests.

---
+++ server/test/testSessionManager.cc
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2014 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cppcutter.h>
+#include "SessionManager.h"
+
+namespace testSessionManager {
+
+// ---------------------------------------------------------------------------
+// Test cases
+// ---------------------------------------------------------------------------
+void test_getInstance(void)
+{
+	cppcut_assert_not_equal(static_cast<SessionManager *>(NULL),
+	                        SessionManager::getInstance());
+}
+
+void test_isntanceIsSingleton(void)
+{
+	SessionManager *instance1 = SessionManager::getInstance();
+	SessionManager *instance2 = SessionManager::getInstance();
+	cppcut_assert_equal(instance1, instance2);
+}
+
+} // namespace testSessionManager
+
