d76c9a3fefcfa91ff91090eae80e22f396ebc176
Takuro Ashie <ashie@clear-code.com>
2014-01-17 12:23:48 +0900

    [client] Add a skeleton of HatoholUserRoleEditor

---
+++ client/test/browser/test_hatohol_user_role_editor.js
@@ -0,0 +1,22 @@
+describe('HatoholUserRoleEditor', function() {
+  var editor;
+
+  beforeEach(function() {
+    editor = undefined;
+  });
+
+  afterEach(function() {
+    if (editor)
+      editor.closeDialog();
+  });
+
+  it('new', function() {
+    var expectedId = "#user-role-editor";
+    editor = new HatoholUserRoleEditor();
+    expect(editor).not.to.be(undefined);
+    expect($(expectedId)).to.have.length(1);
+    var buttons = $(expectedId).dialog("option", "buttons");
+    expect(buttons).to.have.length(1);
+    expect(buttons[0].text).to.be(gettext("CLOSE"));
+  });
+});
