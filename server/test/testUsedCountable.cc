d3f8b600d171a1d0ccd888a6eb976ee5aca0b300
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2014-01-17 11:01:37 +0900

    [server][test] Add a new test for UsedCountable.

---
+++ server/test/testUsedCountable.cc
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2014 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cppcutter.h>
+#include "UsedCountable.h"
+
+namespace testUsedCountable {
+
+class TestUsedCountable : public UsedCountable {
+};
+
+static TestUsedCountable *g_countable = NULL;
+
+void cut_teardown(void)
+{
+	if (g_countable) {
+		while (g_countable->getUsedCount() >= 1)
+			g_countable->unref();
+		g_countable = NULL;
+	}
+}
+
+// ---------------------------------------------------------------------------
+// Test cases
+// ---------------------------------------------------------------------------
+void test_constructor(void)
+{
+	g_countable = new TestUsedCountable();
+	cppcut_assert_equal(1, g_countable->getUsedCount());
+}
+
+} // namespace testUsedCountable
