4fbb6bdf4672998682b2c0688c228b921d9116f9
Takuro Ashie <ashie@clear-code.com>
2014-01-21 09:48:46 +0900

    [server][test] Fix a wrong flag in d057bee

--- server/test/DBClientTest.cc
+++ server/test/DBClientTest.cc
@@ -369,7 +369,7 @@ UserRoleInfo testUserRoleInfo[] = {
 {
 	0,                           // id
 	"Specific Sever admin",      // name
-	(OPPRVLG_UPDATE_SERVER << 1) // flags
+	(1 << OPPRVLG_UPDATE_SERVER) // flags
 }, {
 	0,                 // id
 	"Action master",   // name
