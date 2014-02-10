f2378dc860b0904553b554fd0c7fbc4a8a897d29
Takuro Ashie <ashie@clear-code.com>
2014-02-10 10:19:47 +0900

    [HatoholError] Add error messages

--- server/src/HatoholError.cc
+++ server/src/HatoholError.cc
@@ -31,6 +31,56 @@ void HatoholError::init(void)
 	errorMessages[HTERR_OK] = "OK.";
 	errorMessages[HTERR_UNINITIALIZED] =
 	  "Uninitialized (This is probably a bug).";
+	errorMessages[HTERR_UNKNOWN_REASON]  = "Unknown reason.";
+	errorMessages[HTERR_NOT_IMPLEMENTED] = "Not implemented.";
+	errorMessages[HTERR_GOT_EXCEPTION]   = "Got exception.";
+	errorMessages[HTERR_INVALID_USER]    = "Invalid user.";
+
+	// DBClientConfig
+	errorMessages[HTERR_INVALID_MONITORING_SYSTEM_TYPE] =
+	  "Invalid monitoring system type.";
+	errorMessages[HTERR_INVALID_PORT_NUMBER] = "Invalid port number.";
+	errorMessages[HTERR_INVALID_IP_ADDRESS]  = "Invalid IP address.";
+	errorMessages[HTERR_INVALID_HOST_NAME]   = "Invalid host name.";
+	errorMessages[HTERR_NO_IP_ADDRESS_AND_HOST_NAME] =
+	  "No IP address and host name.";
+	errorMessages[HTERR_NOT_FOUND_SERVER_ID] = "Not found server ID.";
+
+	// DBClientUser
+	errorMessages[HTERR_EMPTY_USER_NAME]      = "Empty user name.";
+	errorMessages[HTERR_TOO_LONG_USER_NAME]   = "Too long user name.";
+	errorMessages[HTERR_INVALID_CHAR]         = "Invalid character.";
+	errorMessages[HTERR_EMPTY_PASSWORD]       = "Password is empty.";
+	errorMessages[HTERR_TOO_LONG_PASSWORD]    = "Too long password.";
+	errorMessages[HTERR_USER_NAME_EXIST]      = "The user name exists.";
+	errorMessages[HTERR_NO_PRIVILEGE]         = "No privilege.";
+	errorMessages[HTERR_INVALID_USER_FLAGS]   = "Invalid user flags.";
+	errorMessages[HTERR_NOT_FOUND_USER_ID]    = "Not found user ID.";
+	errorMessages[HTERR_EMPTY_USER_ROLE_NAME] = "Empty user role name.";
+	errorMessages[HTERR_TOO_LONG_USER_ROLE_NAME] =
+	  "Too long User role name.";
+	errorMessages[HTERR_USER_ROLE_NAME_OR_FLAGS_EXIST] =
+	  "The user role name or the flags exists.";
+	errorMessages[HTERR_NOT_FOUND_USER_ROLE_ID] =
+	  "Not found user role ID.";
+
+	// DBClientHatohol
+	errorMessages[HTERR_NOT_FOUND_SORT_ORDER] = "Not found sort order.";
+
+	// DBClientAction
+	errorMessages[HTERR_DELETE_IMCOMPLETE] = "Deletion imcpleted.";
+
+	// FaceRest
+	errorMessages[HTERR_UNSUPORTED_FORMAT]    = "Unsupported format.";
+	errorMessages[HTERR_NOT_FOUND_SESSION_ID] = "Not found session ID.";
+	errorMessages[HTERR_NOT_FOUND_ID_IN_URL]  = "Not found ID in the URL.";
+	errorMessages[HTERR_NOT_FOUND_PARAMETER]  = "Not found parametr.";
+	errorMessages[HTERR_INVALID_PARAMETER]    = "Invalid parameter.";
+	errorMessages[HTERR_AUTH_FAILED]          = "Authentication failed.";
+	errorMessages[HTERR_NOT_TEST_MODE]        = "Not test mode.";
+
+	// OTHER
+	errorMessages[HTERR_ERROR_TEST] = "Error test.";
 }
 
 HatoholError::HatoholError(const HatoholErrorCode &code,
