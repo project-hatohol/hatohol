c76e977e2fa820579a623277408ffbb8b48b5d5d
Takuro Ashie <ashie@clear-code.com>
2014-02-12 15:19:31 +0900

    [HatoholReplyParser] Add tests for some error statuses

--- client/test/browser/test_hatohol_reply_parser.js
+++ client/test/browser/test_hatohol_reply_parser.js
@@ -1,6 +1,5 @@
 describe('HatoholReplyParser', function() {
   beforeEach(function() {
-
   });
 
   afterEach(function() {
@@ -8,10 +7,37 @@ describe('HatoholReplyParser', function() {
 
   it('new with HTERR_OK', function() {
     var reply = {
-      apiVersion: 3,
+      apiVersion: hatohol.FACE_REST_API_VERSION,
       errorCode: hatohol.HTERR_OK
     };
     var parser = new HatoholReplyParser(reply);
     expect(parser.getStatus()).to.be(REPLY_STATUS.OK);
+  });
+
+  it('null', function() {
+    var parser = new HatoholReplyParser(null);
+    expect(parser.getStatus()).to.be(REPLY_STATUS.NULL_OR_UNDEFINED);
+  });
+
+  it('undefined', function() {
+    var parser = new HatoholReplyParser(undefined);
+    expect(parser.getStatus()).to.be(REPLY_STATUS.NULL_OR_UNDEFINED);
+  });
+
+  it('no apiVersion', function() {
+    var reply = {
+      errorCode: hatohol.HTERR_OK
+    };
+    var parser = new HatoholReplyParser(reply);
+    expect(parser.getStatus()).to.be(REPLY_STATUS.NOT_FOUND_API_VERSION);
+  });
+
+  it('unmatched apiVersion', function() {
+    var reply = {
+      apiVersion: hatohol.FACE_REST_API_VERSION + 1,
+      errorCode: hatohol.HTERR_OK
+    };
+    var parser = new HatoholReplyParser(reply);
+    expect(parser.getStatus()).to.be(REPLY_STATUS.UNSUPPORTED_API_VERSION);
   });
 });
