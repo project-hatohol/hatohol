c67e9567cc14a5a4d97a6900ca52e49ccbcf0c18
Takuro Ashie <ashie@clear-code.com>
2014-02-13 15:06:33 +0900

    [client][test] Fix a wrong regexp

--- client/test/browser/test_hatohol_message_box.js
+++ client/test/browser/test_hatohol_message_box.js
@@ -173,7 +173,7 @@ describe('HatoholMessageBox', function() {
     HatoholDialogObserver.registerCreatedCallback(function(id, obj) {
       if (!isTargetObject(id, obj))
         return;
-      var expectRe = new RegExp(gettext('^Error test.'));
+      var expectRe = new RegExp("^" + gettext('Error test.'));
       expect(obj.getMessage()).to.match(expectRe);
       obj.destroy();
       done();
