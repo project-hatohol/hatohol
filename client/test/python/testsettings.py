ec838ddf8bab4628eab0dfca0586aa94d753fcc1
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2014-02-19 19:44:27 +0900

    [client] Do not load some Django's middleware modules to avoid an error.
    I has got the following Django's error when I accessed '/viewer'
    with a web browser, since 2 day ago.
    
    My environment is Xubuntu 13.10 (64bit). I guess a recent update
    of pacakages caused the problem. To solve it, this patch removes
    the unnecessary middleware modules of the Django, which are
    concerned with the issue.
    
    -------------------------------------------
    Environment:
    
    Request Method: GET
    Request URL: http://localhost:8000/viewer/ajax_users
    
    Django Version: 1.5.4
    Python Version: 2.7.5
    Installed Applications:
    ('django.contrib.staticfiles', 'hatohol')
    Installed Middleware:
    ('django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware')
    
    Traceback:
    File "/usr/local/lib/python2.7/dist-packages/django/core/handlers/base.py" in get_response
      92.                     response = middleware_method(request)
    File "/usr/local/lib/python2.7/dist-packages/django/middleware/locale.py" in process_request
      23.             request, check_path=check_path)
    File "/usr/local/lib/python2.7/dist-packages/django/utils/translation/__init__.py" in get_language_from_request
      126.     return _trans.get_language_from_request(request, check_path)
    File "/usr/local/lib/python2.7/dist-packages/django/utils/translation/trans_real.py" in get_language_from_request
      390.         lang_code = request.session.get('django_language', None)
    File "/usr/local/lib/python2.7/dist-packages/django/contrib/sessions/backends/base.py" in get
      55.         return self._session.get(key, default)
    File "/usr/local/lib/python2.7/dist-packages/django/contrib/sessions/backends/base.py" in _get_session
      167.                 self._session_cache = self.load()
    File "/usr/local/lib/python2.7/dist-packages/django/contrib/sessions/backends/db.py" in load
      18.                 expire_date__gt=timezone.now()
    File "/usr/local/lib/python2.7/dist-packages/django/db/models/manager.py" in get
      143.         return self.get_query_set().get(*args, **kwargs)
    File "/usr/local/lib/python2.7/dist-packages/django/db/models/query.py" in get
      398.         num = len(clone)
    File "/usr/local/lib/python2.7/dist-packages/django/db/models/query.py" in __len__
      106.                 self._result_cache = list(self.iterator())
    File "/usr/local/lib/python2.7/dist-packages/django/db/models/query.py" in iterator
      317.         for row in compiler.results_iter():
    File "/usr/local/lib/python2.7/dist-packages/django/db/models/sql/compiler.py" in results_iter
      775.         for rows in self.execute_sql(MULTI):
    File "/usr/local/lib/python2.7/dist-packages/django/db/models/sql/compiler.py" in execute_sql
      840.         cursor.execute(sql, params)
    File "/usr/local/lib/python2.7/dist-packages/django/db/backends/util.py" in execute
      41.             return self.cursor.execute(sql, params)
    File "/usr/local/lib/python2.7/dist-packages/django/db/backends/mysql/base.py" in execute
      130.             six.reraise(utils.DatabaseError, utils.DatabaseError(*tuple(e.args)), sys.exc_info()[2])
    File "/usr/local/lib/python2.7/dist-packages/django/db/backends/mysql/base.py" in execute
      120.             return self.cursor.execute(query, args)
    File "/usr/local/lib/python2.7/dist-packages/MySQLdb/cursors.py" in execute
      201.             self.errorhandler(self, exc, value)
    File "/usr/local/lib/python2.7/dist-packages/MySQLdb/connections.py" in defaulterrorhandler
      36.     raise errorclass, errorvalue
    
    Exception Type: DatabaseError at /viewer/ajax_users
    Exception Value: (1146, "Table 'hatohol_client.django_session' doesn't exist")

--- client/test/python/testsettings.py
+++ client/test/python/testsettings.py
@@ -115,10 +115,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
