07791a94969f0141873aa4b1feff93c52c4f5ca4
Takuro Ashie <ashie@clear-code.com>
2014-02-26 13:35:03 +0900

    [client] Sorry, I forgot to add a new test file at 6a392422

---
+++ client/test/browser/test_hatohol_monitoring_view.js
@@ -0,0 +1,27 @@
+describe('HatoholMonitoringView', function() {
+  it('set filter candidates', function() {
+    var candidates = [
+      { label: 'apple',  value: 1 },
+      { label: 'orange', value: 2 },
+      { label: 'lemon',  value: 3 }
+    ];
+    var target = $('<select>');
+    var setCandidates = HatoholMonitoringView.prototype.setFilterCandidates;
+    var expected = '<option>---------</option>';
+    var i;
+    for (i = 0; i < candidates.length; i++) {
+      expected += '<option value="' + candidates[i].value + '">' +
+                  candidates[i].label + "</option>";
+    }
+    setCandidates(target, candidates);
+    expect(target.html()).to.be(expected);
+  });
+
+  it('set empty filter', function() {
+    var target = $('<select>');
+    var setCandidates = HatoholMonitoringView.prototype.setFilterCandidates;
+    setCandidates(target);
+    var expected = '<option>---------</option>';
+    expect(target.html()).to.be(expected);
+  });
+});
