7f1a1833bad579e5babd734ccc6b02802aac682f
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2014-03-10 17:39:42 +0900

    [client][test_severs_view] add a new test: 'set nonexisting server id'.

--- client/test/browser/test_server_view.js
+++ client/test/browser/test_server_view.js
@@ -10,4 +10,11 @@ describe('ServerView', function() {
     var parser = new ServerConnStatParser(pkt);
     expect(parser.isBadPacket()).to.be(true); 
   });
+
+  it('set nonexisting server id', function() {
+    var pkt = {serverConnStat:{}};
+    var parser = new ServerConnStatParser(pkt);
+    expect(parser.setServerId(5)).to.be(false); 
+  });
+
 });
