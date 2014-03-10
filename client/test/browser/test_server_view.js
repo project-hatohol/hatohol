6370e684b19c8c1bc590b247ef2afb02759c9a1d
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2014-03-10 18:28:39 +0900

    [client][test_severs_view] add a new test: 'get status label at initial state'.

--- client/test/browser/test_server_view.js
+++ client/test/browser/test_server_view.js
@@ -1,5 +1,17 @@
 describe('ServerView', function() {
 
+  function checkGetStatusLabel(stat, expectMsg, expectMsgClass) {
+    var pkt = {serverConnStat:{'5':{status:stat}}};
+    var parser = new ServerConnStatParser(pkt);
+    expect(parser.setServerId(5)).to.be(true); 
+    var label = parser.getStatusLabel();
+    expect(label.msg).to.be(expectMsg);
+    expect(label.msgClass).to.be(expectMsgClass);
+  }
+
+  // -------------------------------------------------------------------------
+  // Test cases
+  // -------------------------------------------------------------------------
   it('pass an undefined packet', function() {
     var parser = new ServerConnStatParser();
     expect(parser.isBadPacket()).to.be(true); 
@@ -42,6 +54,11 @@ describe('ServerView', function() {
     var parser = new ServerConnStatParser(pkt);
     expect(parser.setServerId(5)).to.be(true); 
     expect(parser.getStatusLabel()).to.be("N/A");
+  });
+
+  it('get status label at initial state', function() {
+    checkGetStatusLabel(hatohol.ARM_WORK_STAT_INIT,
+                        "Initial State", "text-warning");
   });
 
 });
