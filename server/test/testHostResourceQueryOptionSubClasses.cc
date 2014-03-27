5f2d3a27153f8e3bacabd91079e77356f653ed63
Kazuhiro Yamato <kz0817@gmail.com>
2014-03-27 22:38:21 +0900

    [server][testHostResourceQueryOptionSubClasses] Fix an expected value that have been changed by the commit: 467e64bc.

--- server/test/testHostResourceQueryOptionSubClasses.cc
+++ server/test/testHostResourceQueryOptionSubClasses.cc
@@ -544,7 +544,10 @@ void test_hostgroupsQueryOptionCallGetConditionFromUserWithoutAllServers(void)
 	HostgroupsQueryOption option(userId);
 	option.setFilterForDataOfDefunctServers(false);
 	const string actual = option.getCondition();
-	const string expect = "(server_id=1 AND host_group_id IN (0,1))";
+	const string expect = StringUtils::sprintf(
+	  "(%s.server_id=1 AND %s.host_group_id IN (0,1))",
+	  DBClientHatohol::TABLE_NAME_HOSTGROUPS, 
+	  DBClientHatohol::TABLE_NAME_MAP_HOSTS_HOSTGROUPS);
 	cppcut_assert_equal(expect, actual);
 }
 
