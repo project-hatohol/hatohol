8c207135a43a8d8429be8b53f95ac09a43afd6c9
Takuro Ashie <ashie@clear-code.com>
2014-05-09 09:25:30 +0900

    [HatoholPager] Fix broken tests
    The "previous page" button on the first page is removed at e47d0fa8.

--- client/test/browser/test_hatohol_pager.js
+++ client/test/browser/test_hatohol_pager.js
@@ -27,21 +27,19 @@ describe('HatoholPager', function() {
   it('create with 100 records', function() {
     var pager = new HatoholPager({numTotalRecords: 100});
     var i, list = $('#' + fixtureId + ' li');
-    expect(list.length).to.be(4);
-    expect($(list[0]).text()).to.be($('<div/>').html("&laquo;").text());
+    expect(list.length).to.be(3);
     for (i = 0; i < 2; i++)
-      expect($(list[i + 1]).text()).to.be("" + (i + 1));
-    expect($(list[3]).text()).to.be($('<div/>').html("&raquo;").text());
+      expect($(list[i]).text()).to.be("" + (i + 1));
+    expect($(list[2]).text()).to.be($('<div/>').html("&raquo;").text());
   });
 
   it('create with 101 records', function() {
     var pager = new HatoholPager({numTotalRecords: 101});
     var i, list = $('#' + fixtureId + ' li');
-    expect(list.length).to.be(5);
-    expect($(list[0]).text()).to.be($('<div/>').html("&laquo;").text());
+    expect(list.length).to.be(4);
     for (i = 0; i < 3; i++)
-      expect($(list[i + 1]).text()).to.be("" + (i + 1));
-    expect($(list[4]).text()).to.be($('<div/>').html("&raquo;").text());
+      expect($(list[i]).text()).to.be("" + (i + 1));
+    expect($(list[3]).text()).to.be($('<div/>').html("&raquo;").text());
   });
 
   it('0 records per page', function() {
