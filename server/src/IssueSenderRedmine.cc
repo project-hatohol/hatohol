a0e2d175a7c878e575dfc504ad353e98fde82667
Takuro Ashie <ashie@clear-code.com>
2014-05-19 15:18:52 +0900

    [IssueSenderRedmine] Add the simplest code to post an issue

--- server/src/IssueSenderRedmine.cc
+++ server/src/IssueSenderRedmine.cc
@@ -19,9 +19,23 @@
 
 #include "IssueSenderRedmine.h"
 #include "JsonBuilderAgent.h"
+#include <libsoup/soup.h>
 
 using namespace std;
 using namespace mlpl;
+
+static const guint DEFAULT_TIMEOUT_SECONDS = 60;
+static const char *MIME_JSON = "application/json";
+
+static SoupSession *getSoupSession(void)
+{
+	static SoupSession *session = NULL;
+	if (!session)
+		session = soup_session_sync_new_with_options(
+			    SOUP_SESSION_TIMEOUT, DEFAULT_TIMEOUT_SECONDS,
+			    NULL);
+	return session;
+}
 
 struct IssueSenderRedmine::PrivateContext
 {
@@ -36,6 +50,20 @@ IssueSenderRedmine::IssueSenderRedmine(const IssueTrackerInfo &tracker)
 IssueSenderRedmine::~IssueSenderRedmine()
 {
 	delete m_ctx;
+}
+
+static string getPostURL(const IssueTrackerInfo &trackerInfo)
+{
+	string url = trackerInfo.baseURL;
+	url += "/projects/";
+	gchar *escapedProjectId =
+	  g_uri_escape_string(trackerInfo.baseURL.c_str(),
+			      NULL, false);
+	if (escapedProjectId)
+		url += escapedProjectId;
+	g_free(escapedProjectId);
+	url += "/issues.json";
+	return url;
 }
 
 string IssueSenderRedmine::buildJson(const EventInfo &event)
@@ -60,6 +88,17 @@ string IssueSenderRedmine::buildJson(const EventInfo &event)
 
 HatoholError IssueSenderRedmine::send(const EventInfo &event)
 {
+	const IssueTrackerInfo &trackerInfo = getIssueTrackerInfo();
+	string url = getPostURL(trackerInfo);
+
+	SoupMessage *msg = soup_message_new(SOUP_METHOD_POST, url.c_str());
+	soup_message_headers_set_content_type(msg->request_headers,
+	                                      MIME_JSON, NULL);
 	string json = buildJson(event);
+	soup_message_body_append(msg->request_body, SOUP_MEMORY_TEMPORARY,
+	                         json.c_str(), json.size());
+	guint sendResult = soup_session_send_message(getSoupSession(), msg);
+	g_object_unref(msg);
+
 	return HTERR_NOT_IMPLEMENTED;
 }
