fc7f9c689d4ea97c6ff638e9ef77ceb378f28f31
Takuro Ashie <ashie@clear-code.com>
2014-06-04 13:26:00 +0900

    [IssueSenderManager] Add code for creating IssueSender threads
    This code doesn't work yet because a required function is lacked
    in IssueTrackersQueryOption (setTargetId)

--- server/src/IssueSenderManager.cc
+++ server/src/IssueSenderManager.cc
@@ -17,10 +17,12 @@
  * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <queue>
+#include <map>
 #include "MutexLock.h"
+#include "Reaper.h"
 #include "IssueSenderManager.h"
-#include "HatoholThreadBase.h"
+#include "IssueSenderRedmine.h"
+#include "CacheServiceDBClient.h"
 
 using namespace std;
 using namespace mlpl;
@@ -28,9 +30,66 @@ using namespace mlpl;
 struct IssueSenderManager::PrivateContext
 {
 	static IssueSenderManager instance;
+	map<IssueTrackerIdType, IssueSender*> sendersMap;
+	MutexLock sendersLock;
 
 	~PrivateContext()
 	{
+		sendersLock.lock();
+		Reaper<MutexLock> unlocker(&sendersLock, MutexLock::unlock);
+		map<IssueTrackerIdType, IssueSender*>::iterator it;
+		for (it = sendersMap.begin(); it != sendersMap.end(); it++) {
+			IssueSender *sender = it->second;
+			delete sender;
+		}
+		sendersMap.clear();
+	}
+
+	IssueSender *createIssueSender(const IssueTrackerIdType &id)
+	{
+		CacheServiceDBClient cache;
+		DBClientConfig *dbConfig = cache.getConfig();
+		IssueTrackerInfoVect issueTrackerVect;
+		IssueTrackerQueryOption option(USER_ID_SYSTEM);
+		//TODO: not implemented yet
+		//option.setTragetId(id);
+		dbConfig->getIssueTrackers(issueTrackerVect, option);
+
+		if (issueTrackerVect.size() <= 0) {
+		MLPL_ERR("Not found IssueTrackerInfo: %d\n", id);
+			return NULL;
+		}
+		if (issueTrackerVect.size() > 1) {
+			MLPL_ERR("Too many IssueTrackerInfo for ID:%d\n", id);
+			return NULL;
+		}
+
+		IssueTrackerInfo &tracker = *issueTrackerVect.begin();
+		IssueSender *sender = NULL;
+		switch (tracker.type) {
+		case ISSUE_TRACKER_REDMINE:
+			sender = new IssueSenderRedmine(tracker);
+			break;
+		default:
+			MLPL_ERR("Invalid IssueTracker type: %d\n",
+				 tracker.type);
+			break;
+		}
+		return sender;
+	}
+
+	IssueSender *getSender(const IssueTrackerIdType &id)
+	{
+		sendersLock.lock();
+		Reaper<MutexLock> unlocker(&sendersLock, MutexLock::unlock);
+		if (sendersMap.find(id) != sendersMap.end() && sendersMap[id])
+			return sendersMap[id];
+
+		IssueSender *sender = createIssueSender(id);
+		if (sender)
+			sendersMap[id] = sender;
+
+		return sender;
 	}
 };
 
@@ -44,6 +103,14 @@ IssueSenderManager &IssueSenderManager::getInstance(void)
 void IssueSenderManager::queue(
   const IssueTrackerIdType &trackerId, const EventInfo &eventInfo)
 {
+	IssueSender *sender = m_ctx->getSender(trackerId);
+	if (!sender) {
+		MLPL_ERR("Failed to queue sending an issue"
+			 " for the event: %" FMT_EVENT_ID,
+			 eventInfo.id);
+		return;
+	}
+	sender->queue(eventInfo);
 }
 
 IssueSenderManager::IssueSenderManager(void)
