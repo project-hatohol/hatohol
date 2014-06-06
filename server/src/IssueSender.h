fe3b65676998b666eb40f45ab9c3c5a0f112d1d5
Takuro Ashie <ashie@clear-code.com>
2014-06-06 13:48:06 +0900

    [IssueSender] Add descriptions about API

--- server/src/IssueSender.h
+++ server/src/IssueSender.h
@@ -46,12 +46,65 @@ public:
 
 	virtual void waitExit(void) override;
 
+	/**
+	 * Send an EventInfo as an issue to the specified issue tracking system
+	 * synchronously. It will be done by the same thread with the caller.
+	 * You don't need start the thread by start() when you use this
+	 * function directly.
+	 *
+	 * @param event
+	 * An EventInfo to send as an issue.
+	 *
+	 * @return HTERR_OK on succeeded to send. Otherwise an error.
+	 */
 	virtual HatoholError send(const EventInfo &event) = 0;
+
+	/**
+	 * Queue an EventInfo to send it as an issue to the specified issue
+	 * tracking system asynchronously. Sending an issue will be done by a
+	 * dedicated thread (it will call send() internally). You must start
+	 * the thread by calling start() before or after calling this function.
+	 *
+	 * @param event
+	 * An EventInfo to send as an issue.
+	 *
+	 * @param callback
+	 * A callback function which will be called when the status of
+	 * sending job is changed.
+	 *
+	 * @param userData
+	 * A user data which will be passed to the callback function.
+	 *
+	 * @return HTERR_OK on succeeded to send. Otherwise an error.
+	 */
 	void queue(const EventInfo &eventInfo,
 		   StatusCallback callback = NULL,
 		   void *userData = NULL);
+
+	/**
+	 * Set max retry count of sending an issue on failing it.
+	 * It affects only to queue(). send() function never retry to send
+	 * automatically.
+	 *
+	 * @param limit
+	 * A max count of retrying to send an issue.
+	 */
 	void setRetryLimit(const size_t &limit);
+
+	/**
+	 * Set the interval time of retrying to send an issue
+	 *
+	 * @param msec
+	 * Interval time of retrying to send an issue [msec].
+	 */
 	void setRetryInterval(const unsigned int &msec);
+
+	/**
+	 * Check whether all queued sending jobs are finished or not.
+	 *
+	 * @return
+	 * true when there is no running job and no queued job, otherwise false.
+	 */
 	bool isIdling(void);
 
 protected:
