36e062d12c157ee76c60f716767a911b8b9f41ef
Kazuhiro Yamato <kz0817@gmail.com>
2014-06-08 08:39:09 +0900

    [server][HapZabbixAPI] Add a stub class.

---
+++ server/src/HapZabbixAPI.cc
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2014 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "HapZabbixAPI.h"
+#include "ItemTablePtr.h"
+
+using namespace mlpl;
+
+struct HapZabbixAPI::PrivateContext {
+	PrivateContext(void)
+	{
+	}
+};
+
+// ---------------------------------------------------------------------------
+// Public methods
+// ---------------------------------------------------------------------------
+HapZabbixAPI::HapZabbixAPI(const MonitoringServerInfo &serverInfo)
+: ZabbixAPI(serverInfo),
+  m_ctx(NULL)
+{
+	m_ctx = new PrivateContext();
+}
+
+HapZabbixAPI::~HapZabbixAPI()
+{
+	if (m_ctx)
+		delete m_ctx;
+}
+
+// ---------------------------------------------------------------------------
+// Protected methods
+// ---------------------------------------------------------------------------
+void HapZabbixAPI::workOnTriggers(void)
+{
+	SmartTime lastTriggerTime = getTimestampOfLastTrigger();
+	// TODO: getTrigger() should accept SmartTime directly.
+	const int requestSince = lastTriggerTime.getAsTimespec().tv_sec;
+	ItemTablePtr tablePtr = getTrigger(requestSince);
+
+	MLPL_BUG("Not implemented: %s\n", __PRETTY_FUNCTION__);
+}
