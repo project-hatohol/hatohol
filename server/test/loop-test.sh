3e2946756731ab33731ac9de9aeb8b649ccac935
Kazuhiro Yamato <kz0817@gmail.com>
2014-06-20 08:08:14 +0900

    [test/loop-test.sh] Clean up.

--- server/test/loop-test.sh
+++ server/test/loop-test.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-  MLPL_LOGGER_LEVEL=ERR HATOHOL_DB_DIR=/dev/shm HATOHOL_MYSQL_ENGINE_MEMORY=1 ./run-test.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
+  MLPL_LOGGER_LEVEL=ERR HATOHOL_DB_DIR=/dev/shm HATOHOL_MYSQL_ENGINE_MEMORY=1 ./run-test.sh "$@"
   if [ $? -ne 0 ]; then
     exit 1
   fi
