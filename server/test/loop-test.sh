ff279602d7469ed331f326f353778dbdd3a2ddc0
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2014-06-19 09:02:12 +0900

    [test/loop-test.sh] Pass more options.

--- server/test/loop-test.sh
+++ server/test/loop-test.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-option=$1
-
 while :
 do
-  MLPL_LOGGER_LEVEL=ERR HATOHOL_DB_DIR=/dev/shm HATOHOL_MYSQL_ENGINE_MEMORY=1 ./run-test.sh $option
+  MLPL_LOGGER_LEVEL=ERR HATOHOL_DB_DIR=/dev/shm HATOHOL_MYSQL_ENGINE_MEMORY=1 ./run-test.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
   if [ $? -ne 0 ]; then
     exit 1
   fi
