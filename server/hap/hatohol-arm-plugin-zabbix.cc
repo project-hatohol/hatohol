02bde2dc28d55c7e2cd70736fc51af26e603bd31
Kazuhiro Yamato <kz0817@gmail.com>
2014-06-20 13:27:26 +0900

    [hatohol-arm-plugin-zabbix] Add a basic structure to take data.

--- server/hap/hatohol-arm-plugin-zabbix.cc
+++ server/hap/hatohol-arm-plugin-zabbix.cc
@@ -33,6 +33,17 @@ protected:
 
 	bool initMonitoringServerInfo(void);
 
+	/**
+	 * Sleep for a specified time.
+	 *
+	 * @param sleepTimeInSec A sleep time.
+	 *
+	 * @return true if exit is requsted. Otherwise false is returned.
+	 */
+	bool sleepForMainThread(const int &sleepTimeInSec);
+
+	void acquireData(void);
+
 private:
 	struct PrivateContext;
 	PrivateContext *m_ctx;;
@@ -74,7 +85,26 @@ gpointer HapProcessZabbixAPI::hapMainThread(HatoholThreadArg *arg)
 {
 	if (!initMonitoringServerInfo())
 		return NULL;
+
+	bool shouldExit = false;
+	while (shouldExit) {
+		acquireData();
+		shouldExit =
+		  sleepForMainThread(m_ctx->serverInfo.pollingIntervalSec);
+	}
 	return NULL;
+}
+
+bool HapProcessZabbixAPI::sleepForMainThread(const int &sleepTimeInSec)
+{
+	SimpleSemaphore::Status status =
+	  m_ctx->mainThreadSem.timedWait(sleepTimeInSec * 1000);
+	// TODO: Add a mechanism to exit
+	if (status == SimpleSemaphore::STAT_OK ||
+	    status == SimpleSemaphore::STAT_ERROR_UNKNOWN) {
+		HATOHOL_ASSERT(true, "Unexpected result: %d\n", status);
+	}
+	return false;
 }
 
 bool HapProcessZabbixAPI::initMonitoringServerInfo(void)
@@ -96,6 +126,11 @@ bool HapProcessZabbixAPI::initMonitoringServerInfo(void)
 	}
 	setExceptionSleepTime(m_ctx->serverInfo.retryIntervalSec*1000);
 	return true;
+}
+
+void HapProcessZabbixAPI::acquireData(void)
+{
+	MLPL_BUG("Not implemented yet: %s\n", __PRETTY_FUNCTION__);
 }
 
 // ---------------------------------------------------------------------------
