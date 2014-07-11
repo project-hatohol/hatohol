18476dc3848a9a9b375d8b1938866c39fac9d879
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-07-11 10:26:40 +0900

    [HatoholDBBackupTool] Store the result to tarball.

--- server/tools/hatohol-db-backup-tool.sh
+++ server/tools/hatohol-db-backup-tool.sh
@@ -3,13 +3,13 @@
 DIR=$1
 
 dumpConfigDB() {
-    mysqldump -u hatohol -phatohol hatohol > ./hatohol.mysql
-    mysqldump -u hatohol -phatohol hatohol_client > ./hatohol_client.mysql
+    mysqldump -u hatohol -phatohol hatohol > ./hatohol_backupdb/hatohol.mysql
+    mysqldump -u hatohol -phatohol hatohol_client > ./hatohol_backupdb/hatohol_client.mysql
 }
 
 copyCacheDB() {
     if [ -e $DIR ]; then
-        cp $DIR/hatohol*.db ./
+        cp $DIR/hatohol*.db ./hatohol_backupdb/
     else
         echo "There is no directory"
     fi
@@ -20,9 +20,16 @@ showUsage() {
     echo "hatohol-db-backup-tool.sh <Path of CacheDB>"
 }
 
+createTarball() {
+    tar zcvf hatohol_backupdb.tar.gz hatohol_backupdb/
+    rm -rf hatohol_backupdb/
+}
+
 if [ $# -ne 1 ]; then
     showUsage
 else
+    mkdir hatohol_backupdb
     dumpConfigDB
     copyCacheDB
+    createTarball
 fi
