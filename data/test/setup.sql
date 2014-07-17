f2ce738ccb9b9a67bd419e2632c99c8f529a030e
Kouhei Sutou <kou@clear-code.com>
2014-07-17 15:56:41 +0900

    [test][setup] Move test_XXX database configuration to setup SQL
    If the configuration is placed in setup SQL set instead of README, we
    don't care about.

--- data/test/setup.sql
+++ data/test/setup.sql
@@ -3,3 +3,5 @@ CREATE DATABASE hatohol_client;
 GRANT ALL PRIVILEGES ON hatohol_client.* TO hatohol@localhost IDENTIFIED BY 'hatohol';
 CREATE DATABASE test_hatohol_client;
 GRANT ALL PRIVILEGES ON test_hatohol_client.* TO hatohol@localhost IDENTIFIED BY 'hatohol';
+GRANT USAGE ON *.* TO ''@'localhost';
+GRANT ALL PRIVILEGES ON `test\_%`.* TO ''@'localhost';
