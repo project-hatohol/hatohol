799443b07dc4d8a4057031ea88dbf631d01eefd7
Kouhei Sutou <kou@clear-code.com>
2014-07-17 13:51:14 +0900

    Split SQL set for testing
    The SQL set is useful to create a local development environment. If the
    SQL set is stored in a file, we don't need to copy & paste from
    .travis.yml. We just run the following command:
    
        % mysql -u root < data/test/setup.sql

---
+++ data/test/setup.sql
@@ -0,0 +1,5 @@
+GRANT all on ndoutils.* TO ndoutils@localhost IDENTIFIED BY 'admin';
+CREATE DATABASE hatohol_client;
+GRANT ALL PRIVILEGES ON hatohol_client.* TO hatohol@localhost IDENTIFIED BY 'hatohol';
+CREATE DATABASE test_hatohol_client;
+GRANT ALL PRIVILEGES ON test_hatohol_client.* TO hatohol@localhost IDENTIFIED BY 'hatohol';
