bd81403b9e77c448379f14ca4e3d65b309333900
Kouhei Sutou <kou@clear-code.com>
2014-07-19 14:30:17 +0900

    [server][test] Untabify

--- server/test/run-test.sh
+++ server/test/run-test.sh
@@ -13,21 +13,21 @@ top_dir="`cd $top_dir; pwd`"
 
 if test x"$NO_MAKE" != x"yes"; then
     if which gmake > /dev/null; then
-	MAKE=${MAKE:-"gmake"}
+        MAKE=${MAKE:-"gmake"}
     else
-	MAKE=${MAKE:-"make"}
+        MAKE=${MAKE:-"make"}
     fi
     MAKE_ARGS=
     case `uname` in
-	Linux)
-	    MAKE_ARGS="-j$(grep '^processor' /proc/cpuinfo | wc -l)"
-	    ;;
-	Darwin)
-	    MAKE_ARGS="-j$(/usr/sbin/sysctl -n hw.ncpu)"
-	    ;;
-	*)
-	    :
-	    ;;
+        Linux)
+            MAKE_ARGS="-j$(grep '^processor' /proc/cpuinfo | wc -l)"
+            ;;
+        Darwin)
+            MAKE_ARGS="-j$(/usr/sbin/sysctl -n hw.ncpu)"
+            ;;
+        *)
+            :
+            ;;
     esac
     $MAKE $MAKE_ARGS -C $top_dir/ > /dev/null || exit 1
 fi
