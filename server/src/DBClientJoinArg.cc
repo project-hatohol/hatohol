942912c7940cdc6072a53b2c9855b7c4521dbf86
Kazuhiro Yamato <kz0817@gmail.com>
2014-07-22 07:20:35 +0900

    [DBClientJoinArg] Add a new class (almost stub).
    This purpose of this class is to replace the class: SelectMultiTableArg
    and provide a way to use JOIN operations with more simple lines.
    In addition, it will offer a mechanism to limited the join only among
    DBClient subclasses with the same DBAgent.

---
+++ server/src/DBClientJoinArg.cc
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2014 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "DBClientJoinArg.h"
+
+struct DBClientJoinArg::PrivateContext {
+	DBAgent::SelectExArg selectExArg;
+
+	PrivateContext(const DBAgent::TableProfile &table)
+	: selectExArg(table)
+	{
+	}
+};
+
+// ---------------------------------------------------------------------------
+// Public methods
+// ---------------------------------------------------------------------------
+DBClientJoinArg::DBClientJoinArg(const DBAgent::TableProfile &table)
+: m_ctx(NULL)
+{
+	m_ctx = new PrivateContext(table);
+}
+
+DBClientJoinArg::~DBClientJoinArg()
+{
+	delete m_ctx;
+}
+
+void DBClientJoinArg::addTable(
+  const DBAgent::TableProfile &table, const JoinType &type,
+  const size_t &index0, const size_t index1)
+{
+	MLPL_BUG("Not implemneted yet: %s\n", __PRETTY_FUNCTION__);
+}
+
+void DBClientJoinArg::add(const size_t &index)
+{
+	MLPL_BUG("Not implemneted yet: %s\n", __PRETTY_FUNCTION__);
+}
+
+const DBAgent::SelectExArg &DBClientJoinArg::getSelectExArg(void)
+{
+	return m_ctx->selectExArg;
+}
---
+++ server/src/DBClientJoinArg.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2013-2014 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DBClientJoin_h
+#define DBClientJoin_h
+
+#include "DBAgent.h"
+
+class DBClientJoinArg {
+public:
+	enum JoinType {
+		INNER_JOIN,
+		LEFT_JOIN,
+		RIGHT_JOIN,
+		NUM_TYPE_JOIN,
+	};
+
+	DBClientJoinArg(const DBAgent::TableProfile &table);
+	virtual ~DBClientJoinArg();
+
+	void addTable(const DBAgent::TableProfile &table, const JoinType &type,
+	              const size_t &index0, const size_t index1);
+	void add(const size_t &index);
+	const DBAgent::SelectExArg &getSelectExArg(void);
+
+private:
+	struct PrivateContext;
+	PrivateContext *m_ctx;
+};
+
+#endif // DBClientJoin_h
