9f705938f41b5692e59570b88b00af7eab0084dc
Kouhei Sutou <kou@clear-code.com>
2014-08-04 10:58:45 +0900

    [Emacs] Add .js configuration

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,6 @@
               (c-basic-offset   . 8)))
  (c++-mode . ((indent-tabs-mode . t)
               (c-basic-offset   . 8)))
+ (js-mode .  ((indent-tabs-mode . nil)
+              (js-indent-level  . 2)))
  (sh-mode .  ((indent-tabs-mode . nil))))
