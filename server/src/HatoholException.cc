c1edafb797803d01033d24dc92082073f37db594
o-wakabayashi <osamu.wakabayashi@miraclelinux.com>
2014-08-11 20:41:59 +0900

    [HatoholException] Add function which throw the exception message including the error code.
    THROW_HATOHOL_EXCEPTION_WITHERR: should be set the "errocode(HatoholErrorCode)" and "messege".
    THROW_HATOHOL_EXCEPTION(Existing function):no change of usage.
    this change is in order to notifying the  cause of error, when occured the disconnection.

--- server/src/HatoholException.cc
+++ server/src/HatoholException.cc
@@ -40,9 +40,15 @@ void HatoholException::init(void)
 
 HatoholException::HatoholException(
   const string &brief, const string &sourceFileName, const int &lineNumber)
+{
+	HatoholException(HTERR_UNINITIALIZED, brief, sourceFileName, lineNumber);
+}
+HatoholException::HatoholException(
+  const HatoholErrorCode errCode, const string &brief, const string &sourceFileName, const int &lineNumber)
 : m_what(brief),
   m_sourceFileName(sourceFileName),
-  m_lineNumber(lineNumber)
+  m_lineNumber(lineNumber),
+  m_errCode(errCode)
 {
 	MLPL_DBG("HatoholException: <%s:%d> %s\n", sourceFileName.c_str(), lineNumber,
 	         brief.c_str());
@@ -93,6 +99,11 @@ int HatoholException::getLineNumber(void) const
 const string &HatoholException::getStackTrace(void) const
 {
 	return m_stackTrace;
+}
+
+HatoholErrorCode HatoholException::getErrCode(void) const
+{
+	return m_errCode;
 }
 
 // ---------------------------------------------------------------------------
