cfbb955e9d5a2fbfce8205c4897effb2b150e032
Kazuhiro Yamato <kazuhiro.yamato@miraclelinux.com>
2014-08-18 08:07:45 +0900

    [DBAgentSQLite3] Fix the problem that doesn't keep the auto incremented value.
    When the record with a unique key is upserted and it's duplicated,
    we want to keep the value in the primary key's column. However,
    the previous implementation with AUTO_INCREMENT_VALUE doesn't do so.

--- server/test/testDBAgentSQLite3.cc
+++ server/test/testDBAgentSQLite3.cc
@@ -460,6 +460,12 @@ void test_upsert(void)
 	dbAgentTestUpsert(dbAgent, dbAgentChecker);
 }
 
+void test_upsertWithPrimaryKeyAutoInc(void)
+{
+	DBAgentSQLite3 dbAgent;
+	dbAgentTestUpsertWithPrimaryKeyAutoInc(dbAgent, dbAgentChecker);
+}
+
 void test_update(void)
 {
 	DBAgentSQLite3 dbAgent;
