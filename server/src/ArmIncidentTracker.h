25d3438b4ebbd929e00ca43c5332ea9cdba51e9e
Takuro Ashie <ashie@clear-code.com>
2014-09-04 18:26:24 +0900

    [server] Add an abstract class "ArmIncidentTrackerInfo"

---
+++ server/src/ArmIncidentTracker.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ArmIncidentTracker_h
+#define ArmIncidentTracker_h
+
+#include "ArmBase.h"
+#include "DBTablesConfig.h"
+
+class ArmIncidentTracker : public ArmBase
+{
+public:
+	ArmIncidentTracker(const IncidentTrackerInfo &trackerInfo);
+	virtual ~ArmIncidentTracker();
+
+protected:
+	const IncidentTrackerInfo &getIncidentTrackerInfo(void);
+
+private:
+	struct Impl;
+	std::unique_ptr<Impl> m_impl;
+};
+
+#endif // ArmIncidentTracker_h
