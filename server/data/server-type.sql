8b29e2c2d8aae27de6b5e58a76ecd1ea0aaf035d
Kazuhiro Yamato <kz0817@gmail.com>
2014-09-16 02:28:30 +0000

    [server][data][server-type.sql] Generate an SQL file for initialization dynamically.

--- server/data/server-type.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM server_types;
-INSERT server_types VALUES (0, 'Zabbix', '[{"name":"Nickname"},{"name":"Host name"},{"name":"IP address"}]');
-INSERT server_types VALUES (1, 'Nagios', '[{"name":"Nickname"}]');
-INSERT server_types VALUES (2, 'Zabbix (HAPI)', '[{"name":"Nickname"},{"name":"Host name"}]');
