136b57710251e26f6ed582c9dc857192174924e3
o-wakabayashi <osamu.wakabayashi@miraclelinux.com>
2014-09-19 17:38:22 +0900

    [HapZabbixAPI] Count the array number by the compiler.

--- server/hap/HapZabbixAPI.cc
+++ server/hap/HapZabbixAPI.cc
@@ -24,13 +24,14 @@ using namespace mlpl;
 
 static const uint64_t NUMBER_OF_GET_EVENT_PER_ONCE  = 1000;
 
-static const int   hapiZabbixErrorNum = 3;
-static const HatoholArmPluginWatchPoint hapiZabbixErrorList[hapiZabbixErrorNum] =
+static const HatoholArmPluginWatchPoint hapiZabbixErrorList[] =
 {
 	COLLECT_NG_PARSER_ERROR,
 	COLLECT_NG_DISCONNECT_ZABBIX,
 	COLLECT_NG_PLGIN_INTERNAL_ERROR,
 };
+
+static const int   hapiZabbixErrorNum = ARRAY_SIZE(hapiZabbixErrorList);
 
 struct HapZabbixAPI::Impl {
 	Impl(void)
