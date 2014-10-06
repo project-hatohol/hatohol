5cc1ff36f757d07f768f5fd9484aec378b094980
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2014-10-06 22:03:38 +0900

    [Server][Test][loop-test] rename env name prefix MLPL_ -> HFL_

--- server/test/loop-test.sh
+++ server/test/loop-test.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-  MLPL_LOGGER_LEVEL=ERR HATOHOL_DB_DIR=/dev/shm HATOHOL_MYSQL_ENGINE_MEMORY=1 ./run-test.sh "$@"
+  HFL_LOGGER_LEVEL=ERR HATOHOL_DB_DIR=/dev/shm HATOHOL_MYSQL_ENGINE_MEMORY=1 ./run-test.sh "$@"
   if [ $? -ne 0 ]; then
     exit 1
   fi
