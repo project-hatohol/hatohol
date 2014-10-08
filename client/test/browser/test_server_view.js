ba4850fc49e437d0569e5d249c333452bc420da2
Takuro Ashie <ashie@clear-code.com>
2014-10-08 15:31:31 +0900

    [test][ServersView] Add tests for visibility of delete button

--- client/test/browser/test_server_view.js
+++ client/test/browser/test_server_view.js
@@ -1,4 +1,81 @@
 describe('ServerView', function() {
+  var TEST_FIXTURE_ID = 'serversViewFixture';
+  var serversViewHTML;
+  var defaultServers = [
+    {
+      "id": 1,
+      "type": hatohol.MONITORING_SYSTEM_ZABBIX,
+      "hostName": "Test Zabbix",
+      "ipAddress": "127.0.0.1",
+      "nickname": "Test Zabbix",
+      "port": 80,
+      "pollinInterval": 30,
+      "retryInterval": 10,
+      "userName": "TestZabbixUser",
+      "passowrd": "zabbix",
+      "dbName": ""
+    }
+  ];
+
+  function serversJson(servers) {
+    return JSON.stringify({
+      apiVersion: 3,
+      errorCode: hatohol.HTERR_OK,
+      servers: servers ? servers : [],
+    });
+  }
+
+  function fakeAjax() {
+    var requests = this.requests = [];
+    this.xhr = sinon.useFakeXMLHttpRequest();
+    this.xhr.onCreate = function(xhr) {
+      requests.push(xhr);
+    };
+  }
+
+  function restoreAjax() {
+    this.xhr.restore();
+  }
+
+  function respond(serversJson) {
+    var request = this.requests[0];
+    request.respond(200, { "Content-Type": "application/json" },
+                    serversJson);
+  }
+
+  function loadFixture(pathFromTop, onLoad) {
+    var iframe = $("<iframe>", {
+      id: "loaderFrame",
+      src: "../../" + pathFromTop + "?start=false",
+      load: function() {
+        var html = $("#main", this.contentDocument).html();
+        onLoad(html);
+        $('#loaderFrame').remove()
+      }
+    })
+    $('body').append(iframe);
+  }
+
+  beforeEach(function(done) {
+    $('body').append($('<div>', { id: TEST_FIXTURE_ID }));
+    var setupFixture = function(html) {
+      serversViewHTML = html;
+      $("#" + TEST_FIXTURE_ID).append($("<div>", { id: "main" }))
+      $("#main").html(serversViewHTML);
+      fakeAjax();
+      done();
+    };
+
+    if (serversViewHTML)
+      setupFixture(serversViewHTML);
+    else
+      loadFixture("ajax_servers", setupFixture)
+  });
+
+  afterEach(function() {
+    restoreAjax();
+    $("#" + TEST_FIXTURE_ID).remove();
+  });
 
   function checkGetStatusLabel(stat, expectMsg, expectMsgClass) {
     var pkt = {serverConnStat:{'5':{status:stat}}};
@@ -115,6 +192,43 @@ describe('ServerView', function() {
       gettext("Number of failure") + ": 5" + "<br>" +
       gettext("Comment for the failure") + ": Bouno";
     expect(html).to.be(expectStr);
+  });
+
+  it('with delete privilege', function() {
+    var operator = {
+      "userId": 1,
+      "name": "admin",
+      "flags": (1 << hatohol.OPPRVLG_GET_ALL_SERVER |
+                1 << hatohol.OPPRVLG_DELETE_SERVER)
+    };
+    var userProfile = new HatoholUserProfile(operator);
+    var view = new ServersView(userProfile);
+    respond(serversJson(defaultServers));
+
+    var deleteButton = $('#delete-server-button');
+    var checkboxes = $('.delete-selector .selectcheckbox');
+    expect(deleteButton).to.have.length(1);
+    expect(checkboxes).to.have.length(1);
+    expect(deleteButton.is(":visible")).to.be(true);
+    expect(checkboxes.is(":visible")).to.be(true);
+  });
+
+  it('with no delete privilege', function() {
+    var operator = {
+      "userId": 2,
+      "name": "guest",
+      "flags": 0
+    };
+    var userProfile = new HatoholUserProfile(operator);
+    var view = new ServersView(userProfile);
+    respond(serversJson(defaultServers));
+
+    var deleteButton = $('#delete-server-button');
+    var checkboxes = $('.delete-selector .selectcheckbox');
+    expect(deleteButton).to.have.length(1);
+    expect(checkboxes).to.have.length(1);
+    expect(deleteButton.is(":visible")).to.be(false);
+    expect(checkboxes.is(":visible")).to.be(false);
   });
 
 });
