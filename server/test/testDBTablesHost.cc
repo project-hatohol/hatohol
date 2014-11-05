1fff12a9247ac0893897658c426d872c9d5ffa15
Kazuhiro Yamato <kz0817@gmail.com>
2014-11-05 11:03:49 +0000

    [testDBTablesHost] Add a test: test_upsertServerHostDefAutoIncrement().

--- server/test/testDBTablesHost.cc
+++ server/test/testDBTablesHost.cc
@@ -91,4 +91,23 @@ void test_upsertServerHostDef(void)
 	cppcut_assert_not_equal((GenericIdType)AUTO_INCREMENT_VALUE, id);
 }
 
+void test_upsertServerHostDefAutoIncrement(void)
+{
+	ServerHostDef svHostDef;
+	svHostDef.id = AUTO_INCREMENT_VALUE;
+	svHostDef.hostId = 12345;
+	svHostDef.serverId = 15;
+	svHostDef.hostIdInServer = "8023455";
+	svHostDef.name = "test host name";
+
+	DECLARE_DBTABLES_HOST(dbHost);
+	GenericIdType id0 = dbHost.upsertServerHostDef(svHostDef);
+	GenericIdType id1 = dbHost.upsertServerHostDef(svHostDef);
+	const string expect = StringUtils::sprintf(
+	  "%" FMT_GEN_ID "|12345|15|8023455|test host name"
+	  "%" FMT_GEN_ID "|12345|15|8023455|test host name", id0, id1);
+	cppcut_assert_not_equal((GenericIdType)AUTO_INCREMENT_VALUE, id0);
+	cppcut_assert_not_equal((GenericIdType)AUTO_INCREMENT_VALUE, id1);
+}
+
 } // namespace testDBTablesHost
