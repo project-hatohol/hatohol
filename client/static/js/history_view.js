fe318311714e4f52d178226cb6c2d5b0ed134e1e
Takuro Ashie <ashie@clear-code.com>
2014-11-10 16:17:47 +0900

    [HistoryView] Load requested item & history

--- client/static/js/history_view.js
+++ client/static/js/history_view.js
@@ -17,8 +17,49 @@
  * along with Hatohol. If not, see <http://www.gnu.org/licenses/>.
  */
 
-var HistoryView = function(userProfile) {
+var HistoryView = function(userProfile, options) {
+  var self = this;
+  var replyItem, replyHistory;
+  var query;
+
+  if (!options)
+    options = {};
+  query = self.parseQuery(options.query);
+
+  load();
+
+  function updateView(reply) {
+    replyHistory = reply;
+    self.displayUpdateTime();
+  }
+
+  function getItemQuery() {
+    return 'item?' + $.param(query);
+  };
+
+  function getHistoryQuery() {
+    return 'history?' + $.param(query);
+  };
+
+  function onItemLoad(reply) {
+    replyItem = reply;
+    self.startConnection(getHistoryQuery(), updateView);
+  }
+
+  function load() {
+    self.startConnection(getItemQuery(), onItemLoad);
+  }
 };
 
 HistoryView.prototype = Object.create(HatoholMonitoringView.prototype);
 HistoryView.prototype.constructor = HistoryView;
+
+HistoryView.prototype.parseQuery = function(query) {
+  var knownKeys = ["serverId", "hostId", "itemId"];
+  var i, allParams = deparam(query), queryTable = {};
+  for (i = 0; i < knownKeys.length; i++) {
+    if (knownKeys[i] in allParams)
+      queryTable[knownKeys[i]] = allParams[knownKeys[i]];
+  }
+  return queryTable;
+};
