37ff26a2f3c8b2e875c8f5842dad1cd8aca85500
Takuro Ashie <ashie@clear-code.com>
2014-11-19 15:23:50 +0900

    [utils.js] Fix a bug that formatMetricPrefix() doesn't remove trailing zero

--- client/test/browser/test_utils.js
+++ client/test/browser/test_utils.js
@@ -541,6 +541,11 @@ describe('formatItemValue', function() {
     var bytes = "" + (2.5189 * 1024 * 1024 * 1024)
     expect(formatItemValue(bytes, 'B')).eql("2.519 GB");
   });
+
+  it('Float with trailing 0', function() {
+    expect(formatItemValue('0.15000', '')).eql("0.15");
+    expect(formatItemValue('0.0015000', '')).eql("0.0015");
+  });
 });
 
 });
