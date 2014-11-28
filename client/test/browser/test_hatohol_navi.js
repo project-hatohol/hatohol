5dcd0d81cde3db857af9c3fbea515a307b0ad97f
Kouhei Sutou <kou@clear-code.com>
2014-11-28 12:36:09 +0900

    [client][test] Remove "log search systems" entry from navi
    Why? I can see it in my Web brower...

--- client/test/browser/test_hatohol_navi.js
+++ client/test/browser/test_hatohol_navi.js
@@ -57,8 +57,6 @@ describe('HatoholNavi', function() {
       gettext('Monitoring Servers') + '</a></li>';
     expected += '<li><a href="ajax_actions">' +
       gettext('Actions') + '</a></li>';
-    expected += '<li><a href="ajax_log_search_systems">' +
-      gettext('Log search systems') + '</a></li>';
     expected += '</ul></li>';
     expected += '<li class="dropdown">';
     expected += '<a href="#" class="dropdown-toggle" data-toggle="dropdown">' +
