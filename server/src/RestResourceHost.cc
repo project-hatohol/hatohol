2d855abd4f42b55f9d30ce6c2c5ec33982017b0f
Hiroshi Hatake <hatake@clear-code.com>
2014-12-08 15:49:29 +0900

    [server][RestResourceHost] make empty string for appName's default value

--- server/src/RestResourceHost.cc
+++ server/src/RestResourceHost.cc
@@ -168,7 +168,7 @@ static HatoholError parseHostResourceQueryParameter(
 	option.setOffset(offset);
 
 	// appName
-	string appName = " ";
+	string appName = "";
 	err = getParam<string>(query, "appName", "%s", appName);
 	if (err != HTERR_OK && err != HTERR_NOT_FOUND_PARAMETER)
 		return err;
