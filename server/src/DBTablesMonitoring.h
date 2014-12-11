44d160a9daa033762ead0f8b3f684bd5a933d0d6
Kazuhiro Yamato <kz0817@gmail.com>
2014-12-11 03:13:35 +0000

    [DBTablesMonitoring] Use DBTablesHost in getHostgroupElementList().

--- server/src/DBTablesMonitoring.h
+++ server/src/DBTablesMonitoring.h
@@ -212,7 +212,8 @@ public:
 	  __attribute__ ((deprecated));
 	HatoholError getHostgroupElementList
 	  (HostgroupElementList &hostgroupElementList,
-	   const HostgroupElementQueryOption &option);
+	   const HostgroupElementQueryOption &option)
+	  __attribute__ ((deprecated));
 
 	void addHostgroupElement(HostgroupElement *mapHostHostgroupsInfo)
 	  __attribute__ ((deprecated));
