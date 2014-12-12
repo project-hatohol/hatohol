1ef9e741161af474cb65e12ca87e79be3469dd83
Kazuhiro Yamato <kz0817@gmail.com>
2014-12-12 09:25:18 +0000

    [DBTablesMonitoring] Fix the problem in which item is read out with the invalid size.

--- server/src/DBTablesMonitoring.cc
+++ server/src/DBTablesMonitoring.cc
@@ -113,6 +113,14 @@ static bool readViaString(int &dest, ItemGroupStream &itemGroupStream)
 	return readViaString<int>(dest, itemGroupStream);
 }
 
+template<typename Type>
+static void readVia(int &dest, ItemGroupStream &itemGroupStream)
+{
+	Type val;
+	itemGroupStream >> val;
+	dest = val;
+}
+
 // ----------------------------------------------------------------------------
 // Table: triggers
 // ----------------------------------------------------------------------------
@@ -2968,7 +2976,7 @@ HatoholError DBTablesMonitoring::getHostgroupInfoList
 		hostgroupInfoList.push_back(HostgroupInfo());
 		HostgroupInfo &hostgroupInfo = hostgroupInfoList.back();
 
-		itemGroupStream >> hostgroupInfo.id;
+		readVia<GenericIdType>(hostgroupInfo.id, itemGroupStream);
 		itemGroupStream >> hostgroupInfo.serverId;
 
 		if (!readViaString(hostgroupInfo.id, itemGroupStream))
@@ -3003,7 +3011,7 @@ HatoholError DBTablesMonitoring::getHostgroupElementList
 		ItemGroupStream itemGroupStream(*itemGrpItr);
 		hostgroupElementList.push_back(HostgroupElement());
 		HostgroupElement &hostgroupElement = hostgroupElementList.back();
-		itemGroupStream >> hostgroupElement.id;
+		readVia<GenericIdType>(hostgroupElement.id, itemGroupStream);
 		itemGroupStream >> hostgroupElement.serverId;
 
 		if (!readViaString(hostgroupElement.hostId, itemGroupStream))
