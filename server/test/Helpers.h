85224dcd7c538a4d0b04b37060a02112680ea110
Kazuhiro Yamato <kz0817@gmail.com>
2015-01-09 09:30:50 +0000

    [Helpers] Extract a feature to make a comparison string as makeElementsComparisonString().

--- server/test/Helpers.h
+++ server/test/Helpers.h
@@ -224,25 +224,34 @@ void _assertGError(const GError *error);
 #define assertGError(E) cut_trace(_assertGError(E))
 
 template<typename T0, typename T1>
-void _assertEqualSize(const T0 &exp, const T1 &act)
+std::string _makeElementsComparisonString(const T0 &exp, const T1 &act)
 {
 	std::stringstream ss;
-	if (exp.size() != act.size()) {
-		ss << "<<expect>>\n";
-		typename T0::const_iterator it0;
-		for (it0 = exp.begin(); it0 != exp.end(); ++it0) {
-			ss << *it0;
-			ss << ",";
-		}
-		typename T1::const_iterator it1;
-		ss << "\n<<actual>>\n";
-		for (it1 = act.begin(); it1 != act.end(); ++it1) {
-			ss << *it1;
-			ss << ",";
-		}
+	ss << "<<expect>>\n";
+	typename T0::const_iterator it0;
+	for (it0 = exp.begin(); it0 != exp.end(); ++it0) {
+		ss << *it0;
+		ss << ",";
 	}
+	typename T1::const_iterator it1;
+	ss << "\n<<actual>>\n";
+	for (it1 = act.begin(); it1 != act.end(); ++it1) {
+		ss << *it1;
+		ss << ",";
+	}
+	return ss.str();
+}
+#define makeElementsComparisonString(E,A) \
+  _makeElementsComparisonString<__typeof__(E), __typeof__(A)>(E,A)
+
+template<typename T0, typename T1>
+void _assertEqualSize(const T0 &exp, const T1 &act)
+{
+	std::string errMsg;
+	if (exp.size() != act.size())
+		errMsg = makeElementsComparisonString(exp, act);
 	cppcut_assert_equal(exp.size(), act.size(),
-	                    cut_message("%s", ss.str().c_str()));
+	                    cut_message("%s", errMsg.c_str()));
 }
 #define assertEqualSize(E, A) \
   cut_trace((_assertEqualSize<__typeof__(E), __typeof__(A)>(E,A)))
