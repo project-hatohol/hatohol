f88b121e6be7933868a7954bbcc721b97a0bef36
o-wakabayashi <osamu.wakabayashi@miraclelinux.com>
2015-01-13 14:30:05 +0900

    [DBTablesMonitoring] use the "addCondition" for avoid duplicate of "AND".

--- server/src/DBTablesMonitoring.cc
+++ server/src/DBTablesMonitoring.cc
@@ -1190,21 +1190,23 @@ string TriggersQueryOption::getCondition(void) const
 		return condition;
 
 	if (m_impl->triggerCollectHost == VALID_HOST_TRIGGER) {
-		condition += " AND ";
-		condition += StringUtils::sprintf(
-			"%s.%s<%" FMT_HOST_ID,
-			DBTablesMonitoring::TABLE_NAME_TRIGGERS,
-			COLUMN_DEF_TRIGGERS[IDX_TRIGGERS_HOST_ID].columnName,
-			MONITORING_SERVER_SELF_ID);
+		addCondition( 
+		  condition,
+		  StringUtils::sprintf(
+		    "%s.%s<%" FMT_HOST_ID,
+		    DBTablesMonitoring::TABLE_NAME_TRIGGERS,
+		    COLUMN_DEF_TRIGGERS[IDX_TRIGGERS_HOST_ID].columnName,
+		    MONITORING_SERVER_SELF_ID));
 	}
 
 	if (m_impl->triggerCollectHost >= REAL_HOST_TRIGGER) {
-		condition += " AND ";
-		condition += StringUtils::sprintf(
-			"%s.%s!=%d",
-			DBTablesMonitoring::TABLE_NAME_HOSTS,
-			COLUMN_DEF_HOSTS[IDX_HOSTS_VALIDITY].columnName,
-			HOST_INVALID);
+		addCondition(
+		  condition,
+		  StringUtils::sprintf(
+		    "%s.%s!=%d",
+		    DBTablesMonitoring::TABLE_NAME_HOSTS,
+		    COLUMN_DEF_HOSTS[IDX_HOSTS_VALIDITY].columnName,
+		    HOST_INVALID));
 	}
 
 	if (m_impl->targetId != ALL_TRIGGERS) {
