92934f202dfdcdbb752125e022d0351a03dd28c7
Kazuhiro Yamato <kz0817@gmail.com>
2015-01-15 09:06:57 +0000

    [testArmZabbixAPI] Remove deprecated methods in test_oneProcWithoutFetchItems().

--- server/test/testArmZabbixAPI.cc
+++ server/test/testArmZabbixAPI.cc
@@ -36,6 +36,7 @@
 #include "DBTablesAction.h"
 #include "ThreadLocalDBCache.h"
 #include "DBTablesTest.h"
+#include "UnifiedDataStore.h"
 using namespace std;
 
 namespace testArmZabbixAPI {
@@ -567,8 +568,8 @@ void test_oneProcWithoutFetchItems()
 	EventInfoList eventInfoList;
 	TriggerInfoList triggerInfoList;
 	ItemInfoList itemInfoList;
-	HostgroupInfoList hostgroupInfoList;
-	HostgroupElementList hostgroupElementList;
+	HostgroupVect hostgrps;
+	HostgroupMemberVect hostgrpMembers;
 
 	EventsQueryOption eventsQueryOption(USER_ID_SYSTEM);
 	TriggersQueryOption triggersQueryOption(USER_ID_SYSTEM);
@@ -579,14 +580,17 @@ void test_oneProcWithoutFetchItems()
 	dbMonitoring.getEventInfoList(eventInfoList, eventsQueryOption);
 	dbMonitoring.getTriggerInfoList(triggerInfoList, triggersQueryOption);
 	dbMonitoring.getItemInfoList(itemInfoList, itemsQueryOption);
-	dbMonitoring.getHostgroupInfoList(hostgroupInfoList,
-	                                  hostgroupsQueryOption);
-	dbMonitoring.getHostgroupElementList(hostgroupElementList,
-	                                     hostgroupElementQueryOption);
+
+	UnifiedDataStore *uds = UnifiedDataStore::getInstance();
+	assertHatoholError(
+	  HTERR_OK, uds->getHostgroups(hostgrps, hostgroupsQueryOption));
+	assertHatoholError(
+	  HTERR_OK,
+	  uds->getHostgroupMembers(hostgrpMembers, hostgroupElementQueryOption));
 
 	// FIXME: should check contents
-	cppcut_assert_equal(false, hostgroupInfoList.empty());
-	cppcut_assert_equal(false, hostgroupElementList.empty());
+	cppcut_assert_equal(false, hostgrps.empty());
+	cppcut_assert_equal(false, hostgrpMembers.empty());
 
 	cppcut_assert_equal(false, eventInfoList.empty());
 	cppcut_assert_equal(false, triggerInfoList.empty());
