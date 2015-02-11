88b9bc2ce4aa440434789a22185257387b12357c
Kazuhiro Yamato <kz0817@gmail.com>
2015-02-11 07:45:21 +0000

    [testFaceRestNoInit] Fix a wrong expected value.

--- server/test/testFaceRestNoInit.cc
+++ server/test/testFaceRestNoInit.cc
@@ -400,7 +400,7 @@ void test_parseEventParameterTargetHostId(void)
 void test_parseEventParameterInvalidTargetHostId(void)
 {
 	assertParseEventParameterTargetHostId(
-	  "", "hostid", HTERR_INVALID_PARAMETER);
+	  "", "", HTERR_INVALID_PARAMETER);
 }
 
 void test_parseEventParameterNoTargetHostgroupId(void)
