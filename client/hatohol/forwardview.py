1347653c42e1a04b7deb9afbcb4eea8a5dc0b7ac
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-02-11 20:10:26 +0900

    [client] Set "Expires" and "Pragma" into Python code

--- client/hatohol/forwardview.py
+++ client/hatohol/forwardview.py
@@ -58,5 +58,7 @@ def jsonforward(request, path):
 
     response = HttpResponse(content, content_type='application/json')
 
+    response['Pragma'] = 'no-cache'
     response['Cache-Control'] = 'no-cache'
+    response['Expires'] = '0'
     return response
