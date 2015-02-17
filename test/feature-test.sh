40b5c61d6a3dad99bde2429b880858b665161e84
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-02-18 01:46:34 +0900

    [travis] rearrange feature test preparation
    In ./test/launch-hatohol-for-test.sh, hatohol database is
    unexpenctedly droppped.... :cry:
    So, it needs to insert initial data after
    ./test/launch-hatohol-for-test.sh ran.

--- test/feature-test.sh
+++ test/feature-test.sh
@@ -1,7 +1,6 @@
 #/bin/sh
 sudo make install
-LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH hatohol-db-initiator --db_user root --db_password ''
-sleep 5
-mysql -uroot hatohol -e 'select * from users;'
-./test/launch-hatohol-for-test.sh && casperjs test client/test/feature/*.js
+./test/launch-hatohol-for-test.sh && \
+    LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH hatohol-db-initiator --db_user root --db_password '' && \
+    casperjs test client/test/feature/*.js
 mysql -uroot hatohol -e 'select * from users;'
