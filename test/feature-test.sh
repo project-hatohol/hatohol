9c9f14c229a6f63bba013c75166610e6cba146c8
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-02-17 23:37:52 +0900

    [travis] Add debug output for users table

--- test/feature-test.sh
+++ test/feature-test.sh
@@ -2,4 +2,5 @@
 sudo make install
 LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH hatohol-db-initiator --db_user root --db_password ''
 sleep 5
+mysql -uroot hatohol -e 'select * from users;'
 ./test/launch-hatohol-for-test.sh && casperjs test client/test/feature/*.js
