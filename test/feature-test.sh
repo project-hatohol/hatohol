d4fbdec083b6266ad9d473db865e296b2a1a2455
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-02-18 00:51:52 +0900

    [travis] more users table debug log

--- test/feature-test.sh
+++ test/feature-test.sh
@@ -4,3 +4,4 @@ LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH hatohol-db-initiator --db_user r
 sleep 5
 mysql -uroot hatohol -e 'select * from users;'
 ./test/launch-hatohol-for-test.sh && casperjs test client/test/feature/*.js
+mysql -uroot hatohol -e 'select * from users;'
