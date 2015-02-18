abdb40840fa718e762f20d6e10e49f7e28323c6a
Hiroshi Hatake <hatake@clear-code.com>
2015-02-18 10:00:12 +0900

    [travis] Remove debug print

--- test/feature-test.sh
+++ test/feature-test.sh
@@ -3,4 +3,3 @@ sudo make install
 ./test/launch-hatohol-for-test.sh && \
     LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH hatohol-db-initiator --db_user root --db_password '' && \
     casperjs test client/test/feature/*.js
-mysql -uroot hatohol -e 'select * from users;'
