125a6cd1fb61b2b5568ecacdf4c92384ce7ce010
o-wakabayashi <osamu.wakabayashi@miraclelinux.com>
2015-02-18 21:15:19 +0900

    [client][hatohol_item_update] Add the parts that require the update (PUT).

---
+++ client/static/js/hatohol_item_update.js
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2013 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License, version 3
+ * as published by the Free Software Foundation.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Hatohol. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+// ---------------------------------------------------------------------------
+// HatoholItemRemover
+// ---------------------------------------------------------------------------
+var HatoholItemUpdate = function(updateParameters, connParam) {
+  //
+  // updateParameters has following parameters.
+  //
+  // * id: Please enumerate an array of ID to update.
+  // * type
+  // TODO: Add the description.
+  //
+  var count = 0;
+  var total = 0;
+  var errors = 0;
+  var msg = "";
+  for (var i = 0; i < updateParameters.id.length; i++) {
+    count++;
+    total++;
+    new HatoholConnector($.extend({
+      url: '/' + updateParameters.type + '/' + updateParameters.id[i],
+        request: "POST",
+        context: updateParameters.id[i],
+        replyCallback: function(reply, parser, context) {
+        },
+        parseErrorCallback: function(reply, parser) {
+          // TODO: Add line break.
+          msg += "errorCode: " + reply.errorCode + ". ";
+          hatoholErrorMsgBox(msg);
+          errors++;
+        },
+        connectErrorCallback: function(XMLHttpRequest,
+                                textStatus, errorThrown) {
+          // TODO: Add line break.
+          msg += "Error: " + XMLHttpRequest.status + ": " +
+          XMLHttpRequest.statusText + ". ";
+          hatoholErrorMsgBox(msg);
+          errors++;
+        },
+        completionCallback: function(context) {
+          compleOneDel();
+        }
+    }, connParam || {}));
+  }
+
+  function compleOneDel() {
+    count--;
+    var completed = total - count;
+    hatoholErrorMsgBox(msg + gettext("Deleting...") + " " + completed +
+                       " / " + total);
+    if (count > 0)
+      return;
+
+    // close dialogs
+    hatoholInfoMsgBox(msg + gettext("Completed. (Number of errors: ") +
+                      errors + "/" + total + ")");
+
+    if (updateParameters.completionCallback)
+      updateParameters.completionCallback();
+  }
+}
