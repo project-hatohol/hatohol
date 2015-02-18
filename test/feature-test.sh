e71ef04e7a7f3c69f76b0c147d098a29db538d58
Hiroshi Hatake <hatake@clear-code.com>
2015-02-18 16:55:18 +0900

    [travis][feature][client] Try to use `set -e` instead of `&&`

--- test/feature-test.sh
+++ test/feature-test.sh
@@ -1,5 +1,6 @@
 #/bin/sh
 sudo make install
-./test/launch-hatohol-for-test.sh && \
-    LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH hatohol-db-initiator --db_user root --db_password '' && \
-    LANG=ja_JP.utf8 LC_ALL="ja_JP.UTF-8" casperjs test client/test/feature/*_test.js
+set -e
+./test/launch-hatohol-for-test.sh
+LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH hatohol-db-initiator --db_user root --db_password ''
+LANG=ja_JP.utf8 LC_ALL="ja_JP.UTF-8" casperjs test client/test/feature/*_test.js
