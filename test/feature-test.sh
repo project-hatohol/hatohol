a2280a9728cb7233478634d1acbea2bc1067c05e
Hiroshi Hatake <hatake@clear-code.com>
2015-02-18 13:21:53 +0900

    [client][test][feature] Add missing LANG environment variable

--- test/feature-test.sh
+++ test/feature-test.sh
@@ -2,4 +2,4 @@
 sudo make install
 ./test/launch-hatohol-for-test.sh && \
     LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH hatohol-db-initiator --db_user root --db_password '' && \
-    casperjs test client/test/feature/*_test.js
+    LANG=ja_JP.utf8 casperjs test client/test/feature/*_test.js
