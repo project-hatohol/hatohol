ca0fd4952cc598ad1b79abed278b8868f3b0ab85
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2015-02-19 16:34:02 +0900

    [client][event_view] Delete garbage code.

--- client/static/js/events_view.js
+++ client/static/js/events_view.js
@@ -126,10 +126,8 @@ var EventsView = function(userProfile, baseElem) {
 
   function getQuery(page) {
     if (!page) {
-      self.currentPage = 0;
       self.limitOfUnifiedId = 0;
     } else {
-      self.currentPage = page;
       if (!self.limitOfUnifiedId)
         self.limitOfUnifiedId = self.rawData.lastUnifiedEventId;
     }
