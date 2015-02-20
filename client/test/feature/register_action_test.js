f224d654f79c5e66338740ffb268d668ed49a578
Hiroshi Hatake <hatake@clear-code.com>
2015-02-20 10:55:56 +0900

    [client][test][feature] Fold long lines

--- client/test/feature/register_action_test.js
+++ client/test/feature/register_action_test.js
@@ -63,10 +63,13 @@ casper.test.begin('Register/Unregister action test', function(test) {
   casper.waitForSelector(".ui-dialog-buttonset > button",
     function success() {
       test.assertExists(".ui-dialog-buttonset > button",
-                        "Confirmation dialog button appeared when registering target action.");
+                        "Confirmation dialog button appeared when \
+                        registering target action.");
       this.evaluate(function() {
-        $("#server-selector").find("table#selectorMainTable").find("tr").last().click().change();
-        $("div.ui-dialog-buttonset").attr("area-described-by","server-selector").last().find("button").click().change();
+        $("#server-selector").find("table#selectorMainTable").find("tr").last()
+          .click().change();
+        $("div.ui-dialog-buttonset").attr("area-described-by","server-selector")
+          .last().find("button").click().change();
       });
     },
     function fail() {
@@ -82,7 +85,8 @@ casper.test.begin('Register/Unregister action test', function(test) {
   casper.waitForSelector(".ui-dialog-buttonset > button",
     function success() {
       test.assertExists(".ui-dialog-buttonset > button",
-                        "Confirmation dialog button appeared when registering action.");
+                        "Confirmation dialog button appeared when \
+                        registering action.");
       this.click(".ui-dialog-buttonset > button");
     },
     function fail() {
