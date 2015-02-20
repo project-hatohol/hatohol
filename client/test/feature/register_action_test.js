69bb0b00c679f97b2c107bb726463aad9cf833aa
Hiroshi Hatake <hatake@clear-code.com>
2015-02-20 10:52:43 +0900

    [client][test][feature] Add register action test

--- client/test/feature/register_action_test.js
+++ client/test/feature/register_action_test.js
@@ -39,6 +39,96 @@ casper.test.begin('Register/Unregister action test', function(test) {
       test.assertUrlMatch(/.*ajax_actions/, "Move into actions page.");
     });
   });
+
+  // create actions
+  casper.waitForSelector("form button#add-action-button",
+    function success() {
+      test.assertExists("form button#add-action-button",
+                        "Found add action button item");
+      this.click("form button#add-action-button");
+    },
+    function fail() {
+      test.assertExists("form button#add-action-button");
+    });
+  casper.then(function() {
+    casper.evaluate(function() {
+      $("#selectServerId").val("SELECT").change();
+    });
+  });
+  casper.then(function() {
+    this.evaluate(function() {
+      $("#selectorMainTable").find("tr").last().click().change();
+    });
+  });
+  casper.waitForSelector(".ui-dialog-buttonset > button",
+    function success() {
+      test.assertExists(".ui-dialog-buttonset > button",
+                        "Confirmation dialog button appeared when registering target action.");
+      this.evaluate(function() {
+        $("#server-selector").find("table#selectorMainTable").find("tr").last().click().change();
+        $("div.ui-dialog-buttonset").attr("area-described-by","server-selector").last().find("button").click().change();
+      });
+    },
+    function fail() {
+      test.assertExists(".ui-dialog-buttonset > button");
+    });
+  casper.waitForSelector("input#inputActionCommand",
+    function success() {
+      this.sendKeys("input#inputActionCommand", "getlog");
+    },
+    function fail() {
+      test.assertExists("input#inputActionCommand");
+    });
+  casper.waitForSelector(".ui-dialog-buttonset > button",
+    function success() {
+      test.assertExists(".ui-dialog-buttonset > button",
+                        "Confirmation dialog button appeared when registering action.");
+      this.click(".ui-dialog-buttonset > button");
+    },
+    function fail() {
+      test.assertExists(".ui-dialog-buttonset > button");
+    });
+
+  // check delete-selector check box in minitoring server
+  casper.then(function() {
+    casper.wait(200, function() {
+      this.evaluate(function() {
+        $("tr:last").find(".selectcheckbox").click();
+        return true;
+      });
+    });
+  });
+
+  casper.waitForSelector("form button#delete-action-button",
+    function success() {
+      test.assertExists("form button#delete-action-button",
+                        "Found delete action button.");
+      this.click("form button#delete-action-button");
+    },
+    function fail() {
+      test.assertExists("form button#delete-action-button");
+    });
+
+  // close delete status dialog
+  casper.waitForSelector("div.ui-dialog-buttonset button",
+    function success() {
+      test.assertExists("div.ui-dialog-buttonset button",
+                        "Confirmation dialog appeared.");
+      this.evaluate(function() {
+        $("div.ui-dialog-buttonset button").next().click();
+      });
+    },
+    function fail() {
+      test.assertExists("form button#delete-server-button");
+    });
+  // close result confirmation dialog
+  casper.waitForSelector("div.ui-dialog-buttonset > button",
+    function success() {
+      this.click("div.ui-dialog-buttonset > button");
+    },
+    function fail() {
+      test.assertExists("div.ui-dialog-buttonset > button");
+    });
   casper.then(function() {
     util.moveServersPage(test);
     util.unregisterMonitoringServer();
