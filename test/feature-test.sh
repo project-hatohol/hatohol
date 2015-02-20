c3a98616c97f942c76a2df8f402928dbdc28f6b6
Hiroshi Hatake <hatake@clear-code.com>
2015-02-20 16:45:13 +0900

    [client][test][feature] Use `run-test.sh` form
    run_test.sh ->
    run-test.sh
       ^

--- test/feature-test.sh
+++ test/feature-test.sh
@@ -3,4 +3,4 @@ sudo make install
 set -e
 ./test/launch-hatohol-for-test.sh
 LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH hatohol-db-initiator --db_user root --db_password ''
-LANG=ja_JP.utf8 LC_ALL=ja_JP.UTF-8 ./client/test/feature/run_test.sh
+LANG=ja_JP.utf8 LC_ALL=ja_JP.UTF-8 ./client/test/feature/run-test.sh
