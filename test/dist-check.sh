56ee423ca400d8a0a92eb82031874b36b62b61ce
Takuro Ashie <ashie@clear-code.com>
2015-02-23 10:52:48 +0900

    [travis] Don't use the make option "-j" for distcheck

--- test/dist-check.sh
+++ test/dist-check.sh
@@ -7,12 +7,4 @@ else
     MAKE=${MAKE:-"make"}
 fi
 MAKE_ARGS=
-case `uname` in
-    Linux)
-        MAKE_ARGS="-j$(grep '^processor' /proc/cpuinfo | wc -l)"
-        ;;
-    *)
-        :
-        ;;
-esac
 $MAKE $MAKE_ARGS -C $top_dir/ hatoholdistcheck || exit 1
