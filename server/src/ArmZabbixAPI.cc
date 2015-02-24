5fd5ab619643474ee15cf4f459e4f0618b5e138a
o-wakabayashi <osamu.wakabayashi@miraclelinux.com>
2015-02-24 19:27:05 +0900

    [server][ArmZabbixAPI] modify function name.
    The format of the function name is incorrect.

--- server/src/ArmZabbixAPI.cc
+++ server/src/ArmZabbixAPI.cc
@@ -174,7 +174,7 @@ gpointer ArmZabbixAPI::mainThread(HatoholThreadArg *arg)
 	return ArmBase::mainThread(arg);
 }
 
-void ArmZabbixAPI::makeHatoholALLTriggers(void)
+void ArmZabbixAPI::makeHatoholAllTriggers(void)
 {
 	TriggerInfoList mergedTriggerInfoList;
 	ItemTablePtr triggers, expanded, mergedTriggers;
@@ -301,7 +301,7 @@ ArmBase::ArmPollingResult ArmZabbixAPI::mainThreadOneProc(void)
 			ItemTablePtr triggers = updateTriggers();
 			makeHatoholTriggers(triggers);
 		} else {
-			makeHatoholALLTriggers();
+			makeHatoholAllTriggers();
 		}
 		updateEvents();
 
@@ -353,7 +353,7 @@ ArmBase::ArmPollingResult ArmZabbixAPI::mainThreadOneProcFetchTriggers(void)
 		return COLLECT_NG_DISCONNECT_ZABBIX;
 
 	try {
-		makeHatoholALLTriggers();
+		makeHatoholAllTriggers();
 	} catch (const HatoholException &he) {
 		return handleHatoholException(he);
 	}
