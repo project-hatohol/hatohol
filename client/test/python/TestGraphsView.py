1fc6ee44b94848b0f5ee06b2e76853b88f01768d
Takuro Ashie <ashie@clear-code.com>
2015-03-03 14:28:15 +0900

    [django][TestGraphsView] Fix wrong JSON strings

--- client/test/python/TestGraphsView.py
+++ client/test/python/TestGraphsView.py
@@ -81,7 +81,7 @@ class TestGraphsViewAuthorized(TestGraphsView):
     def test_get(self):
         graph = Graph(
             user_id=5,
-            settings_json='{server_id:1,host_id:2,item_id:3}')
+            settings_json='{"server_id":1,"host_id":2,"item_id":3}')
         graph.save()
         response = self._get(None)
         self.assertEquals(response.status_code, httplib.OK)
@@ -96,7 +96,7 @@ class TestGraphsViewAuthorized(TestGraphsView):
     def test_get_without_own_record(self):
         graph = Graph(
             user_id=4,
-            settings_json='{server_id:1,host_id:2,item_id:3}')
+            settings_json='{"server_id":1,"host_id":2,"item_id":3}')
         graph.save()
         response = self._get(None)
         self.assertEquals(response.status_code, httplib.OK)
