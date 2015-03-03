0a331c00eda68ee2ea93cfa8477d4f866a10556c
Hiroshi Hatake <hatake@clear-code.com>
2015-03-03 17:41:07 +0900

    [client][HatoholAddActionDialog] Add missing semicolons(";")

--- client/static/js/hatohol_add_action_dialog.js
+++ client/static/js/hatohol_add_action_dialog.js
@@ -22,7 +22,7 @@ var HatoholAddActionDialog = function(changedCallback, incidentTrackers, actionD
   var self = this;
 
   var IDX_SELECTED_SERVER  = 0;
-  var IDX_SELECTED_HOST_GROUP = 1
+  var IDX_SELECTED_HOST_GROUP = 1;
   var IDX_SELECTED_HOST    = 2;
   var IDX_SELECTED_TRIGGER = 3;
   self.selectedId = new Array();
@@ -92,7 +92,7 @@ var HatoholAddActionDialog = function(changedCallback, incidentTrackers, actionD
       $("#selectTriggerSeverityCompType").css("visibility","visible");
     else
       $("#selectTriggerSeverityCompType").css("visibility","hidden");
-  })
+  });
 
   $("#selectServerId").change(function() {
     var val = $(this).val();
@@ -100,7 +100,7 @@ var HatoholAddActionDialog = function(changedCallback, incidentTrackers, actionD
       new HatoholServerSelector(serverSelectedCb);
     else
       setSelectedServerId(val);
-  })
+  });
 
   $("#selectHostgroupId").change(function() {
     var val = $(this).val();
@@ -110,7 +110,7 @@ var HatoholAddActionDialog = function(changedCallback, incidentTrackers, actionD
     } else {
       setSelectedHostgroupId(val);
     }
-  })
+  });
 
   $("#selectHostId").change(function() {
     var val = $(this).val();
@@ -121,7 +121,7 @@ var HatoholAddActionDialog = function(changedCallback, incidentTrackers, actionD
     } else {
       setSelectedHostId(val);
     }
-  })
+  });
 
   $("#selectTriggerId").change(function() {
     var val = $(this).val();
@@ -132,7 +132,7 @@ var HatoholAddActionDialog = function(changedCallback, incidentTrackers, actionD
     } else {
       setSelectedTriggerId(val);
     }
-  })
+  });
 
   function serverSelectedCb(serverInfo) {
     var label = "";
@@ -489,7 +489,7 @@ var HatoholAddActionDialog = function(changedCallback, incidentTrackers, actionD
     }
     self.setApplyButtonState(true);
   }
-}
+};
 
 HatoholAddActionDialog.prototype = Object.create(HatoholDialog.prototype);
 HatoholAddActionDialog.prototype.constructor = HatoholAddActionDialog;
@@ -682,86 +682,86 @@ HatoholAddActionDialog.prototype.createMainElement = function() {
 
   function makeTriggerConditionArea() {
     var s = "";
-    s += '<h3>' + gettext("Condition") + '</h3>'
-    s += '<form class="form-inline">'
-    s += '  <label>' + gettext("Server") + '</label>'
-    s += '  <select id="selectServerId">'
-    s += '    <option value="ANY">ANY</option>'
-    s += '    <option value="SELECT">== ' + gettext("SELECT") + ' ==</option>'
-    s += '  </select>'
+    s += '<h3>' + gettext("Condition") + '</h3>';
+    s += '<form class="form-inline">';
+    s += '  <label>' + gettext("Server") + '</label>';
+    s += '  <select id="selectServerId">';
+    s += '    <option value="ANY">ANY</option>';
+    s += '    <option value="SELECT">== ' + gettext("SELECT") + ' ==</option>';
+    s += '  </select>';
 
-    s += '  <label>' + gettext("Hostgroup") + '</label>'
-    s += '  <select id="selectHostgroupId">'
-    s += '    <option value="ANY">ANY</option>'
-    s += '  </select>'
+    s += '  <label>' + gettext("Hostgroup") + '</label>';
+    s += '  <select id="selectHostgroupId">';
+    s += '    <option value="ANY">ANY</option>';
+    s += '  </select>';
 
     if (!self.forIncidentSetting) {
-      s += '  <label>' + gettext("Host") + '</label>'
-      s += '  <select id="selectHostId">'
-      s += '    <option value="ANY">ANY</option>'
-      s += '  </select>'
+      s += '  <label>' + gettext("Host") + '</label>';
+      s += '  <select id="selectHostId">';
+      s += '    <option value="ANY">ANY</option>';
+      s += '  </select>';
 
-      s += '  <label>' + gettext("Trigger") + '</label>'
-      s += '  <select id="selectTriggerId">'
-      s += '    <option value="ANY">ANY</option>'
-      s += '  </select>'
+      s += '  <label>' + gettext("Trigger") + '</label>';
+      s += '  <select id="selectTriggerId">';
+      s += '    <option value="ANY">ANY</option>';
+      s += '  </select>';
     }
-    s += '</form>'
+    s += '</form>';
 
-    s += '<form class="form-inline">'
-    s += '  <label for="selectTriggerStatus">' + gettext("Status") + '</label>'
-    s += '  <select id="selectTriggerStatus">'
-    s += '    <option value="ANY">ANY</option>'
-    s += '    <option value="TRIGGER_STATUS_OK">' + gettext("OK") + '</option>'
-    s += '    <option value="TRIGGER_STATUS_PROBLEM">' + gettext("Problem") + '</option>'
-    s += '  </select>'
+    s += '<form class="form-inline">';
+    s += '  <label for="selectTriggerStatus">' + gettext("Status") + '</label>';
+    s += '  <select id="selectTriggerStatus">';
+    s += '    <option value="ANY">ANY</option>';
+    s += '    <option value="TRIGGER_STATUS_OK">' + gettext("OK") + '</option>';
+    s += '    <option value="TRIGGER_STATUS_PROBLEM">' + gettext("Problem") + '</option>';
+    s += '  </select>';
 
-    s += '  <label>' + gettext("Severity") + '</label>'
-    s += '  <select id="selectTriggerSeverity">'
-    s += '    <option value="ANY">ANY</option>'
-    s += '    <option value="INFO">' + gettext("Information") + '</option>'
-    s += '    <option value="WARNING">' + gettext("Warning") + '</option>'
-    s += '    <option value="ERROR">' + gettext("Average") + '</option>'
-    s += '    <option value="CRITICAL">' + gettext("High") + '</option>'
-    s += '    <option value="EMERGENCY">' + gettext("Disaster") + '</option>'
-    s += '  </select>'
-    s += '  <select id="selectTriggerSeverityCompType" style="visibility:hidden;">'
-    s += '    <option value="CMP_EQ">' + gettext("Equal to") + '</option>'
-    s += '    <option value="CMP_EQ_GT">' + gettext("Equal to or greater than") + '</option>'
-    s += '  </select>'
-    s += '</form>'
+    s += '  <label>' + gettext("Severity") + '</label>';
+    s += '  <select id="selectTriggerSeverity">';
+    s += '    <option value="ANY">ANY</option>';
+    s += '    <option value="INFO">' + gettext("Information") + '</option>';
+    s += '    <option value="WARNING">' + gettext("Warning") + '</option>';
+    s += '    <option value="ERROR">' + gettext("Average") + '</option>';
+    s += '    <option value="CRITICAL">' + gettext("High") + '</option>';
+    s += '    <option value="EMERGENCY">' + gettext("Disaster") + '</option>';
+    s += '  </select>';
+    s += '  <select id="selectTriggerSeverityCompType" style="visibility:hidden;">';
+    s += '    <option value="CMP_EQ">' + gettext("Equal to") + '</option>';
+    s += '    <option value="CMP_EQ_GT">' + gettext("Equal to or greater than") + '</option>';
+    s += '  </select>';
+    s += '</form>';
     return s;
   }
 
   function makeExecutionParameterArea() {
     var s = "";
-    s += '<h3>' + gettext("Execution parameters") + '</h3>'
-    s += '<form class="form-inline">'
-    s += '  <label>' + gettext("Templates ") + '</label>'
-    s += '  <input id="actor-mail-dialog-button" type="button" value=' + gettext("e-mail") + ' />'
-    s += '</form>'
+    s += '<h3>' + gettext("Execution parameters") + '</h3>';
+    s += '<form class="form-inline">';
+    s += '  <label>' + gettext("Templates ") + '</label>';
+    s += '  <input id="actor-mail-dialog-button" type="button" value=' + gettext("e-mail") + ' />';
+    s += '</form>';
 
-    s += '<form class="form-inline">'
-    s += '  <label>' + gettext("Type") + '</label>'
-    s += '  <select id="selectType">'
-    s += '    <option value="ACTION_COMMAND">' + gettext("COMMAND") + '</option>'
-    s += '    <option value="ACTION_RESIDENT">' + gettext("RESIDENT") + '</option>'
-    s += '  </select>'
+    s += '<form class="form-inline">';
+    s += '  <label>' + gettext("Type") + '</label>';
+    s += '  <select id="selectType">';
+    s += '    <option value="ACTION_COMMAND">' + gettext("COMMAND") + '</option>';
+    s += '    <option value="ACTION_RESIDENT">' + gettext("RESIDENT") + '</option>';
+    s += '  </select>';
 
-    s += '  <label for="inputTimeout">' + gettext("Time-out (sec)") + '</label>'
-    s += '  <input id="inputTimeout" type="text" value="0">'
-    s += '  <label for="inputTimeout">(0: ' + gettext("No limit") + ') </label>'
-    s += '</form>'
+    s += '  <label for="inputTimeout">' + gettext("Time-out (sec)") + '</label>';
+    s += '  <input id="inputTimeout" type="text" value="0">';
+    s += '  <label for="inputTimeout">(0: ' + gettext("No limit") + ') </label>';
+    s += '</form>';
 
-    s += '<form class="form-inline">'
-    s += '  <label for="inputActionCommand">' + gettext("Command parameter") + '</label>'
-    s += '  <input id="inputActionCommand" type="text" style="width:100%;" value="">'
-    s += '</form>'
+    s += '<form class="form-inline">';
+    s += '  <label for="inputActionCommand">' + gettext("Command parameter") + '</label>';
+    s += '  <input id="inputActionCommand" type="text" style="width:100%;" value="">';
+    s += '</form>';
 
-    s += '<form class="form-inline">'
-    s += '  <label for="inputWorkingDir">' + gettext("Execution directory") + '</label>'
-    s += '  <input id="inputWorkingDir" type="text" value="" style="width:100%;">'
-    s += '</form>'
+    s += '<form class="form-inline">';
+    s += '  <label for="inputWorkingDir">' + gettext("Execution directory") + '</label>';
+    s += '  <input id="inputWorkingDir" type="text" value="" style="width:100%;">';
+    s += '</form>';
     return s;
   }
 
@@ -771,22 +771,22 @@ HatoholAddActionDialog.prototype.createMainElement = function() {
     s += '<form class="form-inline">';
     s += '<select id="selectIncidentTracker">';
     s += '</select>';
-    s += '<input id="editIncidentTrackers" type="button" '
+    s += '<input id="editIncidentTrackers" type="button" ';
     s += '       style="margin-left: 2px;" ';
     s +='        value="' + gettext('EDIT') + '" />';
-    s += '</form>'
+    s += '</form>';
     return s;
   }
 
   function makeMainDivHTML() {
     var s = "";
-    s += '<div id="add-action-div">'
+    s += '<div id="add-action-div">';
     s += makeTriggerConditionArea();
     if (self.forIncidentSetting)
       s += makeIncidentTrackerArea();
     else
       s += makeExecutionParameterArea();
-    s += '</div>'
+    s += '</div>';
     return s;
   }
 }
