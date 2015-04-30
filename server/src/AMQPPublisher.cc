36020ca5db2bc42338fabb562b7b5775dd3fc749
Hiroshi Hatake <hatake@clear-code.com>
2015-04-30 12:00:49 +0900

    [server][AMQPPublisher][HAPI2.0] Inherit AMQPConnection class

--- server/src/AMQPPublisher.cc
+++ server/src/AMQPPublisher.cc
@@ -18,6 +18,7 @@
  */
 
 #include "AMQPPublisher.h"
+#include "AMQPConnection.h"
 #include "AMQPConnectionInfo.h"
 #include "AMQPMessageHandler.h"
 #include <unistd.h>
@@ -29,45 +30,18 @@
 using namespace std;
 using namespace mlpl;
 
-class AMQPConnection {
+class AMQPPublisherConnection : public AMQPConnection {
 public:
-	AMQPConnection(const AMQPConnectionInfo &info)
-	: m_info(info),
+	AMQPPublisherConnection(const AMQPConnectionInfo &info)
+	: AMQPConnection(info),
+	  m_info(info),
 	  m_connection(NULL),
-	  m_socket(NULL),
-	  m_socketOpened(false),
 	  m_channel(0)
 	{
-		const char *virtualHost = getVirtualHost();
-		if (string(virtualHost) == "/") {
-			virtualHost = "";
-		}
-		MLPL_INFO("Broker URL: <%s://%s:%d/%s>\n",
-			  getScheme(),
-			  getHost(),
-			  getPort(),
-			  virtualHost);
 	}
 
-	~AMQPConnection()
+	~AMQPPublisherConnection()
 	{
-		disposeConnection();
-	}
-
-	bool connect()
-	{
-		m_connection = amqp_new_connection();
-		if (initializeConnection()) {
-			return true;
-		} else {
-			disposeConnection();
-			return false;
-		}
-	}
-
-	bool isConnected()
-	{
-		return m_connection;
 	}
 
 	bool publish(string &body)
@@ -109,328 +83,7 @@ public:
 private:
 	const AMQPConnectionInfo &m_info;
 	amqp_connection_state_t m_connection;
-	amqp_socket_t *m_socket;
-	bool m_socketOpened;
 	amqp_channel_t m_channel;
-
-	const char *getScheme(void)
-	{
-		if (m_info.useTLS()) {
-			return "amqps";
-		} else {
-			return "amqp";
-		}
-	}
-
-	const char *getHost()
-	{
-		return m_info.getHost();
-	}
-
-	guint getPort()
-	{
-		return m_info.getPort();
-	}
-
-	const char *getUser()
-	{
-		return m_info.getUser();
-	}
-
-	const char *getPassword()
-	{
-		return m_info.getPassword();
-	}
-
-	const char *getVirtualHost(void)
-	{
-		return m_info.getVirtualHost();
-	}
-
-	const string &getQueueName()
-	{
-		return m_info.getQueueName();
-	}
-
-	time_t getTimeout(void)
-	{
-		return m_info.getTimeout();
-	}
-
-	const string &getTLSCertificatePath(void)
-	{
-		return m_info.getTLSCertificatePath();
-	}
-
-	const string &getTLSKeyPath(void)
-	{
-		return m_info.getTLSKeyPath();
-	}
-
-	const string &getTLSCACertificatePath(void)
-	{
-		return m_info.getTLSCACertificatePath();
-	}
-
-	bool isTLSVerifyEnabled(void)
-	{
-		return m_info.isTLSVerifyEnabled();
-	}
-
-	void logErrorResponse(const char *context, int status)
-	{
-		logErrorResponse(context, static_cast<amqp_status_enum>(status));
-	}
-
-	void logErrorResponse(const char *context, amqp_status_enum status)
-	{
-		if (status == AMQP_STATUS_OK)
-			return;
-		MLPL_ERR("failed to %s: %d: %s\n",
-			 context,
-			 status,
-			 amqp_error_string2(status));
-	}
-
-	void logErrorResponse(const char *context,
-			      const amqp_rpc_reply_t &reply)
-	{
-		switch (reply.reply_type) {
-		case AMQP_RESPONSE_NORMAL:
-			break;
-		case AMQP_RESPONSE_NONE:
-			MLPL_ERR("%s: RPC reply type is missing\n", context);
-			break;
-		case AMQP_RESPONSE_LIBRARY_EXCEPTION:
-			MLPL_ERR("failed to %s: %s\n",
-				 context,
-				 amqp_error_string2(reply.library_error));
-			break;
-		case AMQP_RESPONSE_SERVER_EXCEPTION:
-			// TODO: show more messages
-			MLPL_ERR("%s: server exception\n",
-				 context);
-			break;
-		}
-		return;
-	}
-
-	bool initializeConnection()
-	{
-		if (!openSocket())
-			return false;
-		if (!login())
-			return false;
-		if (!openChannel())
-			return false;
-		if (!declareQueue())
-			return false;
-		return true;
-	}
-
-	bool openSocket()
-	{
-		if (m_info.useTLS()) {
-			if (!createTLSSocket()) {
-				return false;
-			}
-		} else {
-			if (!createPlainSocket()) {
-				return false;
-			}
-		}
-
-		const int status =
-			amqp_socket_open(m_socket, getHost(), getPort());
-		if (status != AMQP_STATUS_OK) {
-			const string context =
-				StringUtils::sprintf("open socket: %s:%u",
-						     getHost(),
-						     getPort());
-			logErrorResponse(context.c_str(), status);
-			return false;
-		}
-
-		m_socketOpened = true;
-		return true;
-	}
-
-	bool createTLSSocket(void)
-	{
-		m_socket = amqp_ssl_socket_new(m_connection);
-		if (!m_socket) {
-			MLPL_ERR("failed to create TLS socket\n");
-			return false;
-		}
-
-		if (!setTLSKey()) {
-			return false;
-		}
-		if (!setTLSCACertificate()) {
-			return false;
-		}
-		if (!setTLSVerification()) {
-			return false;
-		}
-
-		return true;
-	}
-
-	bool setTLSKey(void)
-	{
-		const int status =
-			amqp_ssl_socket_set_key(
-				m_socket,
-				getTLSCertificatePath().c_str(),
-				getTLSKeyPath().c_str());
-		if (status != AMQP_STATUS_OK) {
-			using StringUtils::sprintf;
-			const string context =
-				sprintf("set client certificate to SSL socket: "
-					"<%s>:<%s>",
-					getTLSCertificatePath().c_str(),
-					getTLSKeyPath().c_str());
-			logErrorResponse(context.c_str(), status);
-			return false;
-		}
-		return true;
-	}
-
-	bool setTLSCACertificate(void)
-	{
-		const int status =
-			amqp_ssl_socket_set_cacert(
-				m_socket,
-				getTLSCACertificatePath().c_str());
-		if (status != AMQP_STATUS_OK) {
-			using StringUtils::sprintf;
-			const string context =
-				sprintf("set CA certificate to SSL socket: <%s>",
-					getTLSCACertificatePath().c_str());
-			logErrorResponse(context.c_str(), status);
-			return false;
-		}
-		return true;
-	}
-
-	bool setTLSVerification(void)
-	{
-		amqp_ssl_socket_set_verify(m_socket, isTLSVerifyEnabled());
-		return true;
-	}
-
-	bool createPlainSocket(void)
-	{
-		m_socket = amqp_tcp_socket_new(m_connection);
-		if (!m_socket) {
-			MLPL_ERR("failed to create TCP socket\n");
-			return false;
-		}
-		return true;
-	}
-
-	bool login()
-	{
-		const int heartbeat = 1;
-		const amqp_rpc_reply_t reply =
-			amqp_login(m_connection,
-				   getVirtualHost(),
-				   AMQP_DEFAULT_MAX_CHANNELS,
-				   AMQP_DEFAULT_FRAME_SIZE,
-				   heartbeat,
-				   AMQP_SASL_METHOD_PLAIN,
-				   getUser(),
-				   getPassword());
-		if (reply.reply_type != AMQP_RESPONSE_NORMAL) {
-			const string context =
-				StringUtils::sprintf("login with <%s>",
-						     getUser());
-			logErrorResponse(context.c_str(), reply);
-			return false;
-		}
-		return true;
-	}
-
-	bool openChannel()
-	{
-		amqp_channel_open_ok_t *response;
-		m_channel = 1;
-		response = amqp_channel_open(m_connection, m_channel);
-		if (!response) {
-			const amqp_rpc_reply_t reply =
-				amqp_get_rpc_reply(m_connection);
-			logErrorResponse("open channel", reply);
-			m_channel = 0;
-			return false;
-		}
-		return true;
-	}
-
-	bool declareQueue()
-	{
-		const string &queueName = getQueueName();
-		const amqp_bytes_t queue = amqp_cstring_bytes(queueName.c_str());
-		MLPL_INFO("Queue: <%s>\n", queueName.c_str());
-		const amqp_boolean_t passive = false;
-		const amqp_boolean_t durable = false;
-		const amqp_boolean_t exclusive = false;
-		const amqp_boolean_t auto_delete = false;
-		const amqp_table_t arguments = amqp_empty_table;
-		const amqp_queue_declare_ok_t *response =
-			amqp_queue_declare(m_connection,
-					   m_channel,
-					   queue,
-					   passive,
-					   durable,
-					   exclusive,
-					   auto_delete,
-					   arguments);
-		if (!response) {
-			const amqp_rpc_reply_t reply =
-				amqp_get_rpc_reply(m_connection);
-			if (reply.reply_type != AMQP_RESPONSE_NORMAL) {
-				logErrorResponse("declare queue", reply);
-				return false;
-			}
-		}
-		return true;
-	}
-
-	void disposeConnection()
-	{
-		if (!m_connection)
-			return;
-
-		disposeChannel();
-		disposeSocket();
-		logErrorResponse("destroy connection",
-				 amqp_destroy_connection(m_connection));
-
-		m_socket = NULL;
-		m_connection = NULL;
-	}
-
-	void disposeChannel()
-	{
-		if (m_channel == 0)
-			return;
-
-		logErrorResponse("close channel",
-				 amqp_channel_close(m_connection, m_channel,
-						    AMQP_REPLY_SUCCESS));
-		m_channel = 0;
-	}
-
-	void disposeSocket()
-	{
-		if (!m_socketOpened)
-			return;
-
-		logErrorResponse("close connection",
-				 amqp_connection_close(m_connection,
-						       AMQP_REPLY_SUCCESS));
-		m_socketOpened = false;
-	}
 };
 
 AMQPPublisher::AMQPPublisher(const AMQPConnectionInfo &connectionInfo,
@@ -446,6 +99,6 @@ AMQPPublisher::~AMQPPublisher()
 
 bool AMQPPublisher::publish()
 {
-	AMQPConnection connection(m_connectionInfo);
+	AMQPPublisherConnection connection(m_connectionInfo);
 	return connection.publish(m_body);
 }
