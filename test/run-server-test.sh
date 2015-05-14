20bba326ac63d293563cdccc630f1bf92a638c18
Takuro Ashie <ashie@clear-code.com>
2015-05-14 15:32:45 +0900

    [travis] Don't use matrix for server's tests to reduce load

---
+++ test/run-server-test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+FAILED=0
+BASE_DIR="`dirname $0`"
+TOP_DIR="$BASE_DIR/.."
+
+${TOP_DIR}/server/mlpl/test/run-test.sh || FAILED=1
+${TOP_DIR}/server/test/run-test.sh || FAILED=1
+${TOP_DIR}/server/tools/test/run-test.sh || FAILED=1
+exit $FAILED
