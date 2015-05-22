12423f122327044bc151524fcc163bd28bf91869
Takuro Ashie <ashie@clear-code.com>
2015-05-22 09:55:37 +0900

    [HatoholArmPluginGateHAPI2] Remove needless comments

--- server/src/HatoholArmPluginGateHAPI2.cc
+++ server/src/HatoholArmPluginGateHAPI2.cc
@@ -649,7 +649,6 @@ string HatoholArmPluginGateHAPI2::procedureHandlerUpdateTriggers(
 	agent.add("result", result);
 	agent.add("id", 1);
 	agent.endObject();
-	// TODO: implement replying exchange profile procedure with AMQP
 	return agent.generate();
 }
 
@@ -716,7 +715,6 @@ string HatoholArmPluginGateHAPI2::procedureHandlerUpdateEvents(
 	agent.add("result", result);
 	agent.add("id", 1);
 	agent.endObject();
-	// TODO: implement replying exchange profile procedure with AMQP
 	return agent.generate();
 }
 
@@ -770,7 +768,6 @@ string HatoholArmPluginGateHAPI2::procedureHandlerUpdateHostParents(
 	agent.add("result", result);
 	agent.add("id", 1);
 	agent.endObject();
-	// TODO: implement replying exchange profile procedure with AMQP
 	return agent.generate();
 }
 
@@ -819,7 +816,6 @@ string HatoholArmPluginGateHAPI2::procedureHandlerUpdateArmInfo(
 	agent.add("result", result);
 	agent.add("id", 1);
 	agent.endObject();
-	// TODO: implement replying exchange profile procedure with AMQP
 	return agent.generate();
 }
 
