a9e1a0d49a6de643160d4d2410d29d2aa63c29f0
Hiroshi Hatake <hatake@clear-code.com>
2015-05-22 14:02:47 +0900

    [server][HatoholArmPluginGateHAPI2] Fix a typo

--- server/src/HatoholArmPluginGateHAPI2.cc
+++ server/src/HatoholArmPluginGateHAPI2.cc
@@ -866,7 +866,7 @@ void HatoholArmPluginGateHAPI2::upsertLastInfo(string lastInfoValue, LastInfoTyp
 
 	LastInfoQueryOption option(USER_ID_SYSTEM);
 	option.setLastInfoType(type);
-	option.setTargetId(serverInfo.id);
+	option.setTargetServerId(serverInfo.id);
 	LastInfoDefList lastInfoList;
 	dbLastInfo.getLastInfoList(lastInfoList, option);
 	if (lastInfoList.empty())
