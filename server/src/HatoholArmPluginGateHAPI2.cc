479cc47cd70090ea5c5376c1ecdec22c2104b8df
Hiroshi Hatake <hatake@clear-code.com>
2015-05-26 17:44:55 +0900

    [server][HatoholArmPluginGateHAPI2] Use hap given id in putItems procedure

--- server/src/HatoholArmPluginGateHAPI2.cc
+++ server/src/HatoholArmPluginGateHAPI2.cc
@@ -407,12 +407,14 @@ string HatoholArmPluginGateHAPI2::procedureHandlerPutItems(
 	// TODO: callback
 
 	parser.endObject(); // params
+	int64_t rpcId;
+	parser.read("id", rpcId);
 
 	JSONBuilder agent;
 	agent.startObject();
 	agent.add("jsonrpc", "2.0");
 	agent.add("result", "");
-	agent.add("id", 1);
+	agent.add("id", rpcId);
 	agent.endObject();
 	return agent.generate();
 }
