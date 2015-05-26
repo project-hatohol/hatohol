5f0f7790b1731c78e24ac5d8b84390a71f5bf66a
Hiroshi Hatake <hatake@clear-code.com>
2015-05-26 17:50:11 +0900

    [server][HatoholArmPluginGateHAPI2] Use hap given id in updateHosts procedure

--- server/src/HatoholArmPluginGateHAPI2.cc
+++ server/src/HatoholArmPluginGateHAPI2.cc
@@ -534,12 +534,14 @@ string HatoholArmPluginGateHAPI2::procedureHandlerUpdateHosts(
 	string result = succeeded ? "SUCCESS" : "FAILURE";
 
 	parser.endObject(); // params
+	int64_t rpcId;
+	parser.read("id", rpcId);
 
 	JSONBuilder agent;
 	agent.startObject();
 	agent.add("jsonrpc", "2.0");
 	agent.add("result", result);
-	agent.add("id", 1);
+	agent.add("id", rpcId);
 	agent.endObject();
 	return agent.generate();
 }
