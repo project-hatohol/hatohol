4cb4081a748b1b8f1f7a2217980e9abec2548baa
Kazuhiro Yamato <kz0817@gmail.com>
2015-06-12 10:42:54 +0900

    Use a more smart way.

--- server/hap2/test/run-test.sh
+++ server/hap2/test/run-test.sh
@@ -2,4 +2,4 @@
 
 test_dir=$(cd $(dirname $0) && pwd)
 export PYTHONPATH=$test_dir/..
-python -m unittest discover -s $test_dir -p 'Test*.py' $1 $2 $3 $4 $5 $6 $7 $8 $9
+python -m unittest discover -s $test_dir -p 'Test*.py' "$@"
