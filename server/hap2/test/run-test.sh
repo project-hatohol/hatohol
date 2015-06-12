9545ee5987643a1ccf64d925f576a88b7c445b38
Kazuhiro Yamato <kz0817@gmail.com>
2015-06-12 16:18:17 +0900

    Add a HAPI2.0 transporter implementation RabbitMQConnector.

--- server/hap2/test/run-test.sh
+++ server/hap2/test/run-test.sh
@@ -2,4 +2,9 @@
 
 test_dir=$(cd $(dirname $0) && pwd)
 export PYTHONPATH=$test_dir/..
+
+# Workaround: probably due to amqp-tools bug.
+if [ `cat /etc/issue | grep "^Ubuntu 14.04" | wc -l` = "1" ]; then
+  export RABBITMQ_CONNECTOR_TEST_WORKAROUND=1
+fi
 python -m unittest discover -s $test_dir -p 'Test*.py' "$@"
