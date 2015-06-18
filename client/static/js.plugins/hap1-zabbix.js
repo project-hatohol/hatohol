70067ba1291baeec225e73933632b377c2436600
Takuro Ashie <ashie@clear-code.com>
2015-06-18 19:25:38 +0900

    [client][hap1-zabbix] Fix indent

--- client/static/js.plugins/hap1-zabbix.js
+++ client/static/js.plugins/hap1-zabbix.js
@@ -1,44 +1,44 @@
 (function(hatohol) {
-    var self = hatohol.addNamespace("hatohol.hap_2");
-    // 2 == hatohol.MONITORING_SYSTEM_HAPI_ZABBIX
+  var self = hatohol.addNamespace("hatohol.hap_2");
+  // 2 == hatohol.MONITORING_SYSTEM_HAPI_ZABBIX
 
-    self.type = hatohol.MONITORING_SYSTEM_HAPI_ZABBIX;
-    self.label = "Zabbix (HAPI)";
+  self.type = hatohol.MONITORING_SYSTEM_HAPI_ZABBIX;
+  self.label = "Zabbix (HAPI)";
 
-    self.getTopURL = function(server) {
-        var ipAddress, port, url;
+  self.getTopURL = function(server) {
+    var ipAddress, port, url;
 
-        if (!server)
-            return undefined;
+    if (!server)
+      return undefined;
 
-        ipAddress = server["ipAddress"];
-        port = server["port"];
-        if (hatohol.isIPv4(ipAddress))
-            url = "http://" + ipAddress;
-        else // maybe IPv6
-            url = "http://[" + ipAddress + "]";
-        if (!isNaN(port) && port != "80")
-            url += ":" + port;
-        url += "/zabbix/";
+    ipAddress = server["ipAddress"];
+    port = server["port"];
+    if (hatohol.isIPv4(ipAddress))
+      url = "http://" + ipAddress;
+    else // maybe IPv6
+      url = "http://[" + ipAddress + "]";
+    if (!isNaN(port) && port != "80")
+      url += ":" + port;
+    url += "/zabbix/";
 
-        return url ? hatohol.escapeHTML(url) : url;
-    };
+    return url ? hatohol.escapeHTML(url) : url;
+  };
 
-    self.getItemGraphURL = function(server, itemId) {
-	var location = self.getTopURL(server);
-	if (!location)
-	    return undefined;
+  self.getItemGraphURL = function(server, itemId) {
+    var location = self.getTopURL(server);
+    if (!location)
+      return undefined;
 
-	location += "history.php?action=showgraph&amp;itemid=" + itemId;
-	return location;
-    };
+    location += "history.php?action=showgraph&amp;itemid=" + itemId;
+    return location;
+  };
 
-    self.getMapsURL = function(server) {
-	var location = self.getTopURL(server);
-	if (!location)
-	    return undefined;
+  self.getMapsURL = function(server) {
+    var location = self.getTopURL(server);
+    if (!location)
+      return undefined;
 
-	location += "maps.php";
-	return location;
-    };
+    location += "maps.php";
+    return location;
+  };
 }(hatohol));
