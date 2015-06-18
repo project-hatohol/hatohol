694686246a380a7dc933ed60ec21cf5825fa800e
Takuro Ashie <ashie@clear-code.com>
2015-06-18 14:08:24 +0900

    [client] Load additional JS files for HAP plugins from /static/js.plugins

--- client/viewer/urls.py
+++ client/viewer/urls.py
@@ -16,46 +16,46 @@
 # <http://www.gnu.org/licenses/>.
 
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
 
 from hatohol import hatohol_def
+from views import HatoholView
 
 urlpatterns = patterns(
     '',
     url(r'^$',
-        TemplateView.as_view(template_name='viewer/dashboard_ajax.html')),
+        HatoholView.as_view(template_name='viewer/dashboard_ajax.html')),
     url(r'^ajax_dashboard$',
-        TemplateView.as_view(
+        HatoholView.as_view(
             template_name='viewer/dashboard_ajax.html')),
     url(r'^ajax_overview_triggers$',
-        TemplateView.as_view(
+        HatoholView.as_view(
             template_name='viewer/overview_triggers_ajax.html')),
     url(r'^ajax_overview_items$',
-        TemplateView.as_view(
+        HatoholView.as_view(
             template_name='viewer/overview_items_ajax.html')),
     url(r'^ajax_latest$',
-        TemplateView.as_view(template_name='viewer/latest_ajax.html')),
+        HatoholView.as_view(template_name='viewer/latest_ajax.html')),
     url(r'^ajax_triggers$',
-        TemplateView.as_view(template_name='viewer/triggers_ajax.html')),
+        HatoholView.as_view(template_name='viewer/triggers_ajax.html')),
     url(r'^ajax_events$',
-        TemplateView.as_view(template_name='viewer/events_ajax.html')),
+        HatoholView.as_view(template_name='viewer/events_ajax.html')),
     url(r'^ajax_servers$',
-        TemplateView.as_view(template_name='viewer/servers_ajax.html'),
+        HatoholView.as_view(template_name='viewer/servers_ajax.html'),
         kwargs={'hatohol': hatohol_def}),
     url(r'^ajax_actions$',
-        TemplateView.as_view(template_name='viewer/actions_ajax.html')),
+        HatoholView.as_view(template_name='viewer/actions_ajax.html')),
     url(r'^ajax_incident_settings$',
-        TemplateView.as_view(
+        HatoholView.as_view(
             template_name='viewer/incident_settings_ajax.html')),
     url(r'^ajax_log_search_systems$',
-        TemplateView.as_view(
+        HatoholView.as_view(
             template_name='viewer/log_search_systems_ajax.html')),
     url(r'^ajax_graphs$',
-        TemplateView.as_view(template_name='viewer/graphs_ajax.html')),
+        HatoholView.as_view(template_name='viewer/graphs_ajax.html')),
     url(r'^ajax_users$',
-        TemplateView.as_view(template_name='viewer/users_ajax.html')),
+        HatoholView.as_view(template_name='viewer/users_ajax.html')),
     url(r'^ajax_hosts$',
-        TemplateView.as_view(template_name='viewer/hosts_ajax.html')),
+        HatoholView.as_view(template_name='viewer/hosts_ajax.html')),
     url(r'^ajax_history$',
-        TemplateView.as_view(template_name='viewer/history_ajax.html')),
+        HatoholView.as_view(template_name='viewer/history_ajax.html')),
 )
