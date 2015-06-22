67b695d331bdc51bceb6c832b7f6f4a33b517c26
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2015-06-22 16:30:00 +0900

    Create common file of unit test.

---
+++ server/hap2/test/common.py
@@ -0,0 +1,9 @@
+def assertNotRaises(func, *args, **kwargs):
+    try:
+        func(*args, **kwargs)
+    except Exception:
+        raise
+
+def returnPrivObj(instance, obj_name):
+    class_name = instance.__class__.__name__
+    return eval("instance._"+class_name+obj_name)
