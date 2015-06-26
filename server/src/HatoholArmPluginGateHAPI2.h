46639a9deef7fcad73d5ecaa50b5c311cfb07e4b
Hiroshi Hatake <hatake@clear-code.com>
2015-06-26 15:13:14 +0900

    [server][HatoholArmPluginGateHAPI2] Use correct access qualifier

--- server/src/HatoholArmPluginGateHAPI2.h
+++ server/src/HatoholArmPluginGateHAPI2.h
@@ -66,6 +66,14 @@ protected:
 	void updateSelfMonitoringTrigger(bool hasError,
 	                                 const HAPI2PluginCollectType &type,
 	                                 const HAPI2PluginErrorCode &errorCode);
+	virtual void onSetPluginInitialInfo(void) override;
+	virtual void onConnect(void) override;
+	virtual void onConnectFailure(void) override;
+	void setPluginAvailableTrigger(const HAPI2PluginCollectType &type,
+				       const TriggerIdType &trrigerId,
+				       const HatoholError &hatoholError);
+	void setPluginConnectStatus(const HAPI2PluginCollectType &type,
+				    const HAPI2PluginErrorCode &errorCode);
 
 private:
 	std::string procedureHandlerExchangeProfile(JSONParser &parser);
@@ -85,14 +93,6 @@ public:
 	static bool parseTimeStamp(const std::string &timeStampString,
 				   timespec &timeStamp,
 				   const bool allowEmpty = false);
-	virtual void onSetPluginInitialInfo(void) override;
-	virtual void onConnect(void) override;
-	virtual void onConnectFailure(void) override;
-	void setPluginAvailableTrigger(const HAPI2PluginCollectType &type,
-				       const TriggerIdType &trrigerId,
-				       const HatoholError &hatoholError);
-	void setPluginConnectStatus(const HAPI2PluginCollectType &type,
-				    const HAPI2PluginErrorCode &errorCode);
 
 private:
 	struct Impl;
