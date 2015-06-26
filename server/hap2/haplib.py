b1a6e19de5cbb6ecfe052ff8bf51cfe089088c7b
Kazuhiro Yamato <kz0817@gmail.com>
2015-06-27 00:03:13 +0900

    [hap2] Define some constants.

--- server/hap2/haplib.py
+++ server/hap2/haplib.py
@@ -33,6 +33,7 @@ import imp
 import transporter
 from rabbitmqconnector import RabbitMQConnector
 import calendar
+import sets
 
 SERVER_PROCEDURES = {"exchangeProfile": True,
                      "getMonitoringServerInfo": True,
@@ -117,6 +118,17 @@ PROCEDURES_DEFS = {
             "events": {"type": list(), "mandatory": True}
         }
     },
+    "putItems": {
+        "args": {
+            "items": {"type": list(), "mandatory": True}
+        }
+    },
+    "putHistory": {
+        "args": {
+            "samples": {"type": list(), "mandatory": True},
+            "itemId": {"type": unicode(), "mandatory": True}
+        }
+    },
     "getLastInfo": {
         "args": {}
     },
@@ -137,8 +149,15 @@ ERROR_DICT = {
     ERR_CODE_PARSER_ERROR: "Parse error",
 }
 
-MAX_EVENT_CHUNK_SIZE = 1000
+EVENT_TYPES = sets.ImmutableSet(
+    ["GOOD", "BAD", "UNKNOWN", "NOTIFICATION"])
+TRIGGER_STATUS = sets.ImmutableSet(
+    ["OK", "NG", "UNKNOWN"])
+TRIGGER_SEVERITY = sets.ImmutableSet(
+    ["UNKNOWN", "INFO", "WARNING", "ERROR", "CRITICAL", "EMERGENCY"])
 
+MAX_EVENT_CHUNK_SIZE = 1000
+MAX_LAST_INFO_SIZE = 32767
 
 def handle_exception(raises=()):
     """
