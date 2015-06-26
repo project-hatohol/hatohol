8f7c1aec6ab0b09204720220126ec7dfe4151e4b
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2015-06-26 14:22:34 +0900

    Fix a returnPrivObj
    This method can be assign class name.

--- server/hap2/test/common.py
+++ server/hap2/test/common.py
@@ -4,6 +4,7 @@ def assertNotRaises(func, *args, **kwargs):
     except Exception:
         raise
 
-def returnPrivObj(instance, obj_name):
-    class_name = instance.__class__.__name__
+def returnPrivObj(instance, obj_name, class_name=None):
+    if class_name is None:
+        class_name = instance.__class__.__name__
     return eval("instance._"+class_name+obj_name)
