d9638d8906e2fb1f8727210b8eb36f9e7df16e1c
Toshihiko Miyazawa <toshihiko.miyazawa@miraclelinux.com>
2015-06-26 18:31:35 +0900

    [client][EventView] Save the "event-sort-order", when the "Time" column clicked.

--- client/static/js/events_view.js
+++ client/static/js/events_view.js
@@ -179,10 +179,28 @@ var EventsView = function(userProfile, baseElem) {
   function setupCallbacks() {
     $("#table").stupidtable();
     $("#table").bind('aftertablesort', function(event, data) {
+      var icon;
+      if (data.column == 1) { // "Time" column
+        if (data.direction === "asc") {
+          icon = "up";
+          if (self.baseQuery.sortOrder != hatohol.DATA_QUERY_OPTION_SORT_ASCENDING) {
+            self.baseQuery.sortOrder = hatohol.DATA_QUERY_OPTION_SORT_ASCENDING;
+            saveConfig({'event-sort-order': self.baseQuery.sortOrder});
+            self.startConnection(getQuery(self.currentPage), updateCore);
+          }
+        } else {
+          icon = "down";
+          if (self.baseQuery.sortOrder != hatohol.DATA_QUERY_OPTION_SORT_DESCENDING) {
+            self.baseQuery.sortOrder = hatohol.DATA_QUERY_OPTION_SORT_DESCENDING;
+            saveConfig({'event-sort-order': self.baseQuery.sortOrder});
+            self.startConnection(getQuery(self.currentPage), updateCore);
+          }
+        }
+      }
       var th = $(this).find("th");
       th.find("i.sort").remove();
-      var icon = data.direction === "asc" ? "up" : "down";
-      th.eq(data.column).append("<i class='sort glyphicon glyphicon-arrow-" + icon +"'></i>");
+      if (icon)
+        th.eq(data.column).append("<i class='sort glyphicon glyphicon-arrow-" + icon +"'></i>");
     });
 
     $("#select-severity, #select-status").change(function() {
