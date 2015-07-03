f6bfa2977ce86d82e0e559f176b66cbc7f0d9e9f
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2015-07-03 13:59:26 +0900

    Create install files to python module

---
+++ server/hap2/setup_common.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='hatohol',
+      description='Hatohol arm plugin 2.0',
+      author='Procect Hatohol',
+      author_email='hatohol-users@list.sourceforge.net',
+      url='https://github.com/project-hatohol/hatohol',
+      py_modules=['hatohol.haplib', 'hatohol.transporter', 'hatohol.standardhap']
+     )
