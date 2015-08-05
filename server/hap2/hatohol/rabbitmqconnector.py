3fb41aa9e9c6e810e7de74a51ad31fdfc184fc35
Kazuhiro Yamato <kz0817@gmail.com>
2015-08-05 14:01:46 +0900

    Clean up.

--- server/hap2/hatohol/rabbitmqconnector.py
+++ server/hap2/hatohol/rabbitmqconnector.py
@@ -96,11 +96,7 @@ class RabbitMQConnector(Transporter):
             except:
                 # On some condition such as Ubuntu 14.04, the above close()
                 # raises an exception when the rabbitmq-server is stopped.
-                (exctype, value, tb) = sys.exc_info()
-                if exctype is SystemExit:
-                    raise
-                logging.error("Unexpected error: %s, %s, %s" % \
-                              (exctype, value, traceback.format_tb(tb)))
+                hap.handle_exception()
 
             self.__connection = None
 
