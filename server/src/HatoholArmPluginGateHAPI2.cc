e1e14b232b16891323d559fa9892069dcc25c2a2
Kazuhiro Yamato <kz0817@gmail.com>
2015-08-20 17:20:50 +0900

    [server] Adhoc registration of unknown hosts on putEvents.

--- server/src/HatoholArmPluginGateHAPI2.cc
+++ server/src/HatoholArmPluginGateHAPI2.cc
@@ -1589,6 +1589,23 @@ static bool parseTriggerSeverity(JSONParser &parser,
 	return true;
 }
 
+static HostIdType getHostInfoCacheWithAdhocRegistration(
+  HostInfoCache &hostInfoCache, const ServerIdType &serverId,
+  const LocalHostIdType &hostIdInServer, const string &name)
+{
+	HostInfoCache::Element cacheElem;
+	const bool found = hostInfoCache.getName(hostIdInServer, cacheElem);
+	if (!found) {
+		hostInfoCache.registerAdHoc(hostIdInServer, name, cacheElem);
+		MLPL_INFO(
+		  "Host cache: add-hoc registration. "
+		  "server: %" FMT_SERVER_ID ", "
+		  "hostIdInServer: %" FMT_LOCAL_HOST_ID "\n",
+		  serverId, hostIdInServer.c_str());
+	}
+	return cacheElem.hostId;
+}
+
 static bool parseTriggersParams(JSONParser &parser, TriggerInfoList &triggerInfoList,
 				const MonitoringServerInfo &serverInfo,
 				const HostInfoCache &hostInfoCache,
@@ -1725,7 +1742,7 @@ static bool parseEventType(JSONParser &parser, EventInfo &eventInfo,
 
 static bool parseEventsParams(JSONParser &parser, EventInfoList &eventInfoList,
 			      const MonitoringServerInfo &serverInfo,
-			      const HostInfoCache &hostInfoCache,
+			      HostInfoCache &hostInfoCache,
 			      JSONRPCError &errObj)
 {
 	CHECK_MANDATORY_ARRAY_EXISTENCE("events", errObj);
@@ -1773,17 +1790,10 @@ static bool parseEventsParams(JSONParser &parser, EventInfoList &eventInfoList,
 		PARSE_AS_MANDATORY("extendedInfo", eventInfo.extendedInfo, errObj);
 		parser.endElement();
 
-		HostInfoCache::Element cacheElem;
-		const bool found =
-			hostInfoCache.getName(eventInfo.hostIdInServer, cacheElem);
-		if (!found) {
-			MLPL_WARN(
-			  "Host cache: not found. server: %" FMT_SERVER_ID ", "
-			  "hostIdInServer: %" FMT_LOCAL_HOST_ID "\n",
-			  serverInfo.id, eventInfo.hostIdInServer.c_str());
-			cacheElem.hostId = INVALID_HOST_ID;
-		}
-		eventInfo.globalHostId = cacheElem.hostId;
+		eventInfo.globalHostId =
+		  getHostInfoCacheWithAdhocRegistration(
+		    hostInfoCache, serverInfo.id,
+		    eventInfo.hostIdInServer, eventInfo.hostName);
 		eventInfoList.push_back(eventInfo);
 	}
 	parser.endObject(); // events
@@ -1802,8 +1812,8 @@ string HatoholArmPluginGateHAPI2::procedureHandlerPutEvents(
 	parser.startObject("params");
 
 	const MonitoringServerInfo &serverInfo = m_impl->m_serverInfo;
-	const HostInfoCache &hostInfoCache = m_impl->hostInfoCache;
-	parseEventsParams(parser, eventInfoList, serverInfo, hostInfoCache, errObj);
+	parseEventsParams(parser, eventInfoList, serverInfo,
+	                  m_impl->hostInfoCache, errObj);
 
 	if (parser.isMember("fetchId")) {
 		parser.read("fetchId", fetchId);
