ed73b32b51601abc799728df3cab727b637121e6
Kazuhiro Yamato <kz0817@gmail.com>
2015-09-04 06:37:54 +0000

    Can add date and time to the package version.
    Developers and tester sometimes would like to use a temporarily
    built package. However, it's needed to have a large version number
    update the package on the test platfrom.
    
    This patch enables to include date and time to the version string
    automatically.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# The date and time can be added to the version by
+# setting the environment variable like
+#
+#   $ rm -fr autom4te.cache
+#   $ ADD_DATE_TO_VERSION=1 ./autogen.sh
+
 if [ ! -d m4 ]; then
   mkdir m4
 fi
