d57ae77cef6a40b599b08fb3532ebcd86da2761d
Kazuhiro Yamato <kz0817@gmail.com>
2015-10-07 15:05:21 +0000

    [WebUI] Make DEBUG False to avoid internal information from being shown.
    When the DEBUG variable is True and any problem happens in WebUI code,
    the debug information is sent to the viewer. This behavior is generally
    undesired.

--- client/hatohol/settings.py
+++ client/hatohol/settings.py
@@ -22,7 +22,7 @@ import logging
 
 PROJECT_HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -44,7 +44,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en//ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
