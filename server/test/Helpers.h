7b5354c2db28b9faeb4401da1c4cf9bf556e9cef
Hiroshi Hatake <hatake@clear-code.com>
2015-10-14 12:02:31 +0900

    [Helpers][server] Define helper function for severityRankInfo assertion

--- server/test/Helpers.h
+++ server/test/Helpers.h
@@ -120,6 +120,7 @@ std::string makeItemOutput(const ItemInfo &itemInfo);
 std::string makeHostsOutput(const ServerHostDef &svHostDef, const size_t &id);
 std::string makeHostgroupsOutput(const Hostgroup &hostgrp, const size_t &id);
 std::string makeMapHostsHostgroupsOutput(const HostgroupMember &hostgrpMember, const size_t &id);
+std::string makeSeverityRankInfoOutput(const SeverityRankInfo &severityRankInfo);
 
 void _assertDatetime(int expectedClock, int actualClock);
 #define assertDatetime(E,A) cut_trace(_assertDatetime(E,A))
