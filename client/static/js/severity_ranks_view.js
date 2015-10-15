7b5dc6cfd4c145163ad7d6996d47785f9d0fa0ea
Hiroshi Hatake <hatake@clear-code.com>
2015-10-15 18:57:30 +0900

    [SeverityRanksView] Add viewer/js skeleton code

---
+++ client/static/js/severity_ranks_view.js
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License, version 3
+ * as published by the Free Software Foundation.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Hatohol. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+var SeverityRanksView = function(userProfile) {
+  //
+  // Variables
+  //
+  var self = this;
+
+  // call the constructor of the super class
+  HatoholMonitoringView.apply(this, [userProfile]);
+
+  //
+  // main code
+  //
+  load();
+
+  //
+  // Main view
+  //
+  // TODO: Add code
+
+  //
+  // callback function from the base template
+  //
+  function drawTableBody(logSearchSystems) {
+    var table = "";
+    return table;
+  }
+
+  function load() {
+  }
+};
+
+SeverityRanksView.prototype = Object.create(HatoholMonitoringView.prototype);
+SeverityRanksView.prototype.constructor = SeverityRanksView;
