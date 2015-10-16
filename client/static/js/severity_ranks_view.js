68d19449a7582444ebe26744542a77db3460364b
Hiroshi Hatake <hatake@clear-code.com>
2015-10-16 11:44:28 +0900

    [SeverityRanksView] Draw table body

--- client/static/js/severity_ranks_view.js
+++ client/static/js/severity_ranks_view.js
@@ -22,6 +22,7 @@ var SeverityRanksView = function(userProfile) {
   // Variables
   //
   var self = this;
+  var rawData;
 
   // call the constructor of the super class
   HatoholMonitoringView.apply(this, [userProfile]);
@@ -34,17 +35,58 @@ var SeverityRanksView = function(userProfile) {
   //
   // Main view
   //
-  // TODO: Add code
+  var severity_choices = [
+    gettext("Not classified"),
+    gettext("Information"),
+    gettext("Warning"),
+    gettext("Average"),
+    gettext("High"),
+    gettext("Disaster")
+  ];
 
-  //
-  // callback function from the base template
-  //
-  function drawTableBody(logSearchSystems) {
-    var table = "";
-    return table;
+  function drawTableBody(replyData) {
+    var html, severityRankId, status, color, label, isImportant;
+    html = "";
+
+    for (var x; x < replyData["SeverityRanks"].length; ++x) {
+      severityRankId = replyData["id"][x];
+      status = replyData["status"][x];
+      color = replyData["color"][x];
+      label = replyData["label"][x];
+      isImportant = replyData["isImportant"][x];
+
+      html += "<tr>";
+      html += "<td>" + severity_choices[Number(status)] + "</td>";
+      html += "<td>" + escapeHTML(color) + "</td>";
+      html += "<td>" + escapeHTML(label) + "</td>";
+      html += "<td class='delete-selector'>";
+      html += "<input type='checkbox' class='selectcheckbox' " +
+        "severityRankId='" + escapeHTML(severityRankId) + "'></td>";
+      html += "</tr>";
+    }
+
+    return html;
+  }
+
+  function drawTableContents(data) {
+    $("#table tbody").empty();
+    $("#table tbody").append(drawTableBody(data));
+  }
+
+  function getQuery() {
+    return 'severity-rank';
+  };
+
+  function updateCore(reply) {
+    rawData = reply;
+
+    drawTableContents(rawData);
   }
 
   function load() {
+    self.displayUpdateTime();
+
+    self.startConnection(getQuery(self.currentPage), updateCore);
   }
 };
 
