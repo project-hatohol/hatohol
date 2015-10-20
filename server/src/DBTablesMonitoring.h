a96ec9398b5db4c6bbf449621da87133c1772b9f
Kazuhiro Yamato <kz0817@gmail.com>
2015-10-20 13:44:40 +0000

    [HatoholArmPluginGateHAPI2] Store Triggers and the lastInfo atomically.

--- server/src/DBTablesMonitoring.h
+++ server/src/DBTablesMonitoring.h
@@ -162,14 +162,16 @@ public:
 	virtual ~DBTablesMonitoring();
 
 	void addTriggerInfo(const TriggerInfo *triggerInfo);
-	void addTriggerInfoList(const TriggerInfoList &triggerInfoList);
+	void addTriggerInfoList(const TriggerInfoList &triggerInfoList,
+	                        DBAgent::TransactionHooks *hooks = NULL);
 
 	void updateTrigger(const TriggerInfoList &triggerInfoList,
 			   const ServerIdType &serverId);
 	HatoholError deleteTriggerInfo(const TriggerIdList &idList,
 	                               const ServerIdType &serverId);
 	HatoholError syncTriggers(const TriggerInfoList &triggerInfoList,
-	                          const ServerIdType &serverId);
+	                          const ServerIdType &serverId,
+	                          DBAgent::TransactionHooks *hooks = NULL);
 
 	/**
 	 * Get the trigger information with the specified server ID and
