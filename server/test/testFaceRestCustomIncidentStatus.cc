4c1cdf0645dcff7db1f9590cf46ba1bb1790113a
Hiroshi Hatake <hatake@clear-code.com>
2015-11-11 12:21:20 +0900

    [FaceRestCustomIncidentStatus][test] Add get/post/put REST API test

--- server/test/testFaceRestCustomIncidentStatus.cc
+++ server/test/testFaceRestCustomIncidentStatus.cc
@@ -49,4 +49,131 @@ void cut_teardown(void)
 	g_parser = NULL;
 }
 
+static void _assertCustomIncidentStatuses(
+  const string &path = "/custom-incident-status", const string &callbackName = "")
+{
+	startFaceRest();
+	RequestArg arg(path, callbackName);
+	arg.userId = 2; // FIXME
+	OperationPrivilege privilege(arg.userId);
+	g_parser = getResponseAsJSONParser(arg);
+	assertErrorCode(g_parser);
+	assertStartObject(g_parser, "CustomIncidentStatuses");
+	for (size_t i = 0; i < NumTestCustomIncidentStatus; i++) {
+		const CustomIncidentStatus &customIncidentStatus =
+			testCustomIncidentStatus[i];
+		g_parser->startElement(i);
+		assertValueInParser(g_parser, "id", i + 1);
+		assertValueInParser(g_parser, "code", customIncidentStatus.code);
+		assertValueInParser(g_parser, "label", customIncidentStatus.label);
+		g_parser->endElement();
+	}
+	g_parser->endObject();
+}
+#define assertCustomIncidentStatuses(P,...) \
+cut_trace(_assertCustomIncidentStatuses(P,##__VA_ARGS__))
+
+#define assertAddCustomIncidentStatus(P, ...) \
+cut_trace(_assertAddRecord(P, "/custom-incident-status", ##__VA_ARGS__))
+
+#define assertUpdateCustomIncidentStatus(P, ...) \
+cut_trace(_assertUpdateRecord(P, "/custom-incident-status", ##__VA_ARGS__))
+
+static void _assertCustomIncidentStatusInDB(
+  const CustomIncidentStatus &expectedCustomIncidentStatus,
+  const CustomIncidentStatusIdType &targetId = ALL_CUSTOM_INCIDENT_STATUSES)
+{
+	string statement;
+
+	if (targetId == ALL_CUSTOM_INCIDENT_STATUSES) {
+		statement = "select * from custom_incident_statuses "
+			    "order by id desc limit 1";
+	} else {
+		statement = StringUtils::sprintf(
+			      "select * from custom_incident_statuses where id=%"
+			      FMT_CUSTOM_INCIDENT_STATUS_ID,
+			      targetId);
+	}
+	string expected = makeCustomIncidentStatusOutput(
+			    expectedCustomIncidentStatus);
+	ThreadLocalDBCache cache;
+	assertDBContent(&cache.getConfig().getDBAgent(), statement, expected);
+}
+#define assertCustomIncidentStatusInDB(E, ...) \
+cut_trace(_assertCustomIncidentStatusInDB(E, ##__VA_ARGS__))
+
+void _assertCustomIncidentStatusesInDB(
+  const set<CustomIncidentStatusIdType> &excludeCustomIncidentStatusIdSet)
+{
+	string statement = "select * from custom_incident_statuses ";
+	statement += " ORDER BY id ASC";
+	string expect;
+	for (size_t i = 0; i < NumTestCustomIncidentStatus; i++) {
+		CustomIncidentStatusIdType customIncidentStatusId = i + 1;
+		CustomIncidentStatus customIncidentStatus
+		  = testCustomIncidentStatus[i];
+		customIncidentStatus.id = customIncidentStatusId;
+		set<CustomIncidentStatusIdType>::iterator it
+		  = excludeCustomIncidentStatusIdSet.find(customIncidentStatusId);
+		if (it != excludeCustomIncidentStatusIdSet.end())
+			continue;
+		expect += makeCustomIncidentStatusOutput(customIncidentStatus);
+	}
+	ThreadLocalDBCache cache;
+	assertDBContent(&cache.getConfig().getDBAgent(), statement, expect);
+}
+#define assertCustomIncidentStatusesInDB(E) \
+cut_trace(_assertCustomIncidentStatusesInDB(E))
+
+void test_getCustomIncidentStatuses()
+{
+	assertCustomIncidentStatuses();
+}
+
+static void createTestCustomIncidentStatus(
+  CustomIncidentStatus &customIncidentStatus)
+{
+	customIncidentStatus.id     = AUTO_INCREMENT_VALUE;
+	customIncidentStatus.code  = "__DEFAULT_CODE";
+	customIncidentStatus.label  = "UserDefined 1";
+}
+
+static void createPostData(const CustomIncidentStatus &customIncidentStatus,
+			   StringMap &params)
+{
+	params["id"]     = StringUtils::toString(customIncidentStatus.id);
+	params["code"]  = customIncidentStatus.code;
+	params["label"]  = customIncidentStatus.label;
+}
+
+void test_addCustomIncidentStatus(void)
+{
+	const CustomIncidentStatusIdType expectedId
+	  = NumTestCustomIncidentStatus + 1;
+	CustomIncidentStatus customIncidentStatus;
+	StringMap params;
+	UserIdType userId = 2; // FIXME
+	createTestCustomIncidentStatus(customIncidentStatus);
+	createPostData(customIncidentStatus, params);
+
+	assertAddCustomIncidentStatus(params, userId, HTERR_OK, expectedId);
+	customIncidentStatus.id = expectedId;
+	assertCustomIncidentStatusInDB(customIncidentStatus);
+}
+
+void test_updateCustomIncidentStatus(void)
+{
+	UserIdType userId = 2; // FIXME
+	const CustomIncidentStatusIdType targetId = 2;
+	CustomIncidentStatus customIncidentStatus
+	  = testCustomIncidentStatus[targetId - 1];
+	customIncidentStatus.id    = targetId;
+	customIncidentStatus.code = "IN PROGRESS";
+	customIncidentStatus.label = "In progress (edited)";
+	StringMap params;
+	createPostData(customIncidentStatus, params);
+
+	assertUpdateCustomIncidentStatus(params, targetId, userId, HTERR_OK);
+	assertCustomIncidentStatusInDB(customIncidentStatus, targetId);
+}
 } // namespace testFaceRestCustomIncidentStatus
