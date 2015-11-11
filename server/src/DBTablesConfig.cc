45ebf63537077f19369c0727a14531e3ee46a33c
Takuro Ashie <ashie@clear-code.com>
2015-11-11 18:13:48 +0900

    [DBTablesConfig] Use const reference for arguments of some setter functions

--- server/src/DBTablesConfig.cc
+++ server/src/DBTablesConfig.cc
@@ -2132,7 +2132,7 @@ const string SeverityRankQueryOption::getTargetColor(void)
 	return m_impl->color;
 }
 
-void SeverityRankQueryOption::setTargetIdList(list<SeverityRankIdType> idList) {
+void SeverityRankQueryOption::setTargetIdList(const list<SeverityRankIdType> &idList) {
 	m_impl->idList = idList;
 }
 
@@ -2263,7 +2263,7 @@ const string CustomIncidentStatusesQueryOption::getTargetLabel(void)
 }
 
 void CustomIncidentStatusesQueryOption::setTargetIdList(
-  list<CustomIncidentStatusIdType> idList) {
+  const list<CustomIncidentStatusIdType> &idList) {
 	m_impl->idList = idList;
 }
 
