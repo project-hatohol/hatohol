86815c08dc47cb38c261ba909ef22a2d9743e55f
Kazuhiro Yamato <kz0817@gmail.com>
2015-11-17 16:21:50 +0900

    [test/setup.sql] Skip creating DB if existing.

--- data/test/setup.sql
+++ data/test/setup.sql
@@ -1,7 +1,7 @@
 GRANT all on ndoutils.* TO ndoutils@localhost IDENTIFIED BY 'admin';
-CREATE DATABASE hatohol_client;
+CREATE DATABASE IF NOT EXISTS hatohol_client;
 GRANT ALL PRIVILEGES ON hatohol_client.* TO hatohol@localhost IDENTIFIED BY 'hatohol';
-CREATE DATABASE test_hatohol_client;
+CREATE DATABASE IF NOT EXISTS test_hatohol_client;
 GRANT ALL PRIVILEGES ON test_hatohol_client.* TO hatohol@localhost IDENTIFIED BY 'hatohol';
 GRANT USAGE ON *.* TO ''@'localhost';
 GRANT ALL PRIVILEGES ON `test\_%`.* TO ''@'localhost';
