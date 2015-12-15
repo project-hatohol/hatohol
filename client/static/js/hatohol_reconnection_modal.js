d8b1306dd0415d34a84df4b36199965c3e84863f
Kazuhiro Yamato <kz0817@gmail.com>
2015-12-15 15:20:33 +0900

    [WebUI] Add a new class HatoholReconnectionModal.
    This class shows a modal on which retry countdown is performed.

---
+++ client/static/js/hatohol_reconnection_modal.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2015 Project Hatohol
+ *
+ * This file is part of Hatohol.
+ *
+ * Hatohol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License, version 3
+ * as published by the Free Software Foundation.
+ *
+ * Hatohol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Hatohol. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+var HatoholReconnectModal = function(retryFunc, errorMsg) {
+  var self = this;
+  var DEFAULT_RETRY_INTERVAL = 30;
+  self.timeToReconnect = DEFAULT_RETRY_INTERVAL;
+
+  HatoholModal.apply(self, [{
+    "id": "reconnectionModal",
+    "title": "Connection Failure",
+    "body": $(getBodyHTML()),
+  }]);
+
+  setTimeout(function() {self.countdown();}, 1000);
+
+  function getBodyHTML() {
+    var s = "<p>";
+    if (errorMsg) {
+      s += errorMsg;
+      s += "<hr>";
+    }
+    s += "Try to reconnect after " + self.timeToReconnect + " sec.";
+    s += "</p>";
+    return s;
+  };
+
+  self.countdown = function() {
+    self.timeToReconnect -= 1;
+    if (self.timeToReconnect == 0) {
+      self.close(retryFunc);
+    } else {
+      self.updateBody($(getBodyHTML()));
+      setTimeout(function() {self.countdown();}, 1000);
+    }
+  };
+
+};
+
+HatoholReconnectModal.prototype = Object.create(HatoholModal.prototype);
+HatoholReconnectModal.prototype.constructor = HatoholReconnectModal;
+
