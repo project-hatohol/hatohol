20ffd7b6718c97052a54ad3411b03361ef5147a1
Hiroshi Hatake <hatake@clear-code.com>
2016-01-06 15:58:17 +0900

    [client][livestatus][js.plugins] Use "Livestatus" notation

--- client/static/js.plugins/hap2-nagios-livestatus.js
+++ client/static/js.plugins/hap2-nagios-livestatus.js
@@ -1,4 +1,4 @@
 (function(hatohol) {
   var self = hatohol.registerPlugin("6f024e3e-a2cd-11e5-bfc7-d43d7e3146fb",
-                                    "Nagios LiveStatus (HAPI2)");
+                                    "Nagios Livestatus (HAPI2)");
 }(hatohol));
