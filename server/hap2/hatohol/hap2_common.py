ca44d75b6e742c2d7a37583b67914285ddc4aa57
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2016-02-03 19:30:44 +0900

    [hap2] Create hap2_common
    hap2_common's functions are cut out from haplib.Utils.
    The functioins is high versatility to create hap2 plugins.

---
+++ server/hap2/hatohol/hap2_common.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: UTF-8
+#
+#  Copyright (C) 2015 Project Hatohol
+#
+#  This file is part of Hatohol.
+#
+#  Hatohol is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License, version 3
+#  as published by the Free Software Foundation.
+#
+#  Hatohol is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with Hatohol. If not, see
+#  <http://www.gnu.org/licenses/>.
+
+import time
+import math
+import inspect
+import calendar
+from datetime import datetime
+from datetime import timedelta
+
+def translate_unix_time_to_hatohol_time(unix_time, ns=0):
+    """
+    Translate unix_time into a time string of HAPI 2.0.
+
+    @param unix_time An unix time (integer or string).
+    @param ns A nanosecnd part of the time (integer or string).
+    @return A timestamp string in HAPI2.0
+    """
+    utc_time = time.gmtime(int(unix_time))
+    hatohol_time = time.strftime("%Y%m%d%H%M%S", utc_time)
+    ns_int = int(ns)
+    if ns_int > 1000000000 or ns_int < 0:
+        raise ValueError("Invalid 'ns': %s" % ns)
+    return hatohol_time + ".%09d" % ns_int
+
+def translate_hatohol_time_to_unix_time(hatohol_time):
+    ns = int()
+    if "." in hatohol_time:
+        hatohol_time, ns = hatohol_time.split(".")
+    date_time = datetime.strptime(hatohol_time, "%Y%m%d%H%M%S")
+    unix_time =  calendar.timegm(date_time.timetuple())
+    return unix_time + int(ns) / float(10 ** 9)
+
+def get_biggest_num_of_dict_array(array, key):
+    last_info = None
+
+    digit = int()
+    for target_dict in array:
+        if isinstance(target_dict[key], int):
+            break
+        if digit < len(target_dict[key]):
+            digit = len(target_dict[key])
+
+    for target_dict in array:
+        target_value = target_dict[key]
+        if isinstance(target_value, str) or isinstance(target_value, unicode):
+            target_value = target_value.zfill(digit)
+
+        if last_info < target_value:
+            last_info = target_value
+
+    return last_info
+
+def get_current_hatohol_time():
+    utc_now = datetime.utcnow()
+    return utc_now.strftime("%Y%m%d%H%M%S.") + str(utc_now.microsecond)
+
+def conv_to_hapi_time(date_time, offset=timedelta()):
+    """
+    Convert a datetime object to a string formated for HAPI
+    @param date_time A datatime object
+    @param offset    An offset to the time
+    @return A string of the date and time in HAPI2.0
+    """
+    adjust_time = date_time + offset
+    return adjust_time.strftime("%Y%m%d%H%M%S.") \
+                + "%06d" % adjust_time.microsecond
+
+def translate_int_to_decimal(nano_sec):
+    return float(nano_sec) / 10 ** (int(math.log10(nano_sec)) + 1)
+
+def get_top_file_name():
+    current_frame = inspect.currentframe()
+    while True:
+        if current_frame is None:
+            break
+        top_frame = current_frame
+        current_frame = top_frame.f_back
+
+    top_file_name = top_frame.f_globals["__file__"]
+    slash_index = top_file_name.rfind("/")
+    if slash_index:
+        top_file_name = top_file_name[slash_index+1:]
+
+    return top_file_name
---
+++ server/hap2/hatohol/test/TestHap2Common.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+"""
+  Copyright (C) 2015 Project Hatohol
+
+  This file is part of Hatohol.
+
+  Hatohol is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License, version 3
+  as published by the Free Software Foundation.
+
+  Hatohol is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with Hatohol. If not, see
+  <http://www.gnu.org/licenses/>.
+"""
+
+import json
+import unittest
+import datetime
+import testutils
+from hatohol import haplib
+from hatohol import hap2_common
+import inspect
+
+class Utils(unittest.TestCase):
+    def test_translate_unix_time_to_hatohol_time(self):
+        result = hap2_common.translate_unix_time_to_hatohol_time(0)
+        self.assertEquals(result, "19700101000000.000000000")
+        result = hap2_common.translate_unix_time_to_hatohol_time("0")
+        self.assertEquals(result, "19700101000000.000000000")
+        result = hap2_common.translate_unix_time_to_hatohol_time(0, 123456789)
+        self.assertEquals(result, "19700101000000.123456789")
+        result = hap2_common.translate_unix_time_to_hatohol_time(0, 123456)
+        self.assertEquals(result, "19700101000000.000123456")
+        result = hap2_common.translate_unix_time_to_hatohol_time("0", "123456789")
+        self.assertEquals(result, "19700101000000.123456789")
+
+        result = hap2_common.translate_unix_time_to_hatohol_time("0", "999999999")
+        self.assertEquals(result, "19700101000000.999999999")
+
+    def test_translate_unix_time_to_hatohol_time_with_errors(self):
+        self.assertRaises(
+            ValueError,
+            hap2_common.translate_unix_time_to_hatohol_time, 0, -1)
+
+        self.assertRaises(
+            ValueError,
+            hap2_common.translate_unix_time_to_hatohol_time, 0, 10000000000)
+
+        self.assertRaises(
+            ValueError,
+            hap2_common.translate_unix_time_to_hatohol_time, 0, "")
+
+
+    def test_translate_hatohol_time_to_unix_time(self):
+        result = hap2_common.translate_hatohol_time_to_unix_time("19700101000000.123456789")
+        # This result is utc time
+        self.assertAlmostEquals(result, 0.123456789, delta=0.000000001)
+
+    def test_get_biggest_num_of_dict_array(self):
+        test_target_array = [{"test_value": 3},
+                             {"test_value": 7},
+                             {"test_value": 9}]
+        result = hap2_common.get_biggest_num_of_dict_array(test_target_array,
+                                                            "test_value")
+        self.assertEquals(result, 9)
+
+    def test_get_current_hatohol_time(self):
+        result = hap2_common.get_current_hatohol_time()
+
+        self.assertTrue(14 < len(result) < 22)
+        ns = int(result[15: 21])
+        self.assertTrue(0 <= ns < 1000000)
+
+    def test_conv_to_hapi_time(self):
+        dt = datetime.datetime(2015, 6, 28, 9, 35, 11, 123456)
+        self.assertEquals(hap2_common.conv_to_hapi_time(dt),
+                          "20150628093511.123456")
+
+    def test_conv_to_hapi_time_with_offset(self):
+        dt = datetime.datetime(2015, 6, 28, 9, 35, 11, 123456)
+        ofs = -datetime.timedelta(hours=1, minutes=35, seconds=1)
+        self.assertEquals(hap2_common.conv_to_hapi_time(dt, ofs),
+                          "20150628080010.123456")
+
+    def test_translate_int_to_decimal(self):
+        test_nano_sec = 123456789
+        expect_result = 0.123456789
+        result = hap2_common.translate_int_to_decimal(test_nano_sec)
+
+        self.assertEquals(result, expect_result)
+
+    def test_get_top_file_name(self):
+        expect_result = "runpy.pyc"
+        self.assertEquals(hap2_common.get_top_file_name(), expect_result)
