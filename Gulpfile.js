6592a6be28e74ea88fd1aa49c0c29382079ad13e
Hiroshi Hatake <hatake@clear-code.com>
2016-02-12 14:47:02 +0900

    [gulp][js][lint] Fix a wrong specified file name

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var jshint = require("gulp-jshint");
 
 gulp.task("lint", function() {
-  gulp.src(["./client/static/js/*.js", "!./client/static/js/hatohol_connector.js"])
+  gulp.src(["./client/static/js/*.js", "!./client/static/js/hatohol_def.js"])
     .pipe(jshint())
     .pipe(jshint.reporter("default"));
 });
