fae97187544706cc1a94dd4408c56be35f4f5b4d
Hiroshi Hatake <hatake@clear-code.com>
2016-02-18 14:49:13 +0900

    [Gulp][package.json] Use gulp-phantomjs
    Because this thin wrapper makes to be able to run unified command line
    interface thorough gulp.
    
    In this case, `$(npm bin)/gulp browsertest` will execute browser tests.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require("gulp");
 var jshint = require("gulp-jshint");
+var mochaPhantomJS = require('gulp-mocha-phantomjs');
+
+gulp.task('browsertest', function () {
+  var phantomJSConfig = {
+    reporter: 'spec',
+    phantomjs: {
+      viewportSize: {
+        width: 1024,
+        height: 768
+      },
+      useColors:true
+    }
+  };
+  var stream = mochaPhantomJS(phantomJSConfig);
+  stream.write({path: 'http://localhost:8000/test/index.html'});
+  stream.end();
+  return stream;
+});
 
 gulp.task("lint", function() {
   gulp.src(["./client/static/js/*.js", "./client/static/js.plugins/*.js",
