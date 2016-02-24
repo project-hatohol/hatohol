9ef8516ad0de8fbba09e549122753e79dee7afb2
Hiroshi Hatake <hatake@clear-code.com>
2016-02-24 16:43:56 +0900

    [SeverityRanksView] Add missing parentheses in constructor

--- client/static/js/severity_ranks_view.js
+++ client/static/js/severity_ranks_view.js
@@ -92,7 +92,7 @@ var SeverityRanksView = function(userProfile) {
   }
 
   function saveSeverityRank(status) {
-    var deferred = new $.Deferred;
+    var deferred = new $.Deferred();
     var url = "/severity-rank";
     url += "/" + status;
     new HatoholConnector({
