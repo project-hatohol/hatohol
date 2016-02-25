cfd943be0ae3d4cf3e5759423c3c8026d03957ca
Hiroshi Hatake <hatake@clear-code.com>
2016-02-25 17:43:10 +0900

    [FaceRestHost][Triggers] Add REST API test case for "hostname" query

--- server/test/testFaceRestHost.cc
+++ server/test/testFaceRestHost.cc
@@ -162,7 +162,8 @@ static void _assertTriggers(
   const LocalHostIdType &hostIdInServer = ALL_LOCAL_HOSTS,
   const timespec &beginTime = {0, 0},
   const timespec &endTime = {0, 0},
-  const size_t expectedNumTrigger = -1)
+  const size_t expectedNumTrigger = -1,
+  const string &hostname = "")
 {
 	loadTestDBArmPlugin();
 	loadTestDBTriggers();
@@ -209,6 +210,8 @@ static void _assertTriggers(
 	if (endTime.tv_sec != 0 || endTime.tv_nsec != 0) {
 		queryMap["endTime"] = StringUtils::sprintf("%ld", endTime.tv_sec);
 	}
+	if (!hostname.empty())
+		queryMap["hostname"] = hostname;
 	arg.parameters = queryMap;
 	JSONParser *parser = getResponseAsJSONParser(arg);
 	unique_ptr<JSONParser> parserPtr(parser);
