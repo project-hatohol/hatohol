170720b73f9b5f34396e29e6a4eddadfeda1bbd6
Hiroshi Hatake <hatake@clear-code.com>
2016-02-25 10:04:42 +0900

    [HatoholNavi] Use a type safe comparison operator

--- client/static/js/hatohol_navi.js
+++ client/static/js/hatohol_navi.js
@@ -156,7 +156,7 @@ var HatoholNavi = function(userProfile, currentPage) {
   };
 
   var createMenuItem = function(menuItem) {
-    if (menuItem.flags != undefined &&
+    if (menuItem.flags !== undefined &&
         !userProfile.hasFlags(menuItem.flags))
     {
       return null;
