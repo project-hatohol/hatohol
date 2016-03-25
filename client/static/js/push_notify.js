11d2d196bcc64775ddf758f1d9f6fe853b102e1a
mmikan22 <masakichi@serainno.co.jp>
2016-03-25 15:46:05 +0000

    [WebUI:All page] Push no-monitoring alert

--- client/static/js/push_notify.js
+++ client/static/js/push_notify.js
@@ -1,51 +1,53 @@
-$(function() {
+(function(){
   "use strict";
   var permissionLevel;
   var importantEventCount = 0;
   var noMonitoringTime = 0;
   var timeLimit = 10;
   var pushInterval = 20;
-  if (notify.isSupported) {
-    if (notify.permissionLevel() !== notify.PERMISSION_GRANTED) {
-      notify.requestPermission (function() {
-        permissionLevel = notify.permissionLevel();
-      });
-    } else {
-      permissionLevel = notify.permissionLevel();
-    }
 
-    if (permissionLevel === "granted") {
-      setInterval(severityMonitor, 1000);
-
-      function severityMonitor() {
-        noMonitoringTime ++;
-        if (
-          noMonitoringTime >= timeLimit &&
-          (noMonitoringTime - timeLimit) % pushInterval === 0
-        ) {
-          pushWarning();
-        }
-      }
-
-      function pushWarning() {
-        importantEventCount ++;
-        if (importantEventCount > 0) {
-          notify.createNotification(
-            gettext('Failure has occurred.'),
-            {
-              body: gettext('There are ' + importantEventCount + ' unmet important events .'),
-              icon: '/static/images/icon_push_hatohol.png'
-            }
-          );
-        }
-      }
-
-      $(document).on('mousemove mousedown click keydown focus', importantEventCountReset);
-
-      function importantEventCountReset() {
-        noMonitoringTime = 0;
-        importantEventCount = 0;
-      }
+  function severityMonitor() {
+    noMonitoringTime ++;
+    if (
+      noMonitoringTime >= timeLimit &&
+      (noMonitoringTime - timeLimit) % pushInterval === 0
+    ) {
+      pushWarning();
     }
   }
-});
+
+  function pushWarning() {
+    importantEventCount ++;
+    if (importantEventCount > 0) {
+      notify.createNotification(
+        gettext('Failure has occurred.'),
+        {
+          body: gettext('There are ' + importantEventCount + ' unmet important events .'),
+          icon: '/static/images/icon_push_hatohol.png'
+        }
+      );
+    }
+  }
+
+  $(document).on('mousemove mousedown click keydown focus', importantEventCountReset);
+
+  function importantEventCountReset() {
+    noMonitoringTime = 0;
+    importantEventCount = 0;
+  }
+  $(function() {
+    if (notify.isSupported) {
+      if (notify.permissionLevel() !== notify.PERMISSION_GRANTED) {
+        notify.requestPermission (function() {
+          permissionLevel = notify.permissionLevel();
+        });
+      } else {
+        permissionLevel = notify.permissionLevel();
+      }
+  
+      if (permissionLevel === "granted") {
+        setInterval(severityMonitor, 1000);
+      }
+    }
+  });
+})();
