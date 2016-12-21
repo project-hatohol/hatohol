073ba1dba3cc4a66ea82ca71a9c9b855dbdc49f9
Kazuhiro Yamato <kz0817@gmail.com>
2016-12-21 14:08:36 +0900

    [WebUI] Fix the build error for JS po files.
    This patch merged into the following
    
    commit 0bb46db6c287f80cf01685c3d8a2379679d91d1f
    Author: Kazuhiro Yamato <kz0817@gmail.com>
    Date:   Tue Dec 20 16:07:01 2016 +0900
    
        [WebUI] Update Japanese translation files.
    
    Similary, the following commit should be merged int the above.
    
    22d902c684af34def45d919deed8ae2249f74451
    Author: Kazuhiro Yamato <kz0817@gmail.com>
    Date:   Wed Dec 21 13:06:42 2016 +0900
    
        [WebUI] Update last author's name.

--- client/static/js/before_close.js
+++ client/static/js/before_close.js
@@ -5,8 +5,7 @@
   $(window).on("beforeunload",function(){
     if (isTransitAllowed)
       return;
-    return gettext("Hatohol is running to monitor system(s).\n" +
-    "You are trying to leave or close this page.");
+    return gettext("Hatohol is running to monitor system(s).\nYou are trying to leave or close this page.");
   });
 
   hatoholTracer.addListener(HatoholTracePoint.PRE_HREF_CHANGE, function() {
