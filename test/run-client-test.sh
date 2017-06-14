371f0632255ce952e4a25fb97ee0f2312a1ae0cd
otakuto <otakuto893@gmail.com>
2017-06-14 15:51:47 +0900

    change jshint to eslint

--- test/run-client-test.sh
+++ test/run-client-test.sh
@@ -12,6 +12,6 @@ ${TOP_DIR}/test/launch-hatohol-for-test.sh || exit 1
 $(npm bin)/mocha-phantomjs http://localhost:8000/test/index.html || FAILED=1
 # manage.py has a child process. We have to kill it too.
 cat $PIDS_FILE | awk '{ print "ps h -p " $1 " --ppid " $1 " -o pid" }' | sh | awk '{ print "kill " $1 }' | sh
-$(npm bin)/gulp lint || FAILED=1
+npm run lint || FAILED=1
 
 exit $FAILED
